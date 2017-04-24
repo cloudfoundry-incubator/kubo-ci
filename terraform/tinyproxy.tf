d47dd94a8d800b1131021f72722bbbd78cff0b6d
Josh Winters <jwinters@pivotal.io>
2017-04-24 17:50:04 -0400

    Start a terraform script to deploy tinyproxy
    [#139670355]
    
    Signed-off-by: Felix Reyn <freyn@pivotal.io>

---
+++ terraform/tinyproxy.tf
@@ -0,0 +1,55 @@
+variable "latest_ubuntu" {
+  type = "string"
+  default = "ubuntu-1404-trusty-v20161109"
+}
+
+provider "google" {
+  project = "${var.projectid}"
+  region = "${var.region}"
+  credentials = "${var.service_account_key}"
+}
+
+variable "service_account_key" { }
+
+variable "projectid" { }
+
+variable "region" { }
+
+variable "zone" { }
+
+variable "prefix" { }
+
+variable "subnetwork" { }
+
+resource "google_compute_instance" "tinyproxy" {
+  name         = "${var.prefix}-tinyproxy"
+  machine_type = "n1-standard-1"
+  zone         = "${var.zone}"
+
+  tags = ["internal"]
+
+  disk {
+    image = "${var.latest_ubuntu}"
+  }
+
+  network_interface {
+    subnetwork = "${var.subnetwork}"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+
+  metadata_startup_script = <<EOT
+  #!/bin/bash
+
+  apt-get update
+  apt-get install -y tinyproxy
+  sed -i 's#Allow 127.0.0.1#Allow 0.0.0.0/0#g' /etc/tinyproxy.conf
+  service tinyproxy start
+  EOT
+}
+
+output "proxy_ip" {
+  value = "${google_compute_instance.tinyproxy.network_interface.0.address}"
+}
