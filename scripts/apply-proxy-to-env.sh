f5d5fd856d3e44b4d993704ab0208481544a18a5
Oleksandr Slynko <oslynko@pivotal.io>
2017-05-15 11:49:58 -0400

    Remove internetless opsfile when applying proxy settings to env
    Signed-off-by: Josh Winters <jwinters@pivotal.io>

--- scripts/apply-proxy-to-env.sh
+++ scripts/apply-proxy-to-env.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 
 cp -r kubo-lock/* kubo-lock-with-proxy/
-bosh-cli interpolate kubo-lock/metadata --ops-file=git-kubo-ci/ops-files/internetless.yml > kubo-lock-with-proxy/metadata
 
 proxy_setting=$(bosh-cli int - --path /proxy_setting < proxy-tf/metadata)
 echo "http_proxy: $proxy_setting" >> kubo-lock-with-proxy/metadata
