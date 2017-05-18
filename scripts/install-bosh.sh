5ce07d7a0cc9eb3077024078bc67757c155d9b23
Konstantin Semenov <ksemenov@pivotal.io>
2017-05-18 12:32:20 +0100

    Modify install_bosh to support multiple IaaS platforms
    [#135571805]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/install-bosh.sh
+++ scripts/install-bosh.sh
@@ -1,14 +1,22 @@
 #!/bin/sh -e
 
 . "$(dirname "$0")/lib/environment.sh"
-bosh-cli int kubo-lock/metadata --path=/gcp_service_account > "$PWD/key.json"
+iaas=$(bosh-cli int kubo-lock/metadata --path=/iaas)
+
 
 set -x
 export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="$PWD/bosh.log"
 cp "kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
 
-"${KUBO_DEPLOYMENT_DIR}/bin/deploy_bosh" "${KUBO_ENVIRONMENT_DIR}" "$PWD/key.json"
+if [ "$iaas" = "gcp" ]; then
+  set +x
+  bosh-cli int kubo-lock/metadata --path=/gcp_service_account > "$PWD/key.json"
+  set -x
+  "${KUBO_DEPLOYMENT_DIR}/bin/deploy_bosh" "${KUBO_ENVIRONMENT_DIR}" "$PWD/key.json"
+else
+  "${KUBO_DEPLOYMENT_DIR}/bin/deploy_bosh" "${KUBO_ENVIRONMENT_DIR}"
+fi
 
 cp "${KUBO_ENVIRONMENT_DIR}/creds.yml" "$PWD/bosh-creds/"
 cp "${KUBO_ENVIRONMENT_DIR}/state.json" "$PWD/bosh-state/"
