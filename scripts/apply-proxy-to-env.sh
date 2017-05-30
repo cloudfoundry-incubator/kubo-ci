9188627e085e14d7a0cda6c27a4f3fb0a739f9cf
Brendan Nolan <jnolan@pivotal.io>
2017-05-30 14:14:54 +0100

    Add extra line to lock between new values
    [#144469421]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/apply-proxy-to-env.sh
+++ scripts/apply-proxy-to-env.sh
@@ -3,5 +3,6 @@
 cp -r kubo-lock/* kubo-lock-with-proxy/
 
 proxy_setting=$(bosh-cli int - --path /proxy_setting < proxy-tf/metadata)
+echo >> kubo-lock-with-proxy/metadata
 echo "http_proxy: $proxy_setting" >> kubo-lock-with-proxy/metadata
 echo "https_proxy: $proxy_setting" >> kubo-lock-with-proxy/metadata
