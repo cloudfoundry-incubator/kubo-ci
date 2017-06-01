a00b35d9dd00b139ec26562c89dec70c4afadb1a
Akshay Mankar <amankar@pivotal.io>
2017-06-01 13:46:29 +0100

    Add vsphere-cleaner to the docker image
    [#145685517]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- docker-images/system_tests/container_test.go
+++ docker-images/system_tests/container_test.go
@@ -17,7 +17,6 @@ var _ = Describe("Docker Image", func() {
 		"credhub":  []string{"--version"},
 		"gcloud":   []string{"--version"},
 		"ginkgo":   []string{"help"},
-		"govc":     []string{"version"},
 		"make":     []string{"-v"},
 		"ruby":     []string{"-v"},
 		"kubectl":  []string{"help"},
@@ -35,4 +34,11 @@ var _ = Describe("Docker Image", func() {
 			Eventually(session, "5s").Should(Exit(0))
 		})
 	}
+
+	It("has vsphere-cleaner installed", func() {
+		command := exec.Command("which", "vsphere-cleaner")
+		session, err := Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).ToNot(HaveOccurred())
+		Eventually(session, "1s").Should(Exit(0))
+	})
 })
