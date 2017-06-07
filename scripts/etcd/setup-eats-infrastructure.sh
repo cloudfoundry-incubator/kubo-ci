b641564833032d7a08d9e1cdef95055440a4d6d3
Brendan Nolan <bnolan@pivotal.io>
2017-06-07 14:09:15 +0100

    Create separate task to deploy BOSH
    * EATS expects tests to run with BOSH without UAA
    
    [#141623859]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

---
+++ scripts/etcd/setup-eats-infrastructure.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+set +x
+bosh-cli int kubo-lock/metadata --path=/gcp_service_account > "key.json"
+set -x
+
+bosh-cli create-env "git-kubo-deployment/bosh-deployment/bosh.yml"  \
+  --ops-file "git-kubo-deployment/bosh-deployment/gcp/cpi.yml" \
+  --ops-file "git-kubo-ci/etcd/bosh_admin_user_ops_file.yml" \
+  --state "bosh-state/state.json" \
+  --var-file gcp_credentials_json=key.json \
+  --vars-store "bosh-creds/creds.yml" \
+  --vars-file "kubo-lock/metadata"
+
