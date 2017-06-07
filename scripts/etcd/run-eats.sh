fbc36479316decd94becc6c600cccf1702d2271e
Brendan Nolan <bnolan@pivotal.io>
2017-06-07 09:49:47 +0100

    Add script and task to run EATS
    [#141623859]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

---
+++ scripts/etcd/run-eats.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -eu
+
+root="${PWD}"
+
+TEST_etcd_release_version="99999+dev.$(date +%s)"
+TEST_stemcell_version="$(cat "${root}/stemcell/version")"
+TEST_latest_etcd_release_version="$(cat "${root}/latest-etcd-release/version")"
+export TEST_etcd_release_version TEST_stemcell_version TEST_latest_etcd_release_version
+
+consul_release_version="$(cat "${root}/consul-release/version")"
+turbulence_release_version="$(cat "${root}/turbulence-release/version")"
+
+
+function main() {
+  setup_bosh_env_vars
+
+  upload_stemcell
+  upload_releases
+  force_compilation
+
+  bosh -d eats -n deploy "${root}/etcd-release/manifests/eats.yml" \
+    --vars-env=TEST \
+    --var="bosh_client=${BOSH_CLIENT}" \
+    --var="bosh_client_secret=${BOSH_CLIENT_SECRET}" \
+    --var="bosh_ca_cert=${BOSH_CA_CERT}" \
+    --var="bosh_environment=${BOSH_ENVIRONMENT}"
+
+  bosh -d eats run-errand acceptance-tests
+
+  bosh -n clean-up --all
+}
+
+function setup_bosh_env_vars() {
+  BOSH_CLIENT=bosh_admin
+  BOSH_CLIENT_SECRET=$(bosh-cli int gcs-bosh-creds/creds.yml --path=/bosh_admin_client_secret)
+  BOSH_CA_CERT=$(bosh-cli int gcs-bosh-creds/creds.yml --path=/default_ca/ca)
+  BOSH_ENVIRONMENT=$(bosh-cli int kubo-lock/metadata --path=/internal_ip)
+  export BOSH_ENVIRONMENT BOSH_CA_CERT BOSH_CLIENT BOSH_CLIENT_SECRET
+}
+
+function upload_stemcell() {
+  bosh upload-stemcell stemcell/stemcell.tgz
+}
+
+function upload_releases() {
+  bosh upload-release turbulence-release/release.tgz
+  bosh upload-release consul-release/release.tgz
+  bosh upload-release latest-etcd-release/release.tgz
+
+  bosh -n create-release --force --version "${TEST_etcd_release_version}" --dir=etcd-release
+  bosh upload-release --dir=etcd-release
+}
+
+function force_compilation() {
+    sed -e "s/CONSUL_RELEASE_VERSION/${consul_release_version}/g" \
+      -e "s/ETCD_RELEASE_VERSION/${TEST_etcd_release_version}/g" \
+      -e "s/turbulence_release_version/${turbulence_release_version}/g" \
+      -e "s/STEMCELL_VERSION/${TEST_stemcell_version}/g" \
+      "${root}/ci/scripts/etcd/run-eats/fixtures/eats_compilation.yml" \
+      > "${root}/eats_compilation.yml"
+
+    bosh -d compilation -n deploy "${root}/eats_compilation.yml"
+    bosh -d compilation export-release "etcd/${TEST_etcd_release_version}" "ubuntu-trusty/${TEST_stemcell_version}"
+    bosh -d compilation export-release "consul/${consul_release_version}" "ubuntu-trusty/${TEST_stemcell_version}"
+    bosh -d compilation export-release "turbulence/${turbulence_release_version}" "ubuntu-trusty/${TEST_stemcell_version}"
+    bosh -d compilation -n delete-deployment
+}
+
+function teardown() {
+  set +e
+  bosh -d eats -n delete-deployment
+  bosh -n delete-release etcd
+  bosh -n delete-release consul
+  bosh -n delete-release turbulence
+  set -e
+}
+
+trap teardown EXIT
+
+main
