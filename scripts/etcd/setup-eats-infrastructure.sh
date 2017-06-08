1d59ad8a9af90b725bb2d0a723728e98b60f8191
Brendan Nolan <bnolan@pivotal.io>
2017-06-08 12:23:30 +0100

    Switch to using powerdns from local_dns
    [#141623859]
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- scripts/etcd/setup-eats-infrastructure.sh
+++ scripts/etcd/setup-eats-infrastructure.sh
@@ -6,8 +6,8 @@ set -x
 
 bosh-cli create-env "git-kubo-deployment/bosh-deployment/bosh.yml"  \
   --ops-file "git-kubo-deployment/bosh-deployment/gcp/cpi.yml" \
+  --ops-file "git-kubo-deployment/bosh-deployment/powerdns.yml" \
   --ops-file "git-kubo-ci/etcd/bosh_admin_user_ops_file.yml" \
-  --ops-file "git-kubo-ci/etcd/bosh_local_dns_ops_file.yml" \
   --state "bosh-state/state.json" \
   --var-file gcp_credentials_json=key.json \
   --vars-store "bosh-creds/creds.yml" \
