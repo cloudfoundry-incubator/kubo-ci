a1d59162f347a5ecb7c009a434c3019e7fb39c84
Felix Reyn <freyn@pivotal.io>
2017-06-21 12:13:45 -0400

    BOSH-deployed tinyproxy for current-branch CI
    [#147529923]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/apply-proxy-to-env.sh
+++ scripts/apply-proxy-to-env.sh
@@ -2,9 +2,23 @@
 
 set -exu -o pipefail
 
+. "$(dirname "$0")/lib/environment.sh"
+
+export BOSH_ENV="${KUBO_ENVIRONMENT_DIR}"
+export BOSH_NAME=$(basename ${BOSH_ENV})
+export DEBUG=1
+
+cp "$PWD/kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
+cp "$PWD/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
+
+. "$PWD/git-kubo-deployment/bin/lib/deploy_utils"
+. "$PWD/git-kubo-deployment/bin/set_bosh_environment"
+
+tinyproxy_ip=$(BOSH_CLIENT=bosh_admin BOSH_CLIENT_SECRET="$(get_bosh_secret)" bosh-cli -n -e "${BOSH_ENVIRONMENT}" -d tinyproxy vms --json | bosh-cli int - --path='/Tables/Content=vms/Rows/0/ips')
+proxy_setting="$tinyproxy_ip:8888"
+
 cp -r kubo-lock/* kubo-lock-with-proxy/
 
-proxy_setting=$(bosh-cli int - --path /proxy_setting < proxy-tf/metadata)
 echo >> kubo-lock-with-proxy/metadata
 echo "http_proxy: $proxy_setting" >> kubo-lock-with-proxy/metadata
 echo "https_proxy: $proxy_setting" >> kubo-lock-with-proxy/metadata
---
+++ scripts/deploy-tinyproxy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -exu -o pipefail
+
+. "$(dirname "$0")/lib/environment.sh"
+
+export BOSH_ENV="${KUBO_ENVIRONMENT_DIR}"
+export BOSH_NAME=$(basename ${BOSH_ENV})
+export DEBUG=1
+
+cp "$PWD/kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
+cp "$PWD/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
+
+. "$PWD/git-kubo-deployment/bin/lib/deploy_utils"
+. "$PWD/git-kubo-deployment/bin/set_bosh_environment"
+
+cloud_config_file="$PWD/git-kubo-ci/utils/tinyproxy/cloud-config.yml"
+manifest_file="$PWD/git-kubo-ci/utils/tinyproxy/manifest.yml"
+stemcell_url=$(bosh-cli int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path='/stemcell_url')
+
+BOSH_CLIENT=bosh_admin BOSH_CLIENT_SECRET="$(get_bosh_secret)" bosh-cli -n -e "${BOSH_ENVIRONMENT}" update-cloud-config "$cloud_config_file" -l "${KUBO_ENVIRONMENT_DIR}/director.yml"
+BOSH_CLIENT=bosh_admin BOSH_CLIENT_SECRET="$(get_bosh_secret)" bosh-cli -n -e "${BOSH_ENVIRONMENT}" upload-stemcell "$stemcell_url"
+BOSH_CLIENT=bosh_admin BOSH_CLIENT_SECRET="$(get_bosh_secret)" bosh-cli -n -e "${BOSH_ENVIRONMENT}" deploy "$manifest_file" -d "tinyproxy"
