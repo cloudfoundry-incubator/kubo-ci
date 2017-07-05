1978fe2b44a59cac29f35d842a7cfaf6655b1fdf
Akshay Mankar <amankar@pivotal.io>
2017-07-05 15:12:33 +0100

    Revert "Try to run ETCD pipeline in AWS"
    This reverts commit 7c1bafeb5f9bb29c9069f320fb1d8c444d3191ab.

--- scripts/etcd/setup-eats-infrastructure.sh
+++ scripts/etcd/setup-eats-infrastructure.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -e
 
+set +x
+bosh-cli int kubo-lock/metadata --path=/gcp_service_account > "key.json"
+set -x
+
 bosh-cli create-env "git-kubo-deployment/bosh-deployment/bosh.yml"  \
-  --ops-file "git-kubo-deployment/bosh-deployment/aws/cpi.yml" \
+  --ops-file "git-kubo-deployment/bosh-deployment/gcp/cpi.yml" \
   --ops-file "git-kubo-deployment/bosh-deployment/powerdns.yml" \
   --ops-file "git-kubo-deployment/bosh-deployment/jumpbox-user.yml" \
   --ops-file "git-kubo-ci/etcd/bosh_admin_user_ops_file.yml" \
   --ops-file "git-kubo-ci/etcd/increase_bosh_workers.yml" \
   --state "bosh-state/state.json" \
+  --var-file gcp_credentials_json=key.json \
   --vars-store "bosh-creds/creds.yml" \
   --vars-file "kubo-lock/metadata"
