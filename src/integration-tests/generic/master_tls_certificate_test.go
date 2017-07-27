6a41e5b14381fef03c2dd8d2780f9d15b2dba93b
Felix Reyn <freyn@pivotal.io>
2017-07-27 17:55:56 -0400

    Switching to a docker image that resolves partially qualified cluster.local names; also testing bosh fully and partially qualified names
    Signed-off-by: Tony Wong <twong@pivotal.io>

--- src/integration-tests/generic/master_tls_certificate_test.go
+++ src/integration-tests/generic/master_tls_certificate_test.go
@@ -16,8 +16,15 @@ var _ = Describe("MasterTlsCertificate", func() {
 		runner = test_helpers.NewKubectlRunner()
 	})
 
-	It("should be valid for kube-dns names for master", func() {
-		session := runner.RunKubectlCommand("run", "test-master-cert-via-curl", "--image=governmentpaas/curl-ssl", "--restart=Never", "-ti", "--rm", "--", "curl", "https://kubernetes.default.svc.cluster.local", "--cacert", "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+	It("should be valid for fully-qualified names for master", func() {
+		session := runner.RunKubectlCommand("run", "test-master-cert-via-curl", "--image=tutum/curl", "--restart=Never", "-ti", "--rm", "--", "curl", "https://kubernetes.default.svc.cluster.local", "--cacert", "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+		<-session.Exited
+	  stdo := string(session.Out.Contents())
+		Expect(stdo).To(ContainSubstring("User \"system:anonymous\" cannot get path \"/\".: \"No policy matched.\""))
+	})
+
+	It("should be valid for partial names for master", func() {
+		session := runner.RunKubectlCommand("run", "test-master-cert-via-curl", "--image=tutum/curl", "--restart=Never", "-ti", "--rm", "--", "curl", "https://kubernetes", "--cacert", "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
 		<-session.Exited
 	  stdo := string(session.Out.Contents())
 		Expect(stdo).To(ContainSubstring("User \"system:anonymous\" cannot get path \"/\".: \"No policy matched.\""))
