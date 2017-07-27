655ea5304dc731c2876440e74fcb5862e507e042
Oleksandr Slynko <oslynko@pivotal.io>
2017-07-27 16:26:09 +0100

    Kubo tarball is named kubo-version when precompiled
    [#148252187]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- scripts/deploy-k8s-instance.sh
+++ scripts/deploy-k8s-instance.sh
@@ -8,7 +8,7 @@ export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="$PWD/bosh.log"
 export DEBUG=1
 
-tarball_name=$(ls $PWD/gcs-kubo-release-tarball/kubo-release*.tgz | head -n1)
+tarball_name=$(ls $PWD/gcs-kubo-release-tarball/kubo-*.tgz | head -n1)
 
 cp "$PWD/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
 cp "kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
