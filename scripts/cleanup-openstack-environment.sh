7c5d79ba9772d8618def856edffbbb50850a9bed
Oleksandr Slynko <oslynko@pivotal.io>
2017-08-01 15:15:26 +0100

    Delete openstack volumes in maintenance pipeline
    [#149842378]
    
    Signed-off-by: Brendan Nolan <bnolan@pivotal.io>

--- scripts/cleanup-openstack-environment.sh
+++ scripts/cleanup-openstack-environment.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
 
-set -xu # not using -e or pipefail, since list/delete of resources can potentially failed
+set -xue
 
 delete_vms() {
-  network_id=$(bosh-cli int $lock_file --path='/net_id')
-  network_name=$(openstack network list | grep "$network_id" | awk '{print $4}')
-  openstack server list | grep "$network_name" | awk '{print $2}' | xargs -I{} openstack server delete {}
+  network_id=$(bosh-cli int "$lock_file" --path='/net_id')
+  network_name=$(openstack network list -f value | grep "$network_id" | awk '{print $2}')
+  server_names=$(openstack server list -f value | grep "$network_name" | awk '{print $1}')
 
   openstack volume list --status available -c ID | grep -v ID | grep -v '\-----' | awk '{print $2}' | xargs -I{} openstack volume delete {}
 
   internal_ip=$(bosh-cli int $lock_file --path='/internal_ip')
   openstack port list | grep "$internal_ip" | awk '{print $2}' | xargs -I{} openstack port delete {}
+  internal_ip=$(bosh-cli int $lock_file --path='/internal_ip')
+  openstack port list | grep "$internal_ip" | awk '{print $2}' | xargs -I{} openstack port delete {}
+  
+  for server_name in ${server_names}
+  do
+    volume_name=$(openstack server show "${server_name}" -f yaml | bosh int --path /volumes_attached - | cut -d "'" -f2)
+    if [ ! "${volume_name}" == "" ]; then
+      openstack server remove volume "${server_name}" "${volume_name}"
+
+      if timeout 120 /bin/bash <<EOF
+        until openstack volume show "${volume_name}" -f yaml | bosh int --path /status - | grep "available"; do
+          sleep 2
+        done
+EOF
+      then
+        openstack volume delete "${volume_name}" --force
+        echo "The volume became available and was deleted"
+      else
+        echo "The volume never became available and wasn't deleted"
+      fi
+    fi
+
+    openstack server delete "${server_name}"
+  done
+
+
+  internal_ip=$(bosh-cli int "$lock_file" --path='/internal_ip')
+  openstack port list -f value | grep "$internal_ip" | awk '{print $1}' | xargs -I{} openstack port delete {}
 }
 
 export lock_file="kubo-lock-repo/${POOL_NAME}/claimed/${ENV_NAME}"
 
 export OS_REGION_NAME
-OS_REGION_NAME=$(bosh-cli int $lock_file --path='/region')
+OS_REGION_NAME=$(bosh-cli int "$lock_file" --path='/region')
 export OS_PROJECT_NAME
-OS_PROJECT_NAME=$(bosh-cli int $lock_file --path='/openstack_project')
+OS_PROJECT_NAME=$(bosh-cli int "$lock_file" --path='/openstack_project')
 export OS_PASSWORD
-OS_PASSWORD=$(bosh-cli int $lock_file --path='/openstack_password')
+OS_PASSWORD=$(bosh-cli int "$lock_file" --path='/openstack_password')
 export OS_AUTH_URL
-OS_AUTH_URL=$(bosh-cli int $lock_file --path='/auth_url')
+OS_AUTH_URL=$(bosh-cli int "$lock_file" --path='/auth_url')
 export OS_USERNAME
-OS_USERNAME=$(bosh-cli int $lock_file --path='/openstack_username')
+OS_USERNAME=$(bosh-cli int "$lock_file" --path='/openstack_username')
 
 delete_vms
