7993c68638451bbe2b5b2ccf8e07eafa711ecf04
Felix Reyn <freyn@pivotal.io>
2017-08-02 10:16:11 -0400

    Moving the runtime config step for turbulence to kubo-ci; changes to kubo-deployment forthcoming [#149301477]
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

---
+++ scripts/configure-bosh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -exu -o pipefail
+
+. "$(dirname "$0")/lib/environment.sh"
+
+export BOSH_LOG_LEVEL=debug
+export BOSH_LOG_PATH="$PWD/bosh.log"
+export DEBUG=1
+BOSH_ENV="${KUBO_ENVIRONMENT_DIR}"
+
+source "git-kubo-deployment/bin/set_bosh_environment"
+
+turbulence_release_url=$(bosh-cli int "git-kubo-ci/utils/turbulence/runtime-config.yml" --path='/releases/name=turbulence/url')
+bosh-cli -n -e "${BOSH_ENVIRONMENT}" upload-release "$turbulence_release_url"
+
+runtime_config=$(bosh-cli int "git-kubo-ci/utils/turbulence/runtime-config.yml" --vars-file ${KUBO_ENVIRONMENT_DIR}/director.yml --vars-file ${KUBO_ENVIRONMENT_DIR}/creds.yml)
+
+echo "$runtime_config"  |  bosh-cli -n -e "${BOSH_ENVIRONMENT}" update-runtime-config -
