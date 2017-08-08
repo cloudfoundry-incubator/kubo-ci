a2a912989a214f78b3b590be32658198b689acff
Konstantin Semenov <ksemenov@pivotal.io>
2017-08-08 12:00:54 +0100

    Store a fixed turbulence manifest in this repo
    - Watch for https://github.com/cloudfoundry/bosh-deployment/pull/121
    
    [#149301477]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/install-bosh.sh
+++ scripts/install-bosh.sh
@@ -13,7 +13,7 @@ touch "${KUBO_ENVIRONMENT_DIR}/director-secrets.yml"
 
 iaas=$(bosh-cli int kubo-lock/metadata --path=/iaas)
 
-BOSH_EXTRA_OPS="--ops-file \"${KUBO_DEPLOYMENT_DIR}/bosh-deployment/turbulence.yml\""
+BOSH_EXTRA_OPS="--ops-file \"git-kubo-ci/manifests/turbulence/turbulence.yml\""
 BOSH_EXTRA_OPS="${BOSH_EXTRA_OPS} --ops-file \"${KUBO_DEPLOYMENT_DIR}/bosh-deployment/jumpbox-user.yml\""
 
 if [[ -f "${PWD}/git-kubo-ci/manifests/ops-files/${iaas}-cpi.yml" ]]; then
