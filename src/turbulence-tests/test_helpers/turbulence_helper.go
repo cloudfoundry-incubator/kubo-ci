c199b4804a4bf30ce5b3eb3d6f40ae8efa09abf8
Konstantin Semenov <ksemenov@pivotal.io>
2017-08-11 15:45:01 +0100

    Extracted the kill VM helper
    [#149401731]
    
    Signed-off-by: Bengt Hammarlund <bgadelhahammarlund@pivotal.io>

--- src/turbulence-tests/test_helpers/turbulence_helper.go
+++ src/turbulence-tests/test_helpers/turbulence_helper.go
@@ -3,6 +3,9 @@ package test_helpers
 import (
 	"github.com/cloudfoundry/bosh-utils/logger"
 	"github.com/cppforlife/turbulence/client"
+	"github.com/cppforlife/turbulence/incident/selector"
+	"github.com/cppforlife/turbulence/incident"
+	"github.com/cppforlife/turbulence/tasks"
 )
 
 func TurbulenceClient() client.Turbulence {
@@ -12,3 +15,18 @@ func TurbulenceClient() client.Turbulence {
 }
 
 
+func KillVms(vmType, limitString string) client.Incident {
+	turbulenceClient := TurbulenceClient()
+	limit, _ := selector.NewLimitFromString(limitString)
+	req := incident.Request{
+		Tasks: tasks.OptionsSlice{tasks.KillOptions{}},
+		Selector: selector.Request{
+			Deployment: &selector.NameRequest{Name: "ci-service"},
+			Group:      &selector.NameRequest{Name: vmType},
+			ID:         &selector.IDRequest{Limit: limit},
+		},
+	}
+	inc := turbulenceClient.CreateIncident(req)
+	inc.Wait()
+	return inc
+}
--- src/turbulence-tests/worker_failure/worker_failure_test.go
+++ src/turbulence-tests/worker_failure/worker_failure_test.go
@@ -3,10 +3,6 @@ package workload_test
 import (
 	"turbulence-tests/test_helpers"
 
-	"github.com/cppforlife/turbulence/incident"
-
-	"github.com/cppforlife/turbulence/incident/selector"
-	"github.com/cppforlife/turbulence/tasks"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -14,24 +10,11 @@ import (
 var _ = Describe("Worker failure test", func() {
 
 	It("brings back the failed worker VM", func() {
-		client := test_helpers.TurbulenceClient()
+		incident := test_helpers.KillVms("worker", "1")
 
-		limit, _ := selector.NewLimitFromString("1")
-
-		req := incident.Request{
-			Tasks: tasks.OptionsSlice{tasks.KillOptions{}},
-			Selector: selector.Request{
-				Deployment: &selector.NameRequest{Name: "ci-service"},
-				Group:      &selector.NameRequest{Name: "worker"},
-				ID:         &selector.IDRequest{Limit: limit},
-			},
-		}
-
-		inc := client.CreateIncident(req)
-		inc.Wait()
-
-		Expect(inc.HasTaskErrors()).To(BeFalse())
+		Expect(incident.HasTaskErrors()).To(BeFalse())
 
 		Eventually(test_helpers.RunningVmList("worker"), 600, 20).Should(HaveLen(3))
 	})
+
 })
