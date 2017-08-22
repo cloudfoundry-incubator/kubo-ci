6cd8eb9fff4f6e255d6536bdf82b488597a03892
Konstantin Semenov <ksemenov@pivotal.io>
2017-08-22 17:05:00 +0100

    Detect the env name and path correctly in manual claim
    [#150407576]

--- scripts/claim-lock-manually.sh
+++ scripts/claim-lock-manually.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -exu -o pipefail
- 
-cp -a kubo-lock-repo/. modified-repo
-cd modified-repo
-git config user.email "ci-bot@localhost"
-git config user.name "CI Bot"
 
 pool_name=$(basename $(cd "$(dirname "$ENV_FILE")/.."; pwd))
 env_name=$(basename "$ENV_FILE")
 
+cp -a kubo-lock-repo/. modified-repo
+
+cd modified-repo
+git config user.email "ci-bot@localhost"
+git config user.name "CI Bot"
 
 if git mv "${pool_name}/unclaimed/${env_name}" "${pool_name}/claimed/${env_name}"; then
   git commit -m "Claiming: ${pool_name}/${env_name}"
--- scripts/unclaim-lock-manually.sh
+++ scripts/unclaim-lock-manually.sh
@@ -4,7 +4,9 @@ set -exu -o pipefail
 
 pool_name=$(basename $(cd "$(dirname "$ENV_FILE")/.."; pwd))
 env_name=$(basename "$ENV_FILE")
+
 cp -a kubo-lock-repo/. modified-repo
+
 cd modified-repo
 git config user.email "ci-bot@localhost"
 git config user.name "CI Bot"
