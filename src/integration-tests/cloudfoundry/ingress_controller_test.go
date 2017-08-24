55bb24203c11b7d9ed590bed701a7e244a77c6e3
Oleksandr Slynko <oslynko@pivotal.io>
2017-08-24 10:28:34 +0100

    Use httpClient with timeout in ingress tests
    [#150259587]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- src/integration-tests/cloudfoundry/ingress_controller_test.go
+++ src/integration-tests/cloudfoundry/ingress_controller_test.go
@@ -112,18 +112,18 @@ var _ = Describe("Testing Ingress Controller", func() {
 	It("Allows routing via Ingress Controller", func() {
 		serviceName := test_helpers.GenerateRandomName()
 		appUrl := fmt.Sprintf("http://%s.%s", serviceName, appsDomain)
+		httpClient := http.Client{
+			Timeout: time.Duration(5 * time.Second),
+		}
 
 		By("exposing it via HTTP - " + serviceName)
-		result, err := http.Get(appUrl)
+		result, err := httpClient.Get(appUrl)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result.StatusCode).To(Equal(404))
 
 		httpLabel := fmt.Sprintf("http-route-sync=%s", serviceName)
 		Eventually(runner.RunKubectlCommand("label", "services", "nginx-ingress-controller", httpLabel), "10s").Should(gexec.Exit(0))
 
-		httpClient := http.Client{
-			Timeout: time.Duration(5 * time.Second),
-		}
 		Eventually(func() int {
 			result, err := httpClient.Get(appUrl + "/simple-http-server")
 			if err != nil {
@@ -133,20 +133,20 @@ var _ = Describe("Testing Ingress Controller", func() {
 			return result.StatusCode
 		}, "120s", "5s").Should(Equal(200))
 
-		result, err = http.Get(appUrl)
+		result, err = httpClient.Get(appUrl)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result.StatusCode).To(Equal(404))
 
 		By("exposing it via TCP")
 		appUrl = fmt.Sprintf("http://%s:%s", tcpRouterDNSName, tcpPort)
 
-		result, err = http.Get(appUrl)
+		result, err = httpClient.Get(appUrl)
 		Expect(err).To(HaveOccurred())
 
 		tcpLabel := fmt.Sprintf("tcp-route-sync=%s", tcpPort)
 		Eventually(runner.RunKubectlCommand("label", "services", "nginx-ingress-controller", tcpLabel), "10s").Should(gexec.Exit(0))
 		Eventually(func() error {
-			_, err := http.Get(appUrl)
+			_, err := httpClient.Get(appUrl)
 			return err
 		}, "120s", "5s").ShouldNot(HaveOccurred())
 		hasPassed = true
