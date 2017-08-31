dd816f8976ce947f9e39be9b1af4d426de71a513
Morena De Liddo <mdeliddo@pivotal.io>
2017-08-31 17:06:39 +0100

    Modify tarball source paths
    [#150406128]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- scripts/build-kubo-deployment-tarball.sh
+++ scripts/build-kubo-deployment-tarball.sh
@@ -7,4 +7,4 @@ cp -r git-kubo-deployment tmp/kubo-deployment
 cp gcs-kubo-release-tarball-untested/kubo-*.tgz tmp/kubo-release.tgz
 
 cd tmp
-tar -zcvf "kubo-deployment-tarball/kubo-deployment-$(cat kubo-version/version).tgz" *
+tar -zcvf "../kubo-deployment-tarball/kubo-deployment-$(cat ../kubo-version/version).tgz" *
