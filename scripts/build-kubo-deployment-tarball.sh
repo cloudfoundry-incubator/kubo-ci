28cbac25c276d6fa500a511194817c965583d9cb
Konstantin Semenov <ksemenov@pivotal.io>
2017-08-31 16:50:49 +0100

    Fix release tarball path
    [#150406128]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- scripts/build-kubo-deployment-tarball.sh
+++ scripts/build-kubo-deployment-tarball.sh
@@ -4,7 +4,7 @@ set -exu -o pipefail
 
 mkdir tmp
 cp -r git-kubo-deployment tmp/kubo-deployment
-cp gcs-kubo-release-tarball-untested/kubo-release-*.tgz tmp/kubo-release.tgz
+cp gcs-kubo-release-tarball-untested/kubo-*.tgz tmp/kubo-release.tgz
 
 cd tmp
 tar -zcvf "kubo-deployment-tarball/kubo-deployment-$(cat kubo-version/version).tgz" *
