bb5b63c9daaa37f2d3eb43c99eb704d87a09dcf7
Konstantin Semenov <ksemenov@pivotal.io>
2017-08-31 15:23:34 +0100

    Adds task to package kubo-deployment with kubo-release tarball
    [#150406128]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

---
+++ scripts/build-kubo-deployment-tarball.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -exu -o pipefail
+
+mkdir tmp
+cp -r git-kubo-deployment tmp/kubo-deployment
+cp gcs-kubo-release-tarball-untested/kubo-release-*.tgz tmp/kubo-release.tgz
+
+cd tmp
+tar -zcvf "kubo-deployment-tarball/kubo-deployment-$(cat kubo-version/version).tgz" *
