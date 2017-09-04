ec918546aacc44b716790261f76b038c183ddb3d
Morena De Liddo <mdeliddo@pivotal.io>
2017-09-04 13:43:39 +0100

    Fix lock path
    [#150407782]
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- scripts/recycle-cleanup-gcp.sh
+++ scripts/recycle-cleanup-gcp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
-ENV_FILE=./metadata ./git-kubo-ci/scripts/cleanup-environment.sh
+ENV_FILE=./kubo-lock/metadata ./git-kubo-ci/scripts/cleanup-environment.sh
