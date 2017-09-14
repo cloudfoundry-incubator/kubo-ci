92adc0fef4ed561352478976b0b64abbb70de9e0
Balaji Boggaram Ramanarayan <brnbalaji@gmail.com>
2017-09-14 17:21:21 -0400

    create file if it doesn't exist
    [#150790628]
    
    Signed-off-by: Tony Wong <twong@pivotal.io>

--- docker-images/persistent-example/app.rb
+++ docker-images/persistent-example/app.rb
@@ -6,8 +6,10 @@ set :bind, '0.0.0.0'
 get '/' do
     log_file = ENV['LOG_FILE']
 
+    File.write(log_file, "")
+
     last_line = IO.readlines(log_file)[-1]
-    last_number = last_line.strip.to_i
+    last_number = last_line.nil? ? 0 : last_line.strip.to_i
 
     num_lines = File.open(log_file, "r").readlines.size
 
