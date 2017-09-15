f37190b614757f02c368c82c2e2a54af5c4d9b45
Brendan Nolan <bnolan@pivotal.io>
2017-09-15 15:36:42 +0100

    Increase deployment timeout
    [#150398437]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- src/tests/turbulence-tests/worker_failure/worker_failure_test.go
+++ src/tests/turbulence-tests/worker_failure/worker_failure_test.go
@@ -45,7 +45,7 @@ var _ = Describe("Worker failure scenarios", func() {
 		Eventually(func() bool { return AllBoshWorkersHaveJoinedK8s(deployment, kubectl) }, 600, 20).Should(BeTrue())
 
 		By("Deploying nginx on 3 nodes")
-		Eventually(kubectl.RunKubectlCommand("create", "-f", nginxSpec)).Should(gexec.Exit(0))
+		Eventually(kubectl.RunKubectlCommand("create", "-f", nginxSpec), "30s", "5s").Should(gexec.Exit(0))
 		Eventually(kubectl.RunKubectlCommand("rollout", "status", "deployment/nginx", "-w"), "120s").Should(gexec.Exit(0))
 
 		By("Verifying nginx got deployed on new node")
