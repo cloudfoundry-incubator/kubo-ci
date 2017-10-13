543cd699611e6c23cc455fe17f0ea28cbbf682b1
Tony Wong <twong@pivotal.io>
2017-10-12 21:30:56 -0400

    Fix upgrade-kubo-test-ha to copy creds properly
    [#151546967]

--- scripts/upgrade-kubo-test-ha.sh
+++ scripts/upgrade-kubo-test-ha.sh
@@ -12,8 +12,15 @@ update_kubo() {
   ${DIR}/deploy-k8s-instance.sh
 }
 
-BOSH_ENV="$KUBO_ENVIRONMENT_DIR" source "$KUBO_DEPLOYMENT_DIR/bin/set_bosh_environment"
-bosh-cli upload-release https://bosh.io/d/github.com/cf-platform-eng/docker-boshrelease?v=28.0.1 --sha1 448eaa2f478dc8794933781b478fae02aa44ed6b
-bosh-cli upload-release https://github.com/pivotal-cf-experimental/kubo-etcd/releases/download/v2/kubo-etcd.2.tgz --sha1 ae95e661cd9df3bdc59ee38bf94dd98e2f280d4f
+upload_new_releases() {
+  if [ -z ${LOCAL_DEV+x} ] || [ "$LOCAL_DEV" != "1" ]; then
+    copy_state_and_creds
+  fi
+  BOSH_ENV="$KUBO_ENVIRONMENT_DIR" source "$KUBO_DEPLOYMENT_DIR/bin/set_bosh_environment"
 
+  bosh-cli upload-release https://bosh.io/d/github.com/cf-platform-eng/docker-boshrelease?v=28.0.1 --sha1 448eaa2f478dc8794933781b478fae02aa44ed6b
+  bosh-cli upload-release https://github.com/pivotal-cf-experimental/kubo-etcd/releases/download/v2/kubo-etcd.2.tgz --sha1 ae95e661cd9df3bdc59ee38bf94dd98e2f280d4f
+}
+
+upload_new_releases
 run_upgrade_test update_kubo
