6fdfc274dbdcc353d3fb375dcf2358857e00408b
Travis Hall <thall@vmware.com>
2017-10-13 15:45:51 -0400

    Configure service availability for upgrade tests
    - track success rate for HA test
    
    [#151546967]
    
    Signed-off-by: Tony Wong <twong@pivotal.io>

--- scripts/lib/upgrade-tests.sh
+++ scripts/lib/upgrade-tests.sh
@@ -1,9 +1,18 @@
 . "$DIR/lib/lb-info.sh"
 
 query_loop() {
-  local timeout_seconds=20
+  local timeout_seconds=10
   local pid_to_wait="$1"
   local url="$2"
+  local min_success_rate="${3:-1}"
+
+  # output values
+  local query_loop_count=0
+  local query_success_count=0
+
+  # error colors
+  local red='\033[0;31m'
+  local no_color='\033[0m'
 
   echo "Querying $url while waiting for process with pid $pid_to_wait to finish..."
 
@@ -11,17 +20,31 @@ query_loop() {
   while kill -0 "$pid_to_wait" >/dev/null 2>&1; do
     sleep 1
 
+    (( query_loop_count+=1 ))
+
+    local timestamp=`date`
+
     curl -L --max-time ${timeout_seconds} -IfsS ${url} &> query_loop_last_output.txt
 
     if [ "$?" -ne 0 ]; then
-      echo "Error: request to $url failed"
+      printf "[$timestamp][$query_success_count/$query_loop_count] ${red}Error: request to $url failed${no_color}\n"
       cat query_loop_last_output.txt
-      return 1
     else
-      local timestamp=`date`
-      echo "[$timestamp] Service $url successfully responded"
+      (( query_success_count+=1 ))
+      echo "[$timestamp][$query_success_count/$query_loop_count] Service $url successfully responded"
     fi
   done
+
+  echo "$query_success_count out of $query_loop_count queries succeeded"
+
+  local success_rate=$(echo "$query_success_count $query_loop_count" | awk '{print ($1 / $2)}')
+
+  if (( $(echo "$success_rate $min_success_rate" | awk '{print ($1 >= $2)}') )); then
+    echo "Success rate ($success_rate) passes minimum ($min_success_rate)"
+  else
+    echo "Success rate ($success_rate) fails to pass minimum ($min_success_rate)"
+    return 1
+  fi
 }
 
 wait_for_success() {
@@ -38,10 +61,10 @@ wait_for_success() {
   echo "$work_description succeeded"
 }
 
-# Relies on a function named `update()` to be present in the script. This runs the update process.
 run_upgrade_test() {
   local service_name="nginx"
-  local update_function=$1
+  local update_function="$1"
+  local min_success_rate="${2:-1}"
 
   lb_address_blocking "$service_name" "$KUBO_ENVIRONMENT_DIR" "$KUBO_DEPLOYMENT_DIR"
   if [ -z "$lb_address" ]; then
@@ -58,14 +81,16 @@ run_upgrade_test() {
 
   # exercise the load balancer URL while BOSH is updating
   local query_url="$lb_url"
-  query_loop "$update_pid" "$query_url" &
-  local query_pid="$!"
+  query_loop "$update_pid" "$query_url" "$min_success_rate" &
+  local query_loop_pid="$!"
 
   wait_for_success "$update_pid" "Update"
-  wait_for_success "$query_pid" "HA query loop"
-  if [ "$?" -ne 0 ]; then
-    echo "Output of last query below:"
-    cat query_loop_last_output.txt
+  update_code="$?"
+
+  wait_for_success "$query_loop_pid" "HA query loop"
+  query_code="$?"
+
+  if [ "$update_code" != "0" ] || [ "$query_code" != "0" ]; then
     return 1
   fi
 }
--- scripts/upgrade-kubo-test-ha.sh
+++ scripts/upgrade-kubo-test-ha.sh
@@ -7,6 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$DIR/lib/environment.sh"
 . "$DIR/lib/upgrade-tests.sh"
 
+HA_MIN_SERVICE_AVAILABILITY="${HA_MIN_SERVICE_AVAILABILITY:-1}"
+
 update_kubo() {
   echo "Updating Kubo..."
   ${DIR}/deploy-k8s-instance.sh
@@ -24,4 +26,4 @@ upload_new_releases() {
 }
 
 upload_new_releases
-run_upgrade_test update_kubo
+run_upgrade_test update_kubo "$HA_MIN_SERVICE_AVAILABILITY"
