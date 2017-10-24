5f3ac2d85746d83b9dc8da7500da026fe4b8e7f3
Akshay Mankar <amankar@pivotal.io>
2017-10-24 13:54:44 +0100

    Find director ip in director.yml
    Also make update_kubo function fail immediately after any failure
    
    [#150790628]
    
    Signed-off-by: Iain Sproat <isproat@pivotal.io>

--- scripts/upgrade-kubo-test-ha.sh
+++ scripts/upgrade-kubo-test-ha.sh
@@ -10,9 +10,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HA_MIN_SERVICE_AVAILABILITY="${HA_MIN_SERVICE_AVAILABILITY:-1}"
 
 update_kubo() {
+  set -eo pipefail
+
   # Workaround due to https://www.pivotaltracker.com/story/show/152155545
   echo "Deleting tls-kubernetes from credhub..."
-  credhub api "https://$(bosh-cli int environment/creds.yml --path=/internal_ip):8844"
+  credhub api "https://$(bosh-cli int environment/director.yml --path=/internal_ip):8844"
   credhub login -u credhub-cli -p "$(bosh-cli int environment/creds.yml --path=/credhub_cli_password)"
   credhub delete "$(bosh-cli int environment/director.yml --path=/director_name)/ci-service/tls-kubernetes"
 
