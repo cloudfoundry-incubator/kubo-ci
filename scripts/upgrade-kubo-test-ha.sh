84e87c48e18958fda077e17c31afe3110cc8288b
Akshay Mankar <amankar@pivotal.io>
2017-10-24 10:47:05 +0100

    Delete tls-kubernetes before upgrading k8s
    [#150790628]
    
    Signed-off-by: Iain Sproat <isproat@pivotal.io>

--- scripts/upgrade-kubo-test-ha.sh
+++ scripts/upgrade-kubo-test-ha.sh
@@ -10,6 +10,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HA_MIN_SERVICE_AVAILABILITY="${HA_MIN_SERVICE_AVAILABILITY:-1}"
 
 update_kubo() {
+  # Workaround due to https://www.pivotaltracker.com/story/show/152155545
+  echo "Deleting tls-kubernetes from credhub..."
+  credhub api "https://$(bosh-cli int environment/creds.yml --path=/internal_ip):8844"
+  credhub login -u credhub-cli -p "$(bosh-cli int environment/creds.yml --path=/credhub_cli_password)"
+  credhub delete "$(bosh-cli int environment/director.yml --path=/director_name)/ci-service/tls-kubernetes"
+
   echo "Updating Kubo..."
   ${DIR}/deploy-k8s-instance.sh
 }
