ec968ad7328efd46f4cddd0f582451f24774b2ac
Morena De Liddo <mdeliddo@pivotal.io>
2017-11-23 12:02:36 +0000

    Check all deployments in addons-spec have rolled out
    [#152475519]
    
    Signed-off-by: Jamie Monserrate <jmonserrate@pivotal.io>

--- src/tests/integration-tests/generic/apply_specs_test.go
+++ src/tests/integration-tests/generic/apply_specs_test.go
@@ -1,26 +1,29 @@
 package generic_test
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"tests/test_helpers"
 	"github.com/onsi/gomega/gexec"
 	"github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/ginkgo/extensions/table"
 )
 
-var _ = Describe("apply-specs errand has run", func() {
+var _ = Describe("check apply-specs errand has run correctly", func() {
 	var runner *test_helpers.KubectlRunner
 
 	BeforeEach(func() {
 		runner = test_helpers.NewKubectlRunner()
 	})
 
-	It("Should be able to run kubectl commands within pod", func() {
-
-		session := runner.RunKubectlCommandInNamespace("default", "rollout", "status", "deployment/frontend", "-w")
+	DescribeTable("all deployments have rolled out", func(deploymentName string) {
+		session := runner.RunKubectlCommandInNamespace("default", "rollout", "status", fmt.Sprintf("deployment/%s", deploymentName), "-w")
 		Eventually(session, "120s").Should(gexec.Exit(0))
-		Eventually(session, "120s").Should(gbytes.Say("deployment \"frontend\" successfully rolled out"))
-	})
-	
-
+		Eventually(session, "120s").Should(gbytes.Say(fmt.Sprintf("deployment \"%s\" successfully rolled out", deploymentName)))
+	},
+		Entry("frontend", "frontend"),
+		Entry("redis-slave", "redis-slave"),
+		Entry("redis-master", "redis-master"),
+	)
 })
