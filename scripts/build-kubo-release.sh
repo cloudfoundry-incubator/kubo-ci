93125e11d0d9041459f133f9e26b133dbac1b2ad
Ben Chapman <bchapman@pivotal.io>
2017-11-29 17:01:56 +0000

    Add private.yml for cutting finals
    [#152882120]
    
    Signed-off-by: Iain Sproat <isproat@pivotal.io>

--- scripts/build-kubo-release.sh
+++ scripts/build-kubo-release.sh
@@ -11,5 +11,11 @@ cd git-kubo-release
 final_arg=""
 if [ $FINAL == 'true' ]; then
   final_arg="--final"
+  cat <<EOF > "config/private.yml"
+blobstore:
+  options:
+    access_key_id: ${ACCESS_KEY_ID}
+    secret_access_key: ${SECRET_ACCESS_KEY}
+EOF
 fi
 bosh-cli create-release --name "kubo" --sha2 --tarball="../kubo-release/kubo-release-${version}.tgz" --version=${version} ${final_arg}
