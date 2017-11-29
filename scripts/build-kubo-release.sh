1aa2dbb76e29fddbd096f31417d3594b2858f4dc
Ben Chapman <bchapman@pivotal.io>
2017-11-29 15:18:49 +0000

    Support final args
    [#152882120]
    
    Signed-off-by: Iain Sproat <isproat@pivotal.io>

--- scripts/build-kubo-release.sh
+++ scripts/build-kubo-release.sh
@@ -7,4 +7,9 @@ export BOSH_LOG_PATH="$PWD/bosh.log"
 version=$(cat kubo-version/version)
 
 cd git-kubo-release
-bosh-cli create-release --name "kubo" --tarball="../kubo-release/kubo-release-${version}.tgz" --version=${version}
+
+final_arg=""
+if [ $FINAL == 'true' ]; then
+  final_arg="--final"
+fi
+bosh-cli create-release --name "kubo" --sha2 --tarball="../kubo-release/kubo-release-${version}.tgz" --version=${version} ${final_args}
