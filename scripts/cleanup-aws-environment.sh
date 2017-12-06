9441d8e86003d15635d12332e5b17584fac57c93
Sagar Muchhal <muchhals@vmware.com>
2017-12-06 20:01:45 +0530

    Modified clean-up script to delete EBS volumes attached to worker nodes
    [#153368515]
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- scripts/cleanup-aws-environment.sh
+++ scripts/cleanup-aws-environment.sh
@@ -2,6 +2,7 @@
 
 set -eu -o pipefail
 
+director_name="$(bosh-cli int "${ENV_FILE}" --path=/director_name)"
 director_ip="$(bosh-cli int "${ENV_FILE}" --path=/internal_ip)"
 subnet_id="$(bosh-cli int "${ENV_FILE}" --path=/subnet_id)"
 access_key_id="$(bosh-cli int "${ENV_FILE}" --path=/access_key_id)"
@@ -22,6 +23,14 @@ region=${region}
 output=text
 EOF
 
+delete_volumes() {
+  local volume_ids=$1
+  for volume in $volume_ids; do
+    aws ec2 delete-volume --volume-id "$volume"
+    aws ec2 wait volume-deleted --volume-id "$volume"
+  done
+}
+
 director_instance_id=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output text --filters "Name=network-interface.addresses.private-ip-address,Values=${director_ip}" "Name=subnet-id,Values=${subnet_id}")
 if [ -z "$director_instance_id" ]; then
   echo "No instance found for BOSH Director IP address"
@@ -36,4 +45,16 @@ if [ -z "$instance_ids" ]; then
 else
   aws ec2 terminate-instances --instance-ids ${instance_ids}
   aws ec2 wait instance-terminated --instance-ids ${instance_ids}
+fi
+
+director_name_tag_volume_ids=$(aws ec2 describe-volumes --output text --no-paginate --query 'Volumes[*].VolumeId' --filters "Name=tag:director,Values=${director_name}" "Name=status,Values=available")
+#director_volume_ids=$(aws ec2 describe-volumes --output text --no-paginate --query 'Volumes[*].VolumeId' --filters "Name=attachment.instance-id,Values=${director_instance_id}" "Name=status,Values=available")
+
+#if [ ! -z $director_volume_ids ]
+#  echo "Deleting volumes associated to director '${director_name}'"
+#  delete_volumes director_volume_ids
+#fi
+if [ ! -z "$director_name_tag_volume_ids" ]; then
+  echo "Deleting volumes tagged with director name '${director_name}'"
+  delete_volumes "$director_name_tag_volume_ids"
 fi
