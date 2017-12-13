77de7332f77927c9136588822087982bb2f8695a
James Myers <jmyers@pivotal.io>
2017-12-13 14:43:05 +0000

    Append environment information to kubo-lock metadata
    [#153471651]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- scripts/get-bosh-secrets.sh
+++ scripts/get-bosh-secrets.sh
@@ -14,5 +14,12 @@ set +x
 
 credhub get -n "/concourse/main/${director_name}/creds" --output-json | jq -r .value > bosh-creds/creds.yml
 
+credhub get -n "/concourse/main/cfcr-gcp-cf" --output-json | jq -r .value  >> ${metadata_path}
+credhub get -n "/concourse/main/cfcr-gcp" --output-json | jq -r .value  >> ${metadata_path}
+credhub get -n "/concourse/main/cfcr-cfcr" --output-json | jq -r .value  >> ${metadata_path}
+credhub get -n "/concourse/main/cfcr-gcp-cf-${director_name}-conformance" --output-json | jq -r .value >> ${metadata_path}
+echo "routing_mode: cf" >> ${metadata_path}
+
 set -x
+
 credhub logout
