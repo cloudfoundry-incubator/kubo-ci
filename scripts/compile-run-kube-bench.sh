3d8d83ae87cc1bbba97e39d4e3d3c96fa3e538da
Tony Wong <twong@pivotal.io>
2017-12-14 15:19:42 -0500

    run cis benchmarks on master and worker
    [#153560509]

--- scripts/compile-run-kube-bench.sh
+++ scripts/compile-run-kube-bench.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
 set -eo pipefail
+
+print_usage() {
+  echo "Usage: compile-run-kube-bench.sh master|node"
+}
+
+if [ -z "$1" ]; then
+  print_usage
+  exit 1
+elif [ "$1" != "master" ] && [ "$1" != "node" ]; then
+  print_usage
+  exit 1
+else
+  node_type=$1
+fi
+
 set -x
 
-sudo add-apt-repository ppa:gophers/archive
+sudo add-apt-repository -y ppa:gophers/archive
 sudo apt update
-sudo apt-get install -y golang-1.9-go
+sudo apt-get install -y golang-1.9-go git
 
-go get github.com/aquasecurity/kube-bench
-cp $GOROOT/bin/kube-bench .
-./kube-bench help
+GOROOT="/usr/lib/go-1.9"
+export GOROOT
+
+"$GOROOT/bin/go" get github.com/aquasecurity/kube-bench
+
+cp -R "$HOME/go/src/github.com/aquasecurity/kube-bench/cfg" .
+
+config_path="$PWD/kube-bench-config.yml"
+
+PATH="$PATH:/var/vcap/packages/kubernetes/bin" \
+  ~/go/bin/kube-bench \
+  --config="$config_path" \
+  "$node_type"
+
--- scripts/run-cis-benchmarks.sh
+++ scripts/run-cis-benchmarks.sh
@@ -7,8 +7,8 @@ set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# shellcheck source=lib/environment.sh
 . "$DIR/lib/environment.sh"
-. "$DIR/lib/run_test_suite.sh"
 
 copy_state_and_creds() {
   cp "$PWD/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
@@ -20,30 +20,37 @@ if [ -z ${LOCAL_DEV+x} ] || [ "$LOCAL_DEV" != "1" ]; then
   copy_state_and_creds
 fi
 
-# if [ -z ${CONFORMANCE_RESULTS_DIR+x} ]; then
-#   echo "Error: CONFORMANCE_RESULTS_DIR is not set, exiting..."
-#   exit 1
-# fi
-
-# GOPATH="$KUBO_CI_DIR"
-# export GOPATH
-# export PATH_TO_KUBECONFIG="$HOME/.kube/config"
-# export CONFORMANCE_RESULTS_DIR="$PWD/$CONFORMANCE_RESULTS_DIR"
-# export RELEASE_TARBALL="$PWD/$KUBO_DEPLOYMENT_DIR/kubo-release.tgz"
-
-# kubo::tests::run_test_suite "$GOPATH/src/tests/conformance"
-
-
 BOSH_ENV="${KUBO_ENVIRONMENT_DIR}"
 
 DEBUG=1
 BOSH_CLIENT=bosh_admin
-BOSH_CLIENT_SECRET="$(get_bosh_secret)"
+BOSH_CLIENT_SECRET=$(bosh-cli int "$BOSH_ENV"/creds.yml --path=/bosh_admin_client_secret)
 BOSH_CA_CERT=$(bosh-cli int "$BOSH_ENV"/creds.yml --path=/default_ca/ca)
 BOSH_ENVIRONMENT=$(bosh-cli int "$BOSH_ENV"/director.yml --path=/internal_ip)
 
 export BOSH_NAME DEBUG BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT BOSH_ENVIRONMENT
 
-bosh-cli -d ci-service scp "$DIR/compile-run-kube-bench.sh" master:/tmp/compile-run-kube-bench.sh
-bosh-cli -d ci-service ssh master -c "/tmp/compile-run-kube-bench.sh"
+dst="/tmp/$(date +%s)"
+
+# master
+bosh-cli -d ci-service ssh master --command="mkdir -p $dst"
+
+bosh-cli -d ci-service scp \
+  "$DIR/compile-run-kube-bench.sh" "master:$dst/compile-run-kube-bench.sh"
+bosh-cli -d ci-service scp \
+  "$DIR/kube-bench/config.yml" "master:$dst/kube-bench-config.yml"
+
+bosh-cli -d ci-service ssh master \
+  --command="cp $dst/* .; ./compile-run-kube-bench.sh master"
+
+# worker
+bosh-cli -d ci-service ssh worker/0 --command="mkdir -p $dst"
+
+bosh-cli -d ci-service scp \
+  "$DIR/compile-run-kube-bench.sh" "worker/0:$dst/compile-run-kube-bench.sh"
+bosh-cli -d ci-service scp \
+  "$DIR/kube-bench/config.yml" "worker/0:$dst/kube-bench-config.yml"
+
+bosh-cli -d ci-service ssh worker/0 \
+  --command="cp $dst/* .; ./compile-run-kube-bench.sh node"
 
