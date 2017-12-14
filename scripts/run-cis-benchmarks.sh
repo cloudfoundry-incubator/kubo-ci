8123475232605c1db30c97ad94fb6898f54603a4
Tony Wong <twong@pivotal.io>
2017-12-14 15:50:58 -0500

    split worker and master benchmarks into two tasks
    [#153560509]

--- scripts/run-cis-benchmarks.sh
+++ scripts/run-cis-benchmarks.sh
@@ -2,6 +2,15 @@
 
 [ -z "$DEBUG" ] || set -x
 
+print_usage() {
+  echo "Usage: VM_TYPE=[master | worker] NODE_TYPE=[master | node] run-cis-benchmarks.sh"
+}
+
+if [ -z "$VM_TYPE" ] || [ -z "$NODE_TYPE" ]; then
+  print_usage
+  exit 1
+fi
+
 set -eu
 set -o pipefail
 
@@ -22,35 +31,22 @@ fi
 
 BOSH_ENV="${KUBO_ENVIRONMENT_DIR}"
 
-DEBUG=1
 BOSH_CLIENT=bosh_admin
 BOSH_CLIENT_SECRET=$(bosh-cli int "$BOSH_ENV"/creds.yml --path=/bosh_admin_client_secret)
 BOSH_CA_CERT=$(bosh-cli int "$BOSH_ENV"/creds.yml --path=/default_ca/ca)
 BOSH_ENVIRONMENT=$(bosh-cli int "$BOSH_ENV"/director.yml --path=/internal_ip)
 
-export BOSH_NAME DEBUG BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT BOSH_ENVIRONMENT
+export BOSH_NAME BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT BOSH_ENVIRONMENT
 
 dst="/tmp/$(date +%s)"
 
-# master
-bosh-cli -d ci-service ssh master --command="mkdir -p $dst"
+bosh-cli -d ci-service ssh "$VM_TYPE/0" --command="mkdir -p $dst"
 
 bosh-cli -d ci-service scp \
-  "$DIR/compile-run-kube-bench.sh" "master:$dst/compile-run-kube-bench.sh"
+  "$DIR/compile-run-kube-bench.sh" "$VM_TYPE/0:$dst/compile-run-kube-bench.sh"
 bosh-cli -d ci-service scp \
-  "$DIR/kube-bench/config.yml" "master:$dst/kube-bench-config.yml"
+  "$DIR/kube-bench/config.yml" "$VM_TYPE/0:$dst/kube-bench-config.yml"
 
-bosh-cli -d ci-service ssh master \
-  --command="cp $dst/* .; ./compile-run-kube-bench.sh master"
-
-# worker
-bosh-cli -d ci-service ssh worker/0 --command="mkdir -p $dst"
-
-bosh-cli -d ci-service scp \
-  "$DIR/compile-run-kube-bench.sh" "worker/0:$dst/compile-run-kube-bench.sh"
-bosh-cli -d ci-service scp \
-  "$DIR/kube-bench/config.yml" "worker/0:$dst/kube-bench-config.yml"
-
-bosh-cli -d ci-service ssh worker/0 \
-  --command="cp $dst/* .; ./compile-run-kube-bench.sh node"
+bosh-cli -d ci-service ssh "$VM_TYPE/0" \
+  --command="cp $dst/* .; ./compile-run-kube-bench.sh $NODE_TYPE"
 
