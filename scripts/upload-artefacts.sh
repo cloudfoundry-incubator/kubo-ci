f2c564d41b0370395372f3d4519feb815109ea98
Christian Ang <cang@pivotal.io>
2017-12-15 15:38:42 +0000

    Copy bosh-creds and rename metadata
    [#153471651]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- scripts/upload-artefacts.sh
+++ scripts/upload-artefacts.sh
@@ -3,4 +3,7 @@
 set -exu -o pipefail
 
 tar -zxvf gcs-kubo-deployment-tarball-untested/*.tgz
+cp bosh-creds/creds.yml kubo-lock
+mv kubo-lock/metadata kubo-lock/director.yml
+
 ./kubo-deployment/bin/upload_artefacts "kubo-lock" "local"
