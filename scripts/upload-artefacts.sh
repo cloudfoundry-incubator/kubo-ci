a2a15656e95db65c687f8e903b163ed6df21f55d
Konstantin Semenov <ksemenov@pivotal.io>
2017-12-15 15:31:36 +0000

    Extract to kubo-deployment
    [#153471651]
    
    Signed-off-by: Christian Ang <cang@pivotal.io>

--- scripts/upload-artefacts.sh
+++ scripts/upload-artefacts.sh
@@ -2,6 +2,5 @@
 
 set -exu -o pipefail
 
-mkdir kubo-deployment
-tar -zxvf gcs-kubo-deployment-tarball-untested/*.tgz -C kubo-deployment
+tar -zxvf gcs-kubo-deployment-tarball-untested/*.tgz
 ./kubo-deployment/bin/upload_artefacts "kubo-lock" "local"
