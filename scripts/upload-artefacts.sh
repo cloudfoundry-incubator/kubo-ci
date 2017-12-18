77da513079731fb95d6670a36b50573d20adc793
Christian Ang <cang@pivotal.io>
2017-12-18 14:59:33 +0000

    Change mv to cp
    [#153471651]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- scripts/upload-artefacts.sh
+++ scripts/upload-artefacts.sh
@@ -4,6 +4,6 @@ set -exu -o pipefail
 
 tar -zxvf gcs-kubo-deployment-tarball-untested/*.tgz
 cp bosh-creds/creds.yml kubo-lock
-mv kubo-lock/metadata kubo-lock/director.yml
+cp kubo-lock/metadata kubo-lock/director.yml
 
 ./kubo-deployment/bin/upload_artefacts "kubo-lock" "local"
