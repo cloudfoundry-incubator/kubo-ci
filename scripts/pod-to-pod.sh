2dbf06d542aa980a01956042b05a050f641df5dd
Christian Ang <cang@pivotal.io>
2017-12-18 12:11:27 +0000

    Pass deployment name as env in pod-to-pod
    Default value is ci-service
    
    [#153471651]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- scripts/pod-to-pod.sh
+++ scripts/pod-to-pod.sh
@@ -8,6 +8,7 @@ set -exu -o pipefail
 export BOSH_LOG_LEVEL=debug
 export BOSH_LOG_PATH="$PWD/bosh.log"
 export DEBUG=1
+export DEPLOYMENT_NAME=${DEPLOYMENT_NAME:="ci-service"}
 
 cp "gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
 cp "kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
@@ -17,7 +18,7 @@ client_secret=$(bosh-cli int "${KUBO_ENVIRONMENT_DIR}/creds.yml" --path=/bosh_ad
 
 director_ip=$(bosh-cli int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path="/internal_ip")
 
-"git-kubo-deployment/bin/set_kubeconfig" "${KUBO_ENVIRONMENT_DIR}" ci-service
+"git-kubo-deployment/bin/set_kubeconfig" "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}"
 
 trap "kubectl delete -f 'git-kubo-ci/specs/guestbook.yml'" EXIT
 
@@ -29,7 +30,7 @@ kubectl rollout status deployment/redis-slave -w
 nodeport=$(kubectl describe svc/frontend | grep 'NodePort:' | awk '{print $3}' | sed -e 's/\/TCP//g')
 
 
-worker_ip=$(BOSH_CLIENT=bosh_admin BOSH_CLIENT_SECRET=${client_secret} BOSH_CA_CERT="${bosh_ca_cert}" bosh-cli -e "${director_ip}" vms | grep worker | head -n1 | awk '{print $4}')
+worker_ip=$(BOSH_CLIENT=bosh_admin BOSH_CLIENT_SECRET=${client_secret} BOSH_CA_CERT="${bosh_ca_cert}" bosh-cli -e "${director_ip}" vms -d "${DEPLOYMENT_NAME}"  | grep worker | head -n1 | awk '{print $4}')
 testvalue="$(date +%s)"
 
 if timeout 120 /bin/bash <<EOF
