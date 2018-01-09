fc66c27fcae3b910fec1174eafcce06ec31a7011
Tony Wong <twong@pivotal.io>
2018-01-09 17:15:04 -0500

    create temporary directory for changing stemcell
    [#154051307]

--- scripts/update-stemcell.sh
+++ scripts/update-stemcell.sh
@@ -5,7 +5,9 @@ set -euxo pipefail
 stemcell_version=$(cat ./gcp-stemcell/version)
 ./git-kubo-deployment/bin/update_stemcell "${stemcell_version}"
 
-pushd git-kubo-deployment
+cp -a git-kubo-deployment/. git-kubo-deployment-with-updated-stemcell
+
+pushd git-kubo-deployment-with-updated-stemcell
 
   git config user.email "ci-bot@localhost"
   git config user.name "CI Bot"
