5dac99c65f4ccdc590e648584b1a697ce17d2089
Tony Wong <twong@pivotal.io>
2018-01-09 16:57:07 -0500

    Use CI Bot as git user for updating stemcell
    [#154051307]

--- scripts/update-stemcell.sh
+++ scripts/update-stemcell.sh
@@ -4,6 +4,12 @@ set -euxo pipefail
 
 stemcell_version=$(cat ./gcp-stemcell/version)
 ./git-kubo-deployment/bin/update_stemcell "${stemcell_version}"
-git \
-  -C ./git-kubo-deployment \
-  commit -am "Update stemcell version to $stemcell_version"
+
+pushd git-kubo-deployment
+
+  git config user.email "ci-bot@localhost"
+  git config user.name "CI Bot"
+
+  git commit -am "Update stemcell version to $stemcell_version"
+
+popd
