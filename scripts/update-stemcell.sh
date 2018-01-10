832f0b4b06e3f70e186384cf1403b874344822c9
Morena De Liddo <mdeliddo@pivotal.io>
2018-01-10 16:32:38 +0000

    Update stemcell patch version in cfcr.yml of master
    [#154051307]
    
    Signed-off-by: Greg Patricio <gpatricio@pivotal.io>

--- scripts/update-stemcell.sh
+++ scripts/update-stemcell.sh
@@ -12,7 +12,7 @@ pushd git-kubo-deployment-with-updated-stemcell
   git config user.email "ci-bot@localhost"
   git config user.name "CI Bot"
 
-  git checkout test-update-stemcell
+  git checkout master
   git add .
   git diff-index --quiet HEAD || git commit -m "Update stemcell version to $stemcell_version"
 
