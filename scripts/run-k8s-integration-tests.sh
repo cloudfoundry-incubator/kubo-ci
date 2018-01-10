2695c617f75de24fdbb911feb0acfac5497fc003
Brenda Chan <brchan@pivotal.io>
2018-01-10 15:23:54 -0500

    Updates run-k8s-integration script to take optional skip flags
    [#153855412]

--- scripts/run-k8s-integration-tests.sh
+++ scripts/run-k8s-integration-tests.sh
@@ -10,10 +10,12 @@ BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)
 verify_args() {
   set +e # Cant be set since read returns a non-zero when it reaches EOF
   read -r -d '' usage <<-EOF
-	Usage: $(basename "$0") [-h] environment deployment-name
-	
+	Usage: $(basename "$0") [-h] environment deployment-name [--skip-addons-tests] [--skip-cloud-agnostic-tests]
+
 	Help Options:
-		-h  show this help text
+          -h                             show this help text
+          --skip-addons-tests            skip the add ons tests
+          --skip-cloud-agnostic-tests   skip the cloud agnostic tests
 	EOF
   set -e
 
@@ -37,15 +39,20 @@ verify_args() {
 
 execute_cloud_agnostic_tests() {
   local routing_mode="$1"
+  local skip_addons_tests="$2"
   local cloud_agnostic_tests=("pod_logs" "generic" "oss_only" "api_extensions")
+  local ginkgo_flags
 
-  if [[ ${routing_mode} == "cf" ]]; then
-    ginkgo -progress -v "$BASE_DIR/src/tests/integration-tests/cloudfoundry"
+  if ! [[ -z "$skip_addons_tests" ]]; then
+    ginkgo_flags="--skip=check\ apply-specs"
   fi
 
-  for test in "${cloud_agnostic_tests[@]}"
-  do
-    ginkgo -progress -v "$BASE_DIR/src/tests/integration-tests/${test}"
+  if [[ ${routing_mode} == "cf" ]]; then
+    ginkgo -dryRun -progress -v "$BASE_DIR/src/tests/integration-tests/cloudfoundry"
+  fi
+
+  for test in "${cloud_agnostic_tests[@]}"; do
+    ginkgo -dryRun -progress -v "$ginkgo_flags" "$BASE_DIR/src/tests/integration-tests/${test}"
   done
 
 }
@@ -65,18 +72,34 @@ execute_cloud_specific_tests(){
         ;;
     esac
 
-    ginkgo -progress -v "$BASE_DIR/src/tests/integration-tests/workload/k8s_lbs"
+    ginkgo -dryRun -progress -v "$BASE_DIR/src/tests/integration-tests/workload/k8s_lbs"
   fi
 
   if [[ "${iaas}" != "openstack" ]]; then
-      ginkgo -progress -v "$BASE_DIR/src/tests/integration-tests/persistent_volume"
+      ginkgo -dryRun -progress -v "$BASE_DIR/src/tests/integration-tests/persistent_volume"
   fi
 }
 
 run_tests() {
   local environment="$1"
   local deployment="$2"
-  local run_cloud_agnostic_tests="$3"
+  local skip_addons_tests skip_cloud_agnostic_tests
+
+  shift 2
+  for flag in "$@"; do
+    case "$flag" in
+      --skip-addons-tests)
+        skip_addons_tests=true
+        ;;
+      --skip-cloud-agnostic-tests)
+        skip_cloud_agnostic_tests=true
+        ;;
+      *)
+        echo "$flag is not a valid flag"
+        exit 1
+        ;;
+    esac
+  done
 
   local iaas=$(bosh-cli int "$environment/director.yml" --path='/iaas')
   local routing_mode=$(bosh-cli int "$environment/director.yml" --path='/routing_mode')
@@ -85,8 +108,8 @@ run_tests() {
   $BASE_DIR/scripts/generate-test-config.sh $environment $deployment > $tmpfile
   export CONFIG=$tmpfile
 
-  if [[ ${run_cloud_agnostic_tests} ]]; then
-    execute_cloud_agnostic_tests "${routing_mode}"
+  if [[ -z "$skip_cloud_agnostic_tests" ]]; then
+    execute_cloud_agnostic_tests "${routing_mode}" "${skip_addons_tests}"
   fi
 
   execute_cloud_specific_tests "${routing_mode}" "${iaas}"
