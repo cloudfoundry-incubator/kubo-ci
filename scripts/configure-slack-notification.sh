be2c79ef92c2a9d566be2b1b3b2a048ec0657161
Sagar Muchhal <muchhals@vmware.com>
2018-01-17 16:59:03 -0500

    Use email name as slack username in notification
    [#153294254]
    
    Signed-off-by: Tony Wong <twong@pivotal.io>

--- scripts/configure-slack-notification.sh
+++ scripts/configure-slack-notification.sh
@@ -6,8 +6,11 @@ set -exu -o pipefail
 COMMITTER=$(cat git-kubo-ci/.git/committer)
 export COMMITTER
 
+SLACK_NAME=$(echo $COMMITTER | cut -d@ -f1)
+
 REF=$(cat git-kubo-ci/.git/ref)
 export REF
 
 echo "Committer: $COMMITTER\nRef: $REF" > slack-notification/text
-echo "@tony" > slack-notification/channel
+
+echo "@$SLACK_NAME" > slack-notification/channel
