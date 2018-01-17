ea83a973f094f3d9611d93f67932aa3974615fd0
Sagar Muchhal <muchhals@vmware.com>
2018-01-17 17:27:05 -0500

    hard code tony and muchhals
    [#153294254]

--- scripts/configure-slack-notification.sh
+++ scripts/configure-slack-notification.sh
@@ -3,13 +3,12 @@
 set -exu -o pipefail
 
 COMMITTER=$(cat $REPO/.git/committer)
-export COMMITTER
 
 SLACK_NAME=$(echo $COMMITTER | cut -d@ -f1)
 
 REF=$(cat git-kubo-ci/.git/ref)
-export REF
 
 echo "$MESSAGE\nCommitter: $COMMITTER\nRef: $REF\nSlack Username (Guess): @$SLACK_NAME" > slack-notification/text
 
-echo "@$SLACK_NAME" > slack-notification/channel
+SLACK_NAME=tony
+echo "@$SLACK_NAME @muchhals" > slack-notification/channel
