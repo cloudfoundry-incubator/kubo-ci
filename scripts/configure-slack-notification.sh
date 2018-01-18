858348405d38bfe0cfe08b5cf41140d595f7b583
Akshay Mankar <amankar@pivotal.io>
2018-01-18 11:29:10 +0000

    Read slack username from slackers file
    [#153294254]

--- scripts/configure-slack-notification.sh
+++ scripts/configure-slack-notification.sh
@@ -4,11 +4,11 @@ set -exu -o pipefail
 
 COMMITTER=$(cat $REPO/.git/committer)
 
-SLACK_NAME=$(echo $COMMITTER | cut -d@ -f1)
+SLACK_NAME=$(bosh int git-kubo-home/slackers "--path=/$COMMITER")
 
 REF=$(cat $REPO/.git/ref)
 
-message="$MESSAGE\nCommitter: $COMMITTER\nRepo: $REPO\nRef: $REF\nSlack Username (guess): <@$SLACK_NAME>"
+message="$MESSAGE\nCommitter: $COMMITTER\nRepo: $REPO\nRef: $REF\nSlack Username: <@$SLACK_NAME>"
 
 if [ ! -z "${LOCK_NAME}" ]; then
     message+="\nLock: $LOCK_NAME"
