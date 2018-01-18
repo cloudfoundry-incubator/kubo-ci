533297e4db6aabf0ef43c4284174102bcef04a6f
Akshay Mankar <amankar@pivotal.io>
2018-01-18 14:07:43 +0000

    Use target-repo as default for slack notification
    [#153294254]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- scripts/configure-slack-notification.sh
+++ scripts/configure-slack-notification.sh
@@ -2,21 +2,24 @@
 
 set -exu -o pipefail
 
+REPO=${REPO:-target-repo}
+
 # .git/ref is provided by concourse resource
 REF=$(cat "$REPO/.git/ref")
 
-COMMITTER=$(git -C "$REPO" show -s --format="%ce" "$REF")
-AUTHOR=$(git -C "$REPO" show -s --format="%ae" "$REF")
+alias git="git -C $REPO"
 
-
+COMMITTER=$(git show -s --format="%ce" "$REF")
 COMMITTER_SLACK_NAME=$(bosh int git-kubo-home/slackers "--path=/$COMMITTER")
+
+AUTHOR=$(git show -s --format="%ae" "$REF")
 AUTHOR_SLACK_NAME=$(bosh int git-kubo-home/slackers "--path=/$AUTHOR")
 
 message="$MESSAGE
 Committer: $COMMITTER
 Author: $AUTHOR
 Repo: $REPO
-Ref: $REF
+Ref: https://github.com//$REPO/commit/$REF
 Slack Usernames: <@$COMMITTER_SLACK_NAME> <@$AUTHOR_SLACK_NAME>"
 
 if [ ! -z "${LOCK_NAME}" ]; then
