9bdcc08b05ec1eed7ddccdb5eaecf392a2d02feb
Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>
2018-01-18 14:16:46 +0000

    Alias doesn't work in a subshell
    [#153294254]
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- scripts/configure-slack-notification.sh
+++ scripts/configure-slack-notification.sh
@@ -7,19 +7,17 @@ REPO=${REPO:-target-repo}
 # .git/ref is provided by concourse resource
 REF=$(cat "$REPO/.git/ref")
 
-alias git="git -C $REPO"
-
-COMMITTER=$(git show -s --format="%ce" "$REF")
+COMMITTER=$(git -C "$REPO" show -s --format="%ce" "$REF")
 COMMITTER_SLACK_NAME=$(bosh int git-kubo-home/slackers "--path=/$COMMITTER")
 
-AUTHOR=$(git show -s --format="%ae" "$REF")
+AUTHOR=$(git -C "$REPO" show -s --format="%ae" "$REF")
 AUTHOR_SLACK_NAME=$(bosh int git-kubo-home/slackers "--path=/$AUTHOR")
 
 message="$MESSAGE
 Committer: $COMMITTER
 Author: $AUTHOR
 Repo: $REPO
-Ref: https://github.com//$REPO/commit/$REF
+Ref: https://github.com/$REPO/commit/$REF
 Slack Usernames: <@$COMMITTER_SLACK_NAME> <@$AUTHOR_SLACK_NAME>"
 
 if [ ! -z "${LOCK_NAME}" ]; then
