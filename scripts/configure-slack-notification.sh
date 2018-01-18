ec8ba67dbb86ed3016169f91e7d66bb396c387d1
Akshay Mankar <amankar@pivotal.io>
2018-01-18 12:19:17 +0000

    Prettify the code to write slack messages
    [#153294254]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- scripts/configure-slack-notification.sh
+++ scripts/configure-slack-notification.sh
@@ -12,10 +12,16 @@ AUTHOR=$(git -C "$REPO" show -s --format="%ae" "$REF")
 COMMITTER_SLACK_NAME=$(bosh int git-kubo-home/slackers "--path=/$COMMITTER")
 AUTHOR_SLACK_NAME=$(bosh int git-kubo-home/slackers "--path=/$AUTHOR")
 
-message="$MESSAGE\nCommitter: $COMMITTER\nAuthor: $AUTHOR\nRepo: $REPO\nRef: $REF\nSlack Usernames: <@$COMMITTER_SLACK_NAME> <@$AUTHOR_SLACK_NAME>"
+message="$MESSAGE
+Committer: $COMMITTER
+Author: $AUTHOR
+Repo: $REPO
+Ref: $REF
+Slack Usernames: <@$COMMITTER_SLACK_NAME> <@$AUTHOR_SLACK_NAME>"
 
 if [ ! -z "${LOCK_NAME}" ]; then
-    message+="\nLock: $LOCK_NAME"
+    message+="
+Lock: $LOCK_NAME"
 fi
 
 echo "$message" > slack-notification/text
