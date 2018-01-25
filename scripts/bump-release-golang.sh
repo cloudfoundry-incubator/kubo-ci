b41f5ecec7e0e855a9d7092548f521d21ff6e1f3
Brenda Chan <brchan@pivotal.io>
2018-01-25 11:40:32 -0500

    Fixes golang bosh packages path, sets git config before committing
    [#154521584]

--- scripts/bump-release-golang.sh
+++ scripts/bump-release-golang.sh
@@ -12,10 +12,12 @@ extract_golang_release() {
 
 vendor_golang() {
   pushd modified-release
-    bosh vendor-package golang-"$MINOR_GO_VERSION"-linux "$HOME_DIR"/golang
-
-    git add .
+    bosh vendor-package golang-"$MINOR_GO_VERSION"-linux "$HOME_DIR"/golang/bosh-packages-golang-release-*
     go_version="$(bosh int packages/golang-"$MINOR_GO_VERSION"-linux/spec.lock --path=/name)"
+
+    git config --global user.email "cfcr+cibot@pivotal.io"
+    git config --global user.name "CFCR CI BOT"
+    git add .
     git commit -m "Updates golang to version $go_version"
   popd
 }
