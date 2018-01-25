8ccd9039b6390009f233f70a6b3d5989a2f89421
Brenda Chan <brchan@pivotal.io>
2018-01-25 11:25:15 -0500

    First pass at automating bumping golang for kubo-release
    - uses bosh vendored packages
    
    [#154521584]

---
+++ scripts/bump-release-golang.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -exu -o pipefail
+
+MINOR_GO_VERSION="1.9"
+HOME_DIR="$PWD"
+
+extract_golang_release() {
+  mkdir golang
+  tar -xzvf golang-release/*.tar.gz -C "$HOME_DIR"/golang
+}
+
+vendor_golang() {
+  pushd modified-release
+    bosh vendor-package golang-"$MINOR_GO_VERSION"-linux "$HOME_DIR"/golang
+
+    git add .
+    go_version="$(bosh int packages/golang-"$MINOR_GO_VERSION"-linux/spec.lock --path=/name)"
+    git commit -m "Updates golang to version $go_version"
+  popd
+}
+
+create_output_directory() {
+  cp -a release/. modified-release
+}
+
+main() {
+  create_output_directory
+  extract_golang_release
+  vendor_golang
+}
+
+main
