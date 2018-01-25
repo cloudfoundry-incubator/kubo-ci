2696703ac8ad36e3c8c4060bd68bc9c8bff07df6
Brenda Chan <brchan@pivotal.io>
2018-01-25 12:05:58 -0500

    Fetch full go version, don't error if there is nothing to commit
    [#154521584]

--- scripts/bump-release-golang.sh
+++ scripts/bump-release-golang.sh
@@ -11,14 +11,22 @@ extract_golang_release() {
 }
 
 vendor_golang() {
+  go_version="$(bosh blobs --dir=)"
+  pushd "$HOME_DIR"/golang/bosh-packages-golang-release-*
+    blob_name="$(bosh blobs | grep linux | grep 1.9 | awk '{print $1}')"
+    go_version="${blob_name%.tar.gz}"
+  popd
+
   pushd modified-release
     bosh vendor-package golang-"$MINOR_GO_VERSION"-linux "$HOME_DIR"/golang/bosh-packages-golang-release-*
-    go_version="$(bosh int packages/golang-"$MINOR_GO_VERSION"-linux/spec.lock --path=/name)"
 
     git config --global user.email "cfcr+cibot@pivotal.io"
     git config --global user.name "CFCR CI BOT"
+
+    set +e
     git add .
     git commit -m "Updates golang to version $go_version"
+    set -e
   popd
 }
 
