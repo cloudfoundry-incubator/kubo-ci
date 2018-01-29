b1558859556ff753e72f1d7a3bbc0488434fdce4
Ben Chapman <bchapman@pivotal.io>
2018-01-29 11:46:29 +0100

    Add a pipeline to test ABAC
    [#152937711]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- scripts/update-kubernetes-port.sh
+++ scripts/update-kubernetes-port.sh
@@ -2,29 +2,27 @@
 
 set -exu -o pipefail
 
-add_100_to_property() {
+modify_port() {
   local property_name="${1}"
-  local property_value="$(bosh-cli int --path "/${property_name}" "kubo-lock-pre/metadata")"
-  local new_property_value="$(expr "${property_value}" + 100)"
   local ops_file="$(mktemp)"
 
 cat > "${ops_file}" <<EOF
 - type: replace
   path: /${property_name}
-  value: ${new_property_value}
+  value: ${2}
 EOF
 
   local temp_metadata_file="$(mktemp)"
   bosh-cli int "kubo-lock-pre/metadata" \
-    -o ${ops_file} \
+    -o "${ops_file}" \
     > "${temp_metadata_file}"
 
   cp "${temp_metadata_file}" "kubo-lock-pre/metadata"
 }
 
 main() {
-  add_100_to_property "external_kubo_port"
-  add_100_to_property "kubernetes_master_port"
+  modify_port "external_kubo_port" "${PORT_NUMBER}"
+  modify_port "kubernetes_master_port" "${PORT_NUMBER}"
 
   cp -R kubo-lock-pre/* kubo-lock
 }
