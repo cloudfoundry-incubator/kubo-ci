ee9d4c718b25d3dfd9e174940b0e83a440471c0b
Sagar Muchhal <muchhals@vmware.com>
2018-02-02 14:59:07 -0800

    Fixed packaging script to exclude tests folder

--- scripts/build-kubo-deployment-tarball.sh
+++ scripts/build-kubo-deployment-tarball.sh
@@ -7,10 +7,10 @@ cp -r git-kubo-deployment tmp/kubo-deployment
 cp gcs-kubo-release-tarball-untested/kubo-*.tgz tmp/kubo-release.tgz
 
 cd tmp
-tar --exclude='./src/kubo-deployment-tests' \
+tar --exclude='src/kubo-deployment-tests' \
     --exclude='.git' \
     --exclude='.gitignore' \
-    --exclude='./bosh-deployment/.gitignore' \
-    --exclude='./bosh-deployment/.gitrepo' \
-    --exclude='./bosh-deployment/tests/.gitignore' \
+    --exclude='bosh-deployment/.gitignore' \
+    --exclude='bosh-deployment/.gitrepo' \
+    --exclude='bosh-deployment/tests/.gitignore' \
     -zcvf "../kubo-deployment-tarball/kubo-deployment-$(cat ../kubo-version/version).tgz" *
