ab1ae0cc24ed101a0a5674914af0478f9b4b54da
Sagar Muchhal <muchhals@vmware.com>
2018-02-05 15:54:07 -0800

    Auto bump CI image to use new GOVC release
    [#154945591]

--- scripts/bump-govc-ci-image.sh
+++ scripts/bump-govc-ci-image.sh
@@ -7,4 +7,9 @@ tag=$(cat "$PWD/govc-release/tag")
 cp -r git-kubo-ci/. git-kubo-ci-output
 pushd git-kubo-ci-output/docker-images/kubo-ci
 sed -E -i -e "/govmomi/s/v([0-9]+\.)+[0-9]+/${tag}/" Dockerfile
+
+git config --global user.email "cfcr+cibot@pivotal.io"
+git config --global user.name "CFCR CI BOT"
+git add .
+git commit -m "Bump CI image to govc-$tag"
 popd
