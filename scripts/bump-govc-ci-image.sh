1ec307ad0a3a72502f3b27776e81555c84e133aa
Sagar Muchhal <muchhals@vmware.com>
2018-02-05 14:36:08 -0800

    Auto bump kubo-ci docker image on govc bump

---
+++ scripts/bump-govc-ci-image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -exu -o pipefail
+
+tag=$(cat "$PWD/govc-release/tag")
+
+cp -r git-kubo-ci/. git-kubo-ci-output
+pushd git-kubo-ci-output/docker-images/kubo-ci
+sed -E -i -e "/govmomi/s/v([0-9]+\.)+[0-9]+/${tag}/" Dockerfile
+popd
