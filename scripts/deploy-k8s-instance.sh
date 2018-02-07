4b980a88150ba7d9c2d03b9f14cc7ad8a9be44f0
Christian Ang <cang@pivotal.io>
2018-02-07 14:31:47 +0000

    Enable multi-az deployment on GCP
    [#154869857]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- scripts/deploy-k8s-instance.sh
+++ scripts/deploy-k8s-instance.sh
@@ -9,13 +9,13 @@ export BOSH_LOG_PATH="$PWD/bosh.log"
 export DEBUG=1
 
 metadata_path="${KUBO_ENVIRONMENT_DIR}/director.yml"
-if [ -z ${LOCAL_DEV+x} ] || [ "$LOCAL_DEV" != "1" ]; then
+if [[ -z ${LOCAL_DEV+x} ]] || [[ "$LOCAL_DEV" != "1" ]]; then
   cp "$PWD/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
   cp "kubo-lock/metadata" "$metadata_path"
   tarball_name=$(ls $PWD/gcs-kubo-release-tarball/kubo-*.tgz | head -n1)
 
   # Copy guestbook if WITHOUT_ADDONS isn't set to true
-  if [ -z ${WITHOUT_ADDONS+x} ] || [ "$WITHOUT_ADDONS" != "1" ]; then
+  if [[ -z ${WITHOUT_ADDONS+x} ]] || [[ "$WITHOUT_ADDONS" != "1" ]]; then
     cp "git-kubo-ci/specs/guestbook.yml" "${KUBO_ENVIRONMENT_DIR}/addons.yml"
   else
     # Delete the addons_spec_path from director.yml
@@ -26,7 +26,7 @@ else
   tarball_name="$KUBO_RELEASE_TARBALL"
 fi
 
-if [ -z ${WITH_PRIVILEGED_CONTAINERS+x} ] || [ "$WITH_PRIVILEGED_CONTAINERS" == "1" ]; then
+if [[ -z ${WITH_PRIVILEGED_CONTAINERS+x} ]] || [[ "$WITH_PRIVILEGED_CONTAINERS" == "1" ]]; then
   echo "allow_privileged_containers: true" >> "${metadata_path}"
 fi
 
@@ -47,4 +47,8 @@ export CLOUD_CONFIG_OPS_FILES
 
 release_source="local"
 
-"$KUBO_DEPLOYMENT_DIR/bin/deploy_k8s" "$KUBO_ENVIRONMENT_DIR" "${DEPLOYMENT_NAME}" "$release_source"
+if [[ -f "${KUBO_CI_DIR}/manifests/ops-files/${DEPLOYMENT_OPS_FILE}" ]]; then
+  cp "${KUBO_CI_DIR}/manifests/ops-files/${DEPLOYMENT_OPS_FILE}" "${KUBO_ENVIRONMENT_DIR}/${DEPLOYMENT_NAME}.yml"
+fi
+
+"$KUBO_DEPLOYMENT_DIR/bin/deploy_k8s" "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}" "$release_source"
