45a8ecc5a3154b87d96532face68e65478946c7b
Sagar Muchhal <muchhals@vmware.com>
2018-02-09 15:02:00 -0800

    Autogen PR for k8s binaries on a new release
    [#154530152]

---
+++ scripts/bump-k8s.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -exu -o pipefail
+
+source ./lib/generate-pr.sh
+
+tag=$(cat "$PWD/k8s-release/tag")
+version=$(cat "$PWD/k8s-release/version")
+
+cp -r git-kubo-release/. git-kubo-release-output
+pushd git-kubo-release-output
+
+existing_k8s_version=$(bosh blobs --column path | grep kubelet | grep -o -E '([0-9]+\.)+[0-9]+')
+
+if [ $existing_k8s_version == $version ]; then
+    echo "Kubernetes version already up-to-date."
+    exit 0
+else
+    ./scripts/download_k8s_binaries $version
+    cat <<EOF > "config/private.yml"
+blobstore:
+  options:
+    access_key_id: ${ACCESS_KEY_ID}
+    secret_access_key: ${SECRET_ACCESS_KEY}
+EOF
+    #bosh upload-blobs
+    #generate_pull_request "kubernetes" $tag
+    exit 1
+fi
+
+popd
---
+++ scripts/lib/generate-pr.sh
@@ -0,0 +1,40 @@
+create_pr_payload() {
+  title="$1 upgrade $2"
+  body="This is an auto generated PR created for $1 upgrade to $2"
+  echo '{"title":"'"$title"'","body":"'"$body"'","head":"'"$3"'","base":"master"}'
+}
+
+# Needs to be called from the directory where PR needs to be generated
+generate_pull_request() {
+  local component=$1
+  local tag=$2
+
+  mkdir -p ~/.ssh
+  cat > ~/.ssh/config <<EOF
+StrictHostKeyChecking no
+LogLevel quiet
+EOF
+  chmod 0600 ~/.ssh/config
+
+  cat > ~/.ssh/id_rsa <<EOF
+${GIT_SSH_KEY}
+EOF
+  chmod 0600 ~/.ssh/id_rsa
+  eval $(ssh-agent) >/dev/null 2>&1
+  trap "kill $SSH_AGENT_PID" 0
+  ssh-add ~/.ssh/id_rsa
+
+  git config --global user.email "cfcr+cibot@pivotal.io"
+  git config --global user.name "CFCR CI BOT"
+
+  branch_name="upgrade/${component}${tag}"
+  git checkout -b $branch_name
+  git add .
+  git commit -m "Upgrade $component to $tag"
+  git push origin $branch_name
+
+  # create a PR here
+  token=${CFCR_USER_TOKEN}
+  payload=$(create_pr_payload $component $tag $branch_name)
+  curl -u cfcr:${CFCR_USER_TOKEN} -H "Content-Type: application/json" -X POST -d "$payload" https://api.github.com/repos/cloudfoundry-incubator/kubo-release/pulls
+}
