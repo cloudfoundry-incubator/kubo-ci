7cb46032d8379cb201865c735d22c704a0e99018
Sagar Muchhal <muchhals@vmware.com>
2018-02-12 14:06:14 -0800

    Added changes to k8s packaging during auto PR gen
    [#154530152]

--- scripts/bump-k8s.sh
+++ scripts/bump-k8s.sh
@@ -5,17 +5,24 @@ source git-kubo-ci/scripts/lib/generate-pr.sh
 
 tag=$(cat "$PWD/k8s-release/tag")
 version=$(cat "$PWD/k8s-release/version")
+name="kubernetes-${version}"
 
 cp -r git-kubo-release/. git-kubo-release-output
 pushd git-kubo-release-output
 
-existing_k8s_version=$(bosh blobs --column path | grep kubelet | grep -o -E '([0-9]+\.)+[0-9]+')
+existing_k8s_spec=$(bosh blobs --column path | grep kubelet | grep -o -E 'kubernetes-([0-9]+\.)+[0-9]+')
+existing_k8s_version=$(echo $existing_k8s_spec | grep -o -E '([0-9]+\.)+[0-9]+')
 
 if [ $existing_k8s_version == $version ]; then
     echo "Kubernetes version already up-to-date."
     exit 0
 else
     ./scripts/download_k8s_binaries $version
+    pushd packages/kubernetes
+    sed -E -i -e "s/([0-9]+\.)+[0-9]+/${version}/" packaging
+    sed -E -i -e "s/${existing_k8s_spec}/${name}/" spec
+    popd
+
     cat <<EOF > "config/private.yml"
 blobstore:
   options:
