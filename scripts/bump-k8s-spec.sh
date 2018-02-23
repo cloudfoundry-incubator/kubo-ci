3ff10a48588b0390302359553981b3e03f952cb5
Sagar Muchhal <muchhals@vmware.com>
2018-02-23 11:45:05 -0800

    Scripts to bump k8s dashboard components
    These scripts will be levearged as part of the bump-k8s pipeline to
    automatically post changes to kubo-release when a component gets
    updated.
    Currently, influxdb is not updated since it does not cut new releases to
    trigger the pipeline.
    
    [#155143144]

---
+++ scripts/bump-k8s-spec.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+source /common.sh
+start_docker
+
+#docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+set -x
+if [ ! -f spec-to-update/spec.env ]; then
+    echo "No new versions found to update."
+    exit 0
+fi
+source spec-to-update/spec.env
+
+tag=$(cat "$PWD/$SPEC_RELEASE_DIR/tag")
+version=$(cat "$PWD/$SPEC_RELEASE_DIR/version")
+
+cp -r git-kubo-release/. git-kubo-release-output
+pushd git-kubo-release-output
+
+scripts/download_container_images "$SPEC_IMAGE_URL:$tag"
+sed -E -i -e "/${SPEC_IMAGE_NAME}:/s/v([0-9]+\.)+[0-9]+/${tag}/" scripts/download_container_images
+sed -E -i -e "/${SPEC_IMAGE_NAME}:/s/v([0-9]+\.)+[0-9]+/${tag}/" "jobs/apply-specs/templates/specs/${SPEC_NAME}.yml.erb"
+
+set +x
+cat <<EOF > "config/private.yml"
+blobstore:
+  options:
+    access_key_id: ${ACCESS_KEY_ID}
+    secret_access_key: ${SECRET_ACCESS_KEY}
+EOF
+set -x
+
+git diff
+# bosh upload-blobs
+
+# git config --global user.email "cfcr+cibot@pivotal.io"
+# git config --global user.name "CFCR CI BOT"
+# git add .
+# git commit -m "Bump ${SPEC_NAME} to version ${version}"
+popd
