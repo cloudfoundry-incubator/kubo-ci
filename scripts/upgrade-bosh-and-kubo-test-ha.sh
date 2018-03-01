69f73cb15a3c0b8a408c699808a4caf4074e69db
Christian Ang <cang@pivotal.io>
2018-03-01 16:15:06 +0000

    Add aws ingress group id
    [#155330320]
    
    Signed-off-by: Ben Chapman <bchapman@pivotal.io>

--- scripts/upgrade-bosh-and-kubo-test-ha.sh
+++ scripts/upgrade-bosh-and-kubo-test-ha.sh
@@ -20,6 +20,8 @@ if [[ "${IAAS}" == "aws" ]]; then
   aws configure set aws_access_key_id "$(bosh int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path=/access_key_id)"
   aws configure set aws_secret_access_key  "$(bosh int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path=/secret_access_key)"
   aws configure set default.region "$(bosh int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path=/region)"
+  AWS_INGRESS_GROUP_ID=$(bosh int "${environment}/director.yml" --path=/default_security_groups/0)
+  export AWS_INGRESS_GROUP_ID
 fi
 
 ginkgo -progress -v -failFast "$DIR/../src/tests/upgrade-tests"
