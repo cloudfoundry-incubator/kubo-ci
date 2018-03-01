57927dd7e191b385806d51a4ce485d9d909425d7
Ben Chapman <bchapman@pivotal.io>
2018-03-01 15:53:17 +0000

    Configure aws cli for upgrade test
    - when running on aws
    
    [#155330320]
    
    Signed-off-by: Christian Ang <cang@pivotal.io>

--- scripts/upgrade-bosh-and-kubo-test-ha.sh
+++ scripts/upgrade-bosh-and-kubo-test-ha.sh
@@ -14,4 +14,12 @@ tmpfile=$(mktemp)
 $DIR/generate-test-config.sh "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}" > "${tmpfile}"
 export CONFIG="${tmpfile}"
 
+IAAS=$(bosh int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path='/iaas')
+
+if [[ "${IAAS}" == "aws" ]]; then
+  aws configure set aws_access_key_id "$(bosh int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path=/access_key_id)"
+  aws configure set aws_secret_access_key  "$(bosh int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path=/secret_access_key)"
+  aws configure set default.region "$(bosh int "${KUBO_ENVIRONMENT_DIR}/director.yml" --path=/region)"
+fi
+
 ginkgo -progress -v -failFast "$DIR/../src/tests/upgrade-tests"
