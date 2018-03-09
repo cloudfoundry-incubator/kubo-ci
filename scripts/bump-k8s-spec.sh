4e66b7803845eb963bec09dbfe5cbad6ab221f96
Sagar Muchhal <muchhals@vmware.com>
2018-03-09 01:48:39 -0800

    Fixed k8s bump specs pipeline

--- scripts/bump-k8s-spec.sh
+++ scripts/bump-k8s-spec.sh
@@ -18,7 +18,7 @@ version=$(cat "$PWD/$SPEC_RELEASE_DIR/version")
 cp -r git-kubo-release/. git-kubo-release-output
 pushd git-kubo-release-output
 
-bosh remove-blob $( bosh blobs --column path | grep "${SPEC_IMAGE_NAME}" )
+bosh remove-blob $( bosh blobs --column path | grep "${SPEC_BLOB_NAME}" )
 scripts/download_container_images "$SPEC_IMAGE_URL:$tag"
 sed -E -i -e "/${SPEC_IMAGE_NAME}:/s/v([0-9]+\.)+[0-9]+/${tag}/" scripts/download_container_images
 sed -E -i -e "/${SPEC_IMAGE_NAME}:/s/v([0-9]+\.)+[0-9]+/${tag}/" "jobs/apply-specs/templates/specs/${SPEC_NAME}.yml.erb"
--- scripts/compare-specs.sh
+++ scripts/compare-specs.sh
@@ -5,23 +5,23 @@ set -exu -o pipefail
 base_dir=$PWD
 pushd git-kubo-release
 
-specs=("k8s.gcr.io_heapster-amd64"
-       #"k8s.gcr.io_heapster-influxdb-amd64"
-       "gcr.io_google_containers_kubernetes-dashboard-amd64")
+blob_names=("k8s.gcr.io_heapster-amd64"
+            "gcr.io_google_containers_kubernetes-dashboard-amd64")
 releases=("heapster-release"
-          #"influxdb-release"
           "kubernetes-dashboard-release")
 urls=("k8s.gcr.io/heapster-amd64"
-      #"k8s.gcr.io/heapster-influxdb-amd64"
       "gcr.io/google_containers/kubernetes-dashboard-amd64")
+names=("heapster-amd64"
+       "kubernetes-dashboard-amd64")
 
-for i in ${!specs[@]}; do
-    existing_spec_version=$(bosh blobs --column path | grep "${specs[i]}" | grep -o -E 'v([0-9]+\.)+[0-9]+')
+for i in ${!blob_names[@]}; do
+    existing_spec_version=$(bosh blobs --column path | grep "${blob_names[i]}" | grep -o -E 'v([0-9]+\.)+[0-9]+')
     fetched_spec_version=$(cat "$base_dir/${releases[i]}/tag")
     if [[ $existing_spec_version != $fetched_spec_version ]]; then
         cat <<EOF > "$base_dir/spec-to-update/spec.env"
 export SPEC_RELEASE_DIR=${releases[i]}
-export SPEC_IMAGE_NAME=${specs[i]}
+export SPEC_BLOB_NAME=${blob_names[i]}
+export SPEC_IMAGE_NAME=${names[i]}
 export SPEC_IMAGE_URL=${urls[i]}
 export SPEC_NAME=$(echo "${releases[i]}" | sed 's/.\{8\}$//')
 EOF
