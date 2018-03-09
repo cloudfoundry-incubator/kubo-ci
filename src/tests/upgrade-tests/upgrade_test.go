3bb1a11bc4a37eaf6e2e353c82cba454bf898412
Iain Sproat <isproat@pivotal.io>
2018-03-09 15:11:25 +0000

    Make sure nginx is up before monitoring
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- src/tests/upgrade-tests/upgrade_test.go
+++ src/tests/upgrade-tests/upgrade_test.go
@@ -78,37 +78,46 @@ func upgradeAndMonitorAvailability(pathToScript string, component string, reques
 		return net.LookupHost(loadbalancerAddress)
 	}, "5m", "5s").ShouldNot(HaveLen(0))
 
+	//need make we the first 200
 	By("Monitoring availability")
+	appUrl := fmt.Sprintf("http://%s", loadbalancerAddress)
 	doneChannel := make(chan bool)
 	totalCount := 0
 	successCount := 0
-	go func(doneChannel chan bool) {
+	curlNginx := func() (int, error) {
+		httpClient := http.Client{
+			Timeout: time.Duration(45 * time.Second),
+		}
+		ret, err := httpClient.Get(appUrl)
+		if err != nil {
+			return 0, err
+		}
+		return ret.StatusCode, err
+	}
+	Eventually(curlNginx, "5m", "5s").Should(Equal(200))
+
+	go func(doneChannel chan bool, f func() (int, error)) {
+		fmt.Fprintf(os.Stdout, "\nStart curling endpoint %s", appUrl)
 		for {
 			select {
 			case <-doneChannel:
+				fmt.Fprintf(os.Stdout, "\nDone curling endpoint. Successful response received %d out of %d times (%.2f)", successCount, totalCount, float64(successCount)/float64(totalCount))
 				return
 			default:
-				appUrl := fmt.Sprintf("http://%s", loadbalancerAddress)
-
-				timeout := time.Duration(45 * time.Second)
-				httpClient := http.Client{
-					Timeout: timeout,
-				}
-
-				result, err := httpClient.Get(appUrl)
+				result, err := f()
 				totalCount++
 				if err != nil {
 					fmt.Fprintf(os.Stdout, "\nFailed to get response from %s: %v", appUrl, err)
-				} else if result != nil && result.StatusCode != 200 {
-					fmt.Fprintf(os.Stdout, "\nFailed to get response from %s: StatusCode %v", appUrl, result.StatusCode)
-				} else {
-					successCount++
 				}
-				fmt.Fprintf(os.Stdout, "\nSuccessfully curled server %d out of %d times (%.2f)", successCount, totalCount, float64(successCount)/float64(totalCount))
+				if result == 200 {
+					successCount++
+				} else {
+					fmt.Fprintf(os.Stdout, "\nFailed to get 200 StatusCode from %s. Instead received StatusCode %v", appUrl, result)
+				}
 				time.Sleep(time.Second)
 			}
 		}
-	}(doneChannel)
+	}(doneChannel, curlNginx)
 
 	By(fmt.Sprintf("Running %s upgrade", component))
 	script := test_helpers.PathFromRoot(pathToScript)
