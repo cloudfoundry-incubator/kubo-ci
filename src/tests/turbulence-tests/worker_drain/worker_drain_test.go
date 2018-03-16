b33ea9fbf37c85a1d902f8346ea8a526bc91af67
Iain Sproat <isproat@pivotal.io>
2018-03-16 14:39:53 +0000

    StorageClass used in worker_drain_test
    - worker_drain_test previously assumed a default storage class.
    - drain-types spec requires ci-storage StorageClass
    
    [#155510567]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- src/tests/turbulence-tests/worker_drain/worker_drain_test.go
+++ src/tests/turbulence-tests/worker_drain/worker_drain_test.go
@@ -1,6 +1,7 @@
 package worker_drain
 
 import (
+	"fmt"
 	. "tests/test_helpers"
 
 	director "github.com/cloudfoundry/bosh-cli/director"
@@ -39,8 +40,10 @@ var _ = WorkerDrainDescribe("Worker drain scenarios", func() {
 
 	Specify("Workers are able to drain", func() {
 		By("Deploying all of the drain types")
+		storageClassSpec := PathFromRoot(fmt.Sprintf("specs/storage-class-%s.yml", testconfig.Iaas))
+		Eventually(kubectl.RunKubectlCommand("create", "-f", storageClassSpec), "60s").Should(gexec.Exit(0))
 		Eventually(kubectl.RunKubectlCommand("create", "-f", drainTypesSpec), "30s", "5s").Should(gexec.Exit(0))
-		Eventually(kubectl.RunKubectlCommand("rollout", "status", "daemonset/fluentd-elasticsearch", "-w"), "240s").Should(gexec.Exit(0))
+		Eventually(kubectl.RunKubectlCommand("rollout", "status", "daemonset/fluentd-elasticsearch", "-w"), "120s").Should(gexec.Exit(0))
 
 		By("Recreating all workers successfully")
 		dir := NewDirector(testconfig.Bosh)
