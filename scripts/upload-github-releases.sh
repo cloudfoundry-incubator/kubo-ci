83ad0222255b1d5046227141b9b3eacb1764e458
Akshay Mankar <amankar@pivotal.io>
2018-04-06 15:57:41 +0100

    Parameterize branch for upload-github-releases
    Signed-off-by: Ben Chapman <bchapman@pivotal.io>

--- scripts/upload-github-releases.sh
+++ scripts/upload-github-releases.sh
@@ -11,7 +11,6 @@ git config --global user.email "cf-london-eng@pivotal.io"
 cp -r git-kubo-release/. git-kubo-release-output
 
 cd git-kubo-release-output
-original_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 cat <<EOF > "config/private.yml"
 blobstore:
@@ -38,6 +37,6 @@ git checkout -b tmp/release
 git add .
 git commit -m "Final release for v${version}"
 git tag -a "v${version}" -m "Tag for version v${version}"
-git checkout "${original_branch}"
+git checkout "${BRANCH:-master}"
 git merge tmp/release -m "Merge release branch for v${version}"
 git branch -d tmp/release
