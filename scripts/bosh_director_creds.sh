d0fd5d5b7c54b697a030696345c2b6252155be73
Akshay Mankar <amankar@pivotal.io>
2018-05-09 16:30:48 +0100

    Fix location of metadata file
    [#144108357]
    
    Signed-off-by: Urvashi Reddy <ureddy@pivotal.io>

--- scripts/bosh_director_creds.sh
+++ scripts/bosh_director_creds.sh
@@ -7,7 +7,7 @@ creds_file=gcs-bosh-creds/creds.yml
 BOSH_CA_CERT=$(bosh int ${creds_file} --path=/default_ca/ca)
 BOSH_CLIENT="admin"
 BOSH_CLIENT_SECRET=$(bosh int ${creds_file} --path=/admin_password)
-BOSH_ENVIRONMENT=$(bosh int kubo-locks/metadata --path=/internal_ip)
+BOSH_ENVIRONMENT=$(bosh int kubo-lock/metadata --path=/internal_ip)
 
 export BOSH_CA_CERT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_ENVIRONMENT
 
