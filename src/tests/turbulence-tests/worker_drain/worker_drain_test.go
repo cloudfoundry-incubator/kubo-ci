787687481a79114c459b6be67ed1b03b81c54364
Morena De Liddo <mdeliddo@pivotal.io>
2018-05-11 12:15:31 +0100

    Add turbulence test for worker drain
    [#157145647]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- src/tests/turbulence-tests/worker_drain/worker_drain_test.go
+++ src/tests/turbulence-tests/worker_drain/worker_drain_test.go
@@ -4,6 +4,9 @@ import (
 	. "tests/test_helpers"
 
 	director "github.com/cloudfoundry/bosh-cli/director"
+	"github.com/cppforlife/turbulence/incident"
+	"github.com/cppforlife/turbulence/incident/selector"
+	"github.com/cppforlife/turbulence/tasks"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -28,6 +31,7 @@ var _ = WorkerDrainDescribe("Worker drain scenarios", func() {
 
 		Expect(countRunningWorkers()).To(Equal(3))
 		Expect(AllBoshWorkersHaveJoinedK8s(deployment, kubectl)).To(BeTrue())
+		DeploySmorgasbord(kubectl, testconfig.Iaas)
 	})
 
 	AfterEach(func() {
@@ -38,15 +42,37 @@ var _ = WorkerDrainDescribe("Worker drain scenarios", func() {
 		kubectl.RunKubectlCommand("delete", "namespace", kubectl.Namespace())
 	})
 
-	Specify("Workers are able to drain", func() {
-		By("Deploying all of the drain types")
-		DeploySmorgasbord(kubectl, testconfig.Iaas)
+	Specify("Drain doesn't fail with temporary network issues", func() {
+		vmInfos := DeploymentVmsOfType(deployment, WorkerVmType, VmRunningState)
+		blockedWorkerID := vmInfos[0].ID
+
+		hellRaiser := TurbulenceClient(testconfig.Turbulence)
+		blockOneWorker := incident.Request{
+			Selector: selector.Request{
+				Deployment: &selector.NameRequest{
+					Name: testconfig.Bosh.Deployment,
+				},
+				Group: &selector.NameRequest{
+					Name: WorkerVmType,
+				},
+				ID: &selector.IDRequest{
+					Values: []string{blockedWorkerID},
+				},
+			},
+			Tasks: tasks.OptionsSlice{
+				tasks.FirewallOptions{
+					Type:    "Firewall",
+					Timeout: "3m",
+				},
+			},
+		}
 
 		By("Recreating all workers successfully")
 		dir := NewDirector(testconfig.Bosh)
 		deployment, err := dir.FindDeployment(testconfig.Bosh.Deployment)
 		Expect(err).NotTo(HaveOccurred())
-		err = deployment.Recreate(director.NewAllOrInstanceGroupOrInstanceSlug("worker", ""), director.RecreateOpts{Canaries: "0", MaxInFlight: "100%"})
+		hellRaiser.CreateIncident(blockOneWorker)
+		err = deployment.Recreate(director.NewAllOrInstanceGroupOrInstanceSlug("worker", blockedWorkerID), director.RecreateOpts{Canaries: "0", MaxInFlight: "100%"})
 		Expect(err).NotTo(HaveOccurred())
 	})
 
