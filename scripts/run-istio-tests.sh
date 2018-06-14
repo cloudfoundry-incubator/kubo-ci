224ff08eac2517d9630abc90e3a2e07109c48b0d
Iain Sproat <isproat@pivotal.io>
2018-06-14 17:18:13 +0100

    Updates istioctl path to use kubo-ci location
    [#157047814]

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -6,4 +6,4 @@ source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
 setup_env "${KUBO_ENVIRONMENT_DIR}"
 
 cd "$ISTIO_REPO"
-make e2e_simple E2E_ARGS="--istioctl=$HOME/workspace/istio/istio-0.8.0/bin/istioctl" TAG='0.8.0'
+make e2e_simple E2E_ARGS="--istioctl=/usr/local/istio-0.8.0/bin/istioctl" TAG='0.8.0'
