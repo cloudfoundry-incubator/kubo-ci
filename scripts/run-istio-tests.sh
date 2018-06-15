567b409b6afff4a8ae3c68bf5b2bcd39663d3732
Iain Sproat <isproat@pivotal.io>
2018-06-15 12:28:19 +0100

    Use the built istioctl
    [#157047814]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -7,5 +7,5 @@ setup_env "${KUBO_ENVIRONMENT_DIR}"
 
 go get istio.io/istio || true # go get returns error "no Go files", which is expected
 cd $GOPATH/src/istio.io/istio
-git checkout -b 0.8.0
-make e2e_simple E2E_ARGS="--istioctl=/usr/local/istio-0.8.0/bin/istioctl" TAG='0.8.0'
+git checkout tags/0.8.0 -b 0.8.0
+make e2e_simple TAG='0.8.0'
