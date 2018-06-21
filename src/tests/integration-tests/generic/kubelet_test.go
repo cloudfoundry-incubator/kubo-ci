a69ed2d8c6095389354dc20c52fadfe920d35b43
Morena De Liddo <mdeliddo@pivotal.io>
2018-06-21 14:45:34 +0100

    Test invalid Bearer token requests made to kubelet
    [#158356114]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- src/tests/integration-tests/generic/kubelet_test.go
+++ src/tests/integration-tests/generic/kubelet_test.go
@@ -24,4 +24,21 @@ var _ = Describe("Kubelet", func() {
 		Expect(err).ToNot(HaveOccurred())
 		Expect(resp.StatusCode).To(Equal(401))
 	})
+
+	It("Should fail when requests are made to kubelet with invalid Bearer Token", func() {
+		firstWorkerIP, err := GetNodeIP()
+		Expect(err).NotTo(HaveOccurred())
+		endpoint := fmt.Sprintf("https://%s:10250/pods", firstWorkerIP)
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: tr}
+		req, err := http.NewRequest("GET", endpoint, nil)
+		Expect(err).ToNot(HaveOccurred())
+
+		req.Header.Add("Authorization", "Bearer IMFAKEBEAR")
+		resp, err := client.Do(req)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(resp.StatusCode).To(Equal(401))
+	})
 })
