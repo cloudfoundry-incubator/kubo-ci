78b38f867c0c204699590aeb79eab908fde9bf35
Akshay Mankar <amankar@pivotal.io>
2018-06-28 15:09:46 +0100

    Use helm to deploy istio in tests
    The normal deploy fails waiting for `kubectl rollout status` due to this
    bug: kubernetes/kubernetes#40224
    
    Using helm takes 15 minutes extra to run the tests, which is because of
    a bug in the test code. More info here: istio/istio#6700
    
    [#158625376]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -8,4 +8,4 @@ setup_env "${KUBO_ENVIRONMENT_DIR}"
 go get istio.io/istio || true # go get returns error "no Go files", which is expected
 cd $GOPATH/src/istio.io/istio
 git checkout tags/0.8.0 -b 0.8.0
-make e2e_simple TAG='0.8.0'
+make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm'
