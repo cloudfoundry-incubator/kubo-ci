1317ec7604daa9c2c0406a205d62c1d81ecda4f0
Ben Chapman <bchapman@pivotal.io>
2018-07-03 09:40:41 +0100

    Install helm before the tests run
    [#158625376]
    
    Signed-off-by: Neil Hickey <nhickey@pivotal.io>

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euox pipefail
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
-export PATH="$PATH:/root/go/out/linux_amd64/release"
 
 source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
 setup_env "${KUBO_ENVIRONMENT_DIR}"
@@ -9,4 +8,5 @@ setup_env "${KUBO_ENVIRONMENT_DIR}"
 go get istio.io/istio || true # go get returns error "no Go files", which is expected
 cd $GOPATH/src/istio.io/istio
 git checkout tags/0.8.0 -b 0.8.0
+./bin/init_helm.sh
 make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm --skip_cleanup'
