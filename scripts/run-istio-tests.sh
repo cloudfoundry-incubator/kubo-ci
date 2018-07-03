3f37e1fd530d092c779de8f0af2f47038176e174
Ben Chapman <bchapman@pivotal.io>
2018-07-03 14:58:57 +0100

    Increase timeout for istio tests
    [#158625376]
    
    Signed-off-by: Neil Hickey <nhickey@pivotal.io>

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -9,4 +9,6 @@ go get istio.io/istio || true # go get returns error "no Go files", which is exp
 cd $GOPATH/src/istio.io/istio
 git checkout tags/0.8.0 -b 0.8.0
 ./bin/init_helm.sh
+# increase istio test timeouts. This will hopefully in future
+sed -i -e 's/-timeout 20m/-timeout 60m/g' /root/go/src/istio.io/istio/tests/istio.mk
 make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm'
