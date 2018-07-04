55bae18bd276b7e630861507d0bc12b32c13aa11
Ben Chapman <bchapman@pivotal.io>
2018-07-04 11:17:08 +0100

    Use istio.io/istio as the path
    [#158625376]

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -6,7 +6,8 @@ source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
 setup_env "${KUBO_ENVIRONMENT_DIR}"
 
 go get github.com/cfcr/istio || true # go get returns error "no Go files", which is expected
-cd $GOPATH/src/github.com/cfcr/istio
+mv $PATH/src/github.com/cfcr/istio $PATH/src/istio.io/istio
+cd $GOPATH/src/istio.io/istio
 git checkout 0.8.0
 ./bin/init_helm.sh
 make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm'
