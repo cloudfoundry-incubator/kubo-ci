e520888357d627f17a41e347b741bec1d4aa9580
Ben Chapman <bchapman@pivotal.io>
2018-07-04 10:48:31 +0100

    Fix spelling in istio test script
    [#158625376]

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -6,7 +6,7 @@ source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
 setup_env "${KUBO_ENVIRONMENT_DIR}"
 
 go get github.com/cfcr/istio || true # go get returns error "no Go files", which is expected
-cd $GOPATH/src/gtihub.com/cfcr/istio
+cd $GOPATH/src/github.com/cfcr/istio
 git checkout 0.8.0
 ./bin/init_helm.sh
 make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm'
