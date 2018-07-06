8c878e76ec02eb621f89b5216c52f448fa5d7e5b
Iain Sproat <isproat@pivotal.io>
2018-07-06 10:29:38 +0100

    conformance tests do not require director.yml
    - tests can be run by setting GOPATH, CONFIG and KUBECONFIG variables
    - if CONFIG is not set, it is generated from director.yml
    
    [#157562500]
    
    Signed-off-by: Ben Chapman <bchapman@pivotal.io>

--- scripts/run-k8s-conformance-tests.sh
+++ scripts/run-k8s-conformance-tests.sh
@@ -5,23 +5,29 @@
 set -eu
 set -o pipefail
 
-ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 
-DEPLOYMENT_NAME="${DEPLOYMENT_NAME:="ci-service"}"
-KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
+generate_config() {
+  ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
+  KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
+  DEPLOYMENT_NAME="${DEPLOYMENT_NAME:="ci-service"}"
 
-export GOPATH="${ROOT}/git-kubo-ci"
-export CONFORMANCE_RELEASE_VERSION="$(cat kubo-version/version)"
-export CONFORMANCE_RESULTS_DIR="${ROOT}/${CONFORMANCE_RESULTS_DIR}"
-
-main() {
+  export GOPATH="${ROOT}/git-kubo-ci"
+  export CONFORMANCE_RELEASE_VERSION="$(cat kubo-version/version)"
+  export CONFORMANCE_RESULTS_DIR="${ROOT}/${CONFORMANCE_RESULTS_DIR}"
   source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
   setup_env "${KUBO_ENVIRONMENT_DIR}"
 
   local tmpfile="$(mktemp)" && echo "CONFIG=${tmpfile}"
-  "${ROOT}/git-kubo-ci/scripts/generate-test-config.sh" "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}" > "${tmpfile}"
+  export CONFIG="${tmpfile}"
+  "${ROOT}/git-kubo-ci/scripts/generate-test-config.sh" "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}" > "${CONFIG}"
+}
 
-  CONFIG="${tmpfile}" ginkgo -progress -v "${ROOT}/git-kubo-ci/src/tests/conformance"
+main() {
+  if [[ -z "${CONFIG+x}" ]]; then
+    generate_config
+  fi
+
+  ginkgo -progress -v "${GOPATH}/src/tests/conformance"
 }
 
 main
