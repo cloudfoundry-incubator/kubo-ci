10fbae0c726c43b92740e2a11943647046750539
Oleksandr Slynko <oslynko@pivotal.io>
2018-07-09 12:24:54 +0100

    Fix flag to use dashes instead of underscores
    * Remove `go get` in favour of cloning and reduces lines of bash
    
    [#158825168]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -5,10 +5,9 @@ ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
 setup_env "${KUBO_ENVIRONMENT_DIR}"
 
-go get -d github.com/cfcr/istio
-mkdir -p $GOPATH/src/istio.io
-ln -s $GOPATH/src/github.com/cfcr/istio $GOPATH/src/istio.io/istio
+git clone https://github.com/cfcr/istio $GOPATH/src/istio.io/istio
+
 cd $GOPATH/src/istio.io/istio
 git checkout 0.8.0
 ./bin/init_helm.sh
-make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm --use_automatic_injection'
+make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm --use-automatic-injection'
