987f645f5967017bacad38db9c220e75143b1bf4
Mark James Hender <mhender@pivotal.io>
2018-07-09 10:26:18 +0100

    Use automatic injection for side car
    So we are more sure that Istio works
    
    [#158825168]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -5,10 +5,10 @@ ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
 setup_env "${KUBO_ENVIRONMENT_DIR}"
 
-go get github.com/cfcr/istio || true # go get returns error "no Go files", which is expected
+go get -d github.com/cfcr/istio
 mkdir -p $GOPATH/src/istio.io
-mv $GOPATH/src/github.com/cfcr/istio $GOPATH/src/istio.io/istio
+ln -s $GOPATH/src/github.com/cfcr/istio $GOPATH/src/istio.io/istio
 cd $GOPATH/src/istio.io/istio
 git checkout 0.8.0
 ./bin/init_helm.sh
-make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm'
+make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm --use_automatic_injection'
