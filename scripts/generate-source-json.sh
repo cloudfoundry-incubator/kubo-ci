ca0b0e92f411f9b77082adb1384a21fc45c3e33c
Akshay Mankar <amankar@pivotal.io>
2018-07-09 13:20:11 +0100

    Get dynamic var file from gcs
    [#158294350]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

--- scripts/generate-source-json.sh
+++ scripts/generate-source-json.sh
@@ -4,12 +4,10 @@ target="https://$(bosh int kubo-lock/metadata --path=/internal_ip):25555"
 client="admin"
 client_secret="$(bosh int gcs-bosh-creds/creds.yml --path=/admin_password)"
 ca_cert="$(bosh int gcs-bosh-creds/creds.yml --path=/director_ssl/ca)"
-kubernetes_master_host="$(bosh int kubo-lock/metadata --path=/kubernetes_master_host)"
 
 jq -n \
-  --arg kubernetes_master_host "$kubernetes_master_host" \
   --arg target "$target" \
   --arg client "$client" \
   --arg client_secret "$client_secret" \
   --arg ca_cert "$ca_cert" \
-  '{"target": $target, "client": $client, "client_secret": $client_secret, "ca_cert": $ca_cert, "CI_kubernetes_master_host": $kubernetes_master_host}' > source-json/source.json
+  '{"target": $target, "client": $client, "client_secret": $client_secret, "ca_cert": $ca_cert' > source-json/source.json
