a461679a31109141e2bbb082b71864a01b61bcaa
Akshay Mankar <amankar@pivotal.io>
2018-07-09 11:10:04 +0100

    deploy bosh and generate source.json
    [#158294350]
    
    Signed-off-by: Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>

---
+++ scripts/generate-source-json.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+target="https://$(bosh int kubo-lock/metadata --path=/internal_ip):25555"
+client="admin"
+client_secret="$(bosh int gcs-bosh-creds/creds.yml --path=/admin_password)"
+ca_cert="$(bosh int gcs-bosh-creds/creds.yml --path=/director_tls/ca)"
+kubernetes_master_host="$(bosh int kubo-lock/metadata --path=/kubernetes_master_host)"
+
+jq -n \
+  --arg kubernetes_master_host "$kubernetes_master_host" \
+  --arg target "$target" \
+  --arg client "$client" \
+  --arg client_secret "$client_secret" \
+  --arg ca_cert "$ca_cert" \
+  '{"target": $target, "client": $client, "client_secret": $client_secret, "ca_cert": $ca_cert, "CI_kubernetes_master_host": $kubernetes_master_host}' > source-json/source.json
