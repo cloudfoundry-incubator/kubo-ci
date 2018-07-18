44748dbf0af940ba46b910ae958d43b2b54109f5
Oleksandr Slynko <oslynko@pivotal.io>
2018-07-18 17:43:45 +0100

    Get kubeconfig from external place
    [#158795870]

--- scripts/run-conformance-tests.sh
+++ scripts/run-conformance-tests.sh
@@ -5,26 +5,13 @@
 set -eu
 set -o pipefail
 
-
-generate_config() {
-  ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
-  KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
-  DEPLOYMENT_NAME="${DEPLOYMENT_NAME:="ci-service"}"
-
-  source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
-  setup_env "${KUBO_ENVIRONMENT_DIR}"
-
-}
-
 run_conformance_test() {
   ginkgo -p -progress -focus  "\[Conformance\]" -skip "\[Serial\]" /e2e.test
   ginkgo -focus="\[Serial\].*\[Conformance\]" /e2e.test
 }
 
 main() {
-  if [[ -z "${CONFIG+x}" ]]; then
-    generate_config
-  fi
+  cp gcs-kubeconfig/config $KUBECONFIG
 
   run_conformance_test
 }
