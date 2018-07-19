96d3ee5407e7e8069a379e7fa8180ca4d9e819bb
Carlo Colombo <ccolombo@pivotal.io>
2018-07-19 20:15:55 +0200

    including oidc auth provider

--- src/tests/integration-tests/generic/kubelet_test.go
+++ src/tests/integration-tests/generic/kubelet_test.go
@@ -12,6 +12,7 @@ import (
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
 var _ = Describe("Kubelet", func() {
