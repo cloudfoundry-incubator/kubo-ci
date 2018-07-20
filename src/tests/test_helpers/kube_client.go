cf6e115515d8ab6a7fba7df1bb01db4536688316
Oleksandr Slynko <oslynko@pivotal.io>
2018-07-20 10:40:01 +0100

    Add default auth plugins for client-go
    Fixes #25

--- src/tests/test_helpers/kube_client.go
+++ src/tests/test_helpers/kube_client.go
@@ -6,6 +6,7 @@ import (
 
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8s "k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth" // load oidc auth
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
