b4add710645280033cdbd8989c09c7ec184bd817
Iain Sproat <isproat@pivotal.io>
2018-07-26 09:49:53 +0100

    Use KUBECONFIG env var to set path to kube config
    [#158946553]
    
    Signed-off-by: Ben Chapman <bchapman@pivotal.io>

--- scripts/run-k8s-bbr-tests.sh
+++ scripts/run-k8s-bbr-tests.sh
@@ -14,10 +14,11 @@ main() {
   BOSH_CLIENT_SECRET="$(jq '.client_secret' "${ROOT}/gcs-source-json/source.json")"
   BOSH_CA_CERT="$(jq '.ca_cert' "${ROOT}/gcs-source-json/source.json")"
   BOSH_DEPLOYMENT="$DEPLOYMENT_NAME"
+  KUBECONFIG="${ROOT}/gcs-kubeconfig/k8s/config"
 
-  export BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT BOSH_DEPLOYMENT
+  export BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT BOSH_DEPLOYMENT KUBECONFIG
 
-  CONFIG="${ROOT}/gcs-kubeconfig/k8s/config" ginkgo -r -progress "${ROOT}/git-kubo-ci/src/tests/bbr-tests/"
+  ginkgo -r -progress "${ROOT}/git-kubo-ci/src/tests/bbr-tests/"
 }
 
 main
