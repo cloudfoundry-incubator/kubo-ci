b73db19371d48b65f067b468c20b6e3f67dc6a6f
Ben Chapman <bchapman@pivotal.io>
2018-07-26 10:05:29 +0100

    Add bbr-cli to bbr test jobs
    [#158946553]
    
    Signed-off-by: Iain Sproat <isproat@pivotal.io>

--- scripts/run-k8s-bbr-tests.sh
+++ scripts/run-k8s-bbr-tests.sh
@@ -9,12 +9,17 @@ DEPLOYMENT_NAME="${DEPLOYMENT_NAME:="ci-service"}"
 export GOPATH="${ROOT}/git-kubo-ci"
 
 main() {
+  pushd "${ROOT}/bbr-cli"
+    tar xvf bbr-*.tar
+  popd
+
   BOSH_ENVIRONMENT="$(jq '.target' "${ROOT}/gcs-source-json/source.json")"
   BOSH_CLIENT="$(jq '.client' "${ROOT}/gcs-source-json/source.json")"
   BOSH_CLIENT_SECRET="$(jq '.client_secret' "${ROOT}/gcs-source-json/source.json")"
   BOSH_CA_CERT="$(jq '.ca_cert' "${ROOT}/gcs-source-json/source.json")"
   BOSH_DEPLOYMENT="$DEPLOYMENT_NAME"
-  KUBECONFIG="${ROOT}/gcs-kubeconfig/k8s/config"
+  KUBECONFIG="${ROOT}/gcs-kubeconfig/config"
+  PATH="$PATH:${ROOT}/bbr-cli/releases/"
 
   export BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT BOSH_DEPLOYMENT KUBECONFIG
 
