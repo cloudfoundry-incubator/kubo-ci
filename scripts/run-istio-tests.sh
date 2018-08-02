f6b20187212c75a2f71c56b415b46548dcf5a661
Oleksandr Slynko <oslynko@pivotal.io>
2018-08-02 13:50:00 +0100

    Bump Istio to 1.0.0
    Signed-off-by: Iain Sproat <isproat@pivotal.io>

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -4,10 +4,10 @@ ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 
 source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
 setup_env "${KUBO_ENVIRONMENT_DIR}"
+ISTIO_VERSION=1.0.0
 
-git clone https://github.com/cfcr/istio $GOPATH/src/istio.io/istio
+git clone --depth 50 --branch $ISTIO_VERSION https://github.com/istio/istio $GOPATH/src/istio.io/istio
 
 cd $GOPATH/src/istio.io/istio
-git checkout 0.8.0
 ./bin/init_helm.sh
-make e2e_simple TAG='0.8.0' E2E_ARGS='--installer=helm --use-automatic-injection'
+make e2e_simple TAG=${ISTIO_VERSION} E2E_ARGS='--installer=helm --use-automatic-injection'
