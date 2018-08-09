344e8fe32744a0a85e7fab54c03fb4eaa03f6330
Oleksandr Slynko <oslynko@pivotal.io>
2018-08-09 12:41:21 +0100

    Install Istio outside of the tests
    [#158294350]

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -9,5 +9,16 @@ ISTIO_VERSION=1.0.0
 git clone --depth 50 --branch $ISTIO_VERSION https://github.com/istio/istio $GOPATH/src/istio.io/istio
 
 cd $GOPATH/src/istio.io/istio
+
+# init helm
 ./bin/init_helm.sh
-make e2e_simple TAG=${ISTIO_VERSION} E2E_ARGS='--installer=helm --use-automatic-injection'
+kubectl apply -f install/kubernetes/helm/helm-service-account.yaml
+helm init --service-account tiller
+kubectl rollout status -w -n kube-system deployment.apps/tiller-deploy
+
+# install Istio
+trap "helm del --purge istio-system || kubectl delete namespace istio-system" 0 1 2 3 15
+helm install install/kubernetes/helm/istio --name istio-system --namespace istio-system --set global.mtls.enabled=true \
+  --set sidecarInjectorWebhook.enabled=true --set global.hub=istio --set global.tag=$ISTIO_VERSION --set global.crds=false --debug
+
+make e2e_simple TAG=${ISTIO_VERSION} E2E_ARGS='--installer=helm --skip_setup'
