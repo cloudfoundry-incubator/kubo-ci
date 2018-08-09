c1e2f23974249361b1433167c218cdd347ba8b74
Oleksandr Slynko <oslynko@pivotal.io>
2018-08-09 17:12:20 +0100

    Fix environment variable for cleanup

--- scripts/cleanup-environment.sh
+++ scripts/cleanup-environment.sh
@@ -18,7 +18,7 @@ login_gcp() {
 delete_gcloud_vms() {
   subnetwork=$(bosh int "${ENV_FILE}" --path='/subnetwork')
   subnetLink=$(gcloud compute networks subnets list --filter name="$subnetwork" --format=json | bosh int - --path=/0/selfLink)
-  vms=$(gcloud  compute instances list --filter="networkInterfaces.subnetwork=$subnet" --format="table(name,zone)[no-heading]")
+  vms=$(gcloud  compute instances list --filter="networkInterfaces.subnetwork=$subnetLink" --format="table(name,zone)[no-heading]")
 
   IFS=$'\n'
 
