9ef3c8493d81360bf84bb7953bc18ff137452e80
Urvashi Reddy <ureddy@pivotal.io>
2018-08-10 16:28:58 -0700

    Remove heapster scheme from top tests
    - Since we default metrics server, we don't need top to rely on heapster
    anymore
    
    [#157915809]
    
    Signed-off-by: Travis Hall <thall@vmware.com>

--- src/tests/integration-tests/generic/kubectl_test.go
+++ src/tests/integration-tests/generic/kubectl_test.go
@@ -43,13 +43,13 @@ var _ = Describe("Kubectl", func() {
 
 	It("Should be able to run kubectl top pod successfully", func() {
 		Eventually(func() int {
-			return kubectl.RunKubectlCommand("top", "pods", "--heapster-scheme=https", "-n", "kube-system").Wait(30 * time.Second).ExitCode()
+			return kubectl.RunKubectlCommand("top", "pods", "-n", "kube-system").Wait(30 * time.Second).ExitCode()
 		}, "300s", "10s").Should(Equal(0))
 	})
 
 	It("Should be able to run kubectl top nodes successfully", func() {
 		Eventually(func() int {
-			return kubectl.RunKubectlCommand("top", "nodes", "--heapster-scheme=https").Wait(30 * time.Second).ExitCode()
+			return kubectl.RunKubectlCommand("top", "nodes").Wait(30 * time.Second).ExitCode()
 		}, "300s", "10s").Should(Equal(0))
 	})
 
