7f73c1a4a4162c2a0cd51c9439edfc9054614c2d
Ben Chapman <bchapman@pivotal.io>
2018-08-29 17:15:23 +0100

    Extract bumping logic to kubo-release
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/bump-k8s.sh
+++ scripts/bump-k8s.sh
@@ -13,11 +13,11 @@ pr_kubo_ci() {
 
   existing_k8s_version="$(grep "ENV KUBE_VERSION" "$docker_file" | sed 's/.*"v\(.*\).*"/\1/g')"
 
-  if [ $existing_k8s_version == $version ]; then
+  if [ "$existing_k8s_version" == "$version" ]; then
       echo "Kubernetes version already up-to-date."
   else
       sed -i "s/ENV KUBE_VERSION=.*/ENV KUBE_VERSION=\"v${version}\"/g" "$docker_file"
-      generate_pull_request "k8s_in_conformance" $tag "kubo-ci"
+      generate_pull_request "k8s_in_conformance" "$tag" "kubo-ci"
   fi
 
   popd
@@ -26,31 +26,21 @@ pr_kubo_ci() {
 pr_kubo_release() {
   version="$1"
   tag="$2"
-  name="kubernetes-${version}"
 
   cp -r git-kubo-release/. git-kubo-release-output
   pushd git-kubo-release-output
 
-  existing_k8s_spec=$(bosh blobs --column path | grep kubelet | grep -o -E 'kubernetes-([0-9]+\.)+[0-9]+')
-  existing_k8s_version=$(echo $existing_k8s_spec | grep -o -E '([0-9]+\.)+[0-9]+')
+  ./scripts/download_k8s_binaries $version
 
-  if [ $existing_k8s_version == $version ]; then
-      echo "Kubernetes version already up-to-date."
-  else
-      ./scripts/download_k8s_binaries $version
-      pushd packages/kubernetes
-      sed -E -i -e "s/([0-9]+\.)+[0-9]+/${version}/" packaging
-      sed -E -i -e "s/${existing_k8s_spec}/${name}/" spec
-      popd
-
-      cat <<EOF > "config/private.yml"
+  if ! git diff-index --quiet HEAD --; then
+    cat <<EOF > "config/private.yml"
 blobstore:
   options:
     access_key_id: ${ACCESS_KEY_ID}
     secret_access_key: ${SECRET_ACCESS_KEY}
 EOF
-      bosh upload-blobs
-      generate_pull_request "kubernetes" $tag "kubo-release"
+    bosh upload-blobs
+    generate_pull_request "kubernetes" "$tag" "kubo-release"
   fi
 
   popd
