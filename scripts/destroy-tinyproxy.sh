4df66eb55e8e381b9d822862825e495e8837b8a5
Ben Chapman <bchapman@pivotal.io>
2018-09-06 09:29:37 +0100

    Move deploy utils script for destroy tinyproxy job
    [#159687609]

--- scripts/destroy-tinyproxy.sh
+++ scripts/destroy-tinyproxy.sh
@@ -11,7 +11,7 @@ export DEBUG=1
 cp "$PWD/kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
 cp "$PWD/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
 
-. "git-kubo-deployment/bin/lib/deploy_utils"
+. "git-kubo-ci/scripts/lib/deploy_utils"
 . "$PWD/git-kubo-deployment/bin/set_bosh_environment"
 
 BOSH_CLIENT=bosh_admin BOSH_CLIENT_SECRET="$(get_bosh_secret)" bosh -n -e "${BOSH_ENVIRONMENT}" delete-deployment -d "tinyproxy"
