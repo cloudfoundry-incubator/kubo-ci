9868009614c0d153304c5bf2996259d048bbe9a2
Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>
2018-09-17 14:59:04 +0100

    Readd upload-stemcell script
    Signed-off-by: Ben Chapman <bchapman@pivotal.io>

---
+++ scripts/upload-stemcell.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
+KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
+
+mkdir -p "${KUBO_ENVIRONMENT_DIR}"
+cp "${ROOT}/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
+cp "${ROOT}/kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
+
+BOSH_ENV="${KUBO_ENVIRONMENT_DIR}" source "${ROOT}/git-kubo-deployment/bin/set_bosh_environment"
+stemcell_version="$(bosh int --path=/stemcells/0/version $ROOT/git-kubo-deployment/manifests/cfcr.yml)"
+stemcell_line="$(bosh int --path=/stemcells/0/os $ROOT/git-kubo-deployment/manifests/cfcr.yml)"
+
+bosh upload-stemcell "https://boshstemcells.com/${IAAS}/${stemcell_line}/${stemcell_version}"
