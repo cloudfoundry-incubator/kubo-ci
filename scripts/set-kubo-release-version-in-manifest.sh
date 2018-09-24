12d57413bd544cf2272810aaf3efac9f2520911c
Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>
2018-09-24 13:42:43 +0100

    Create commit when update kubo-release in manifest
    [#155998950]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/set-kubo-release-version-in-manifest.sh
+++ scripts/set-kubo-release-version-in-manifest.sh
@@ -17,3 +17,12 @@ version="$(cat kubo-version/version)"
 sha="$(shasum kubo-release-tarball/kubo-release-${version}.tgz | cut -d ' ' -f 1)"
 url="https://github.com/cloudfoundry-incubator/kubo-release/releases/download/v${version}/kubo-release-${version}.tgz"
 bosh int git-kubo-deployment/manifests/cfcr.yml -o replace-kubo-version.yml -v version="$version" -v sha="$sha" -v url="$url" > git-kubo-deployment-output/manifests/cfcr.yml
+
+
+git config --global user.name "cfcr"
+git config --global user.email "cfcr@pivotal.io"
+cd git-kubo-deployment-output
+
+git add .
+git commit -m "Final release for v${version}"
+git tag -a "v${version}" -m "Tag for version v${version}"
