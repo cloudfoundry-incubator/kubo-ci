45d8766253929d108eec2f82fc8ce5a55ae7f8b0
Oleksandr Slynko <oslynko@pivotal.io>
2018-09-27 15:43:53 +0100

    Remove director.yml dependency from istio tests
    [#158871479]

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -2,8 +2,7 @@
 set -euox pipefail
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 
-source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
-setup_env "${KUBO_ENVIRONMENT_DIR}"
+export KUBECONFIG=$ROOT/gcs-kubeconfig/config
 ISTIO_VERSION=1.1.0.snapshot.0
 
 git clone --depth 50 --branch $ISTIO_VERSION https://github.com/istio/istio $GOPATH/src/istio.io/istio
