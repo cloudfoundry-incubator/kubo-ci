fb7c77c5dfce7a5a52d499b291ba3e4e119964df
Oleksandr Slynko <oslynko@pivotal.io>
2018-11-05 11:27:32 +0000

    Bump kubectl in main integration test as well
    [#161715382]

--- scripts/bump-k8s.sh
+++ scripts/bump-k8s.sh
@@ -6,20 +6,28 @@ source git-kubo-ci/scripts/lib/generate-pr.sh
 pr_kubo_ci() {
   version="$1"
   tag="$2"
-  docker_file="docker-images/conformance/Dockerfile"
-
   cp -r git-kubo-ci/. git-kubo-ci-output
   pushd git-kubo-ci-output
 
-  existing_k8s_version="$(grep "ENV KUBE_VERSION" "$docker_file" | sed 's/.*"v\(.*\).*"/\1/g')"
+  docker_conformance_file="docker-images/conformance/Dockerfile"
+  existing_conformance_k8s_version="$(grep "ENV KUBE_VERSION" "$docker_conformance_file" | sed 's/.*"v\(.*\).*"/\1/g')"
 
-  if [ "$existing_k8s_version" == "$version" ]; then
-      echo "Kubernetes version already up-to-date."
-  else
-      sed -i "s/ENV KUBE_VERSION=.*/ENV KUBE_VERSION=\"v${version}\"/g" "$docker_file"
-      generate_pull_request "k8s_in_conformance" "$tag" "kubo-ci"
+  if [ "$existing_conformance_k8s_version" != "$version" ]; then
+      sed -i "s/ENV KUBE_VERSION=.*/ENV KUBE_VERSION=\"v${version}\"/g" "$docker_conformance_file"
   fi
 
+  docker_ci_file="docker-images/kubo-ci/Dockerfile"
+  existing_ci_k8s_version="$(grep "ENV KUBE_VERSION" "$docker_ci_file" | sed 's/.*"v\(.*\).*"/\1/g')"
+
+  if [ "$existing_ci_k8s_version" != "$version" ]; then
+      sed -i "s/ENV KUBE_VERSION=.*/ENV KUBE_VERSION=\"v${version}\"/g" "$docker_ci_file"
+  fi
+
+  if [ "$existing_conformance_k8s_version" == "$version" ] && [ "$existing_ci_k8s_version" == "$version" ]; then
+      echo "Kubernetes version already up-to-date."
+  else
+      generate_pull_request "k8s" "$tag" "kubo-ci"
+  fi
   popd
 }
 
