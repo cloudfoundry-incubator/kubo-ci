5cf609a150e080a444d282d8ab1f9175a7f00a3f
Ben Moss <bmoss@pivotal.io>
2018-11-12 11:33:17 -0500

    Only run Windows tests when a Windows worker is detected

--- src/tests/integration-tests/windows/windows_suite_test.go
+++ src/tests/integration-tests/windows/windows_suite_test.go
@@ -1,11 +1,20 @@
 package windows_test
 
 import (
+	"testing"
+	"tests/test_helpers"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"testing"
 )
+
+var _ = BeforeSuite(func() {
+	hasWindowsWorkers, err := test_helpers.HasWindowsWorkers()
+	Expect(err).To(BeNil())
+	if !hasWindowsWorkers {
+		Skip("skipping Windows tests since no Windows nodes were detected")
+	}
+})
 
 func TestWindows(t *testing.T) {
 	RegisterFailHandler(Fail)
