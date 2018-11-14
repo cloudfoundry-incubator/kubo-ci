6088e8b040365a33b0a1158c2a38b191f020ca67
Travis Hall <thall@vmware.com>
2018-11-14 15:45:40 -0800

    bump-bosh-release script doesn't fail if no commit
    - The script now checks if there's changes to be commited, making it
    easier for debugging
    - Also fix the script to be not hardcoded for the cfcr-etcd release url
    
    [#161875482]
    
    Signed-off-by: Urvashi Reddy <ureddy@pivotal.io>

--- scripts/bump-boshrelease.sh
+++ scripts/bump-boshrelease.sh
@@ -7,10 +7,10 @@ cp -r git-kubo-deployment/. git-kubo-deployment-output
 release="${RELEASE_NAME}"
 version="$(cat boshrelease/version)"
 
-if [ -z ${REPO_URL+x} ]; then
+if [ -z ${REPO_URL} ]; then
  url="$(cat boshrelease/url)"
 else
- url="${REPO_URL}/releases/download/v${version}/cfcr-etcd-release-${version}.tgz"
+ url="${REPO_URL}/releases/download/v${version}/${release}-release-${version}.tgz"
 fi
 
 if [ -f "boshrelease/sha1" ]; then
@@ -35,5 +35,12 @@ bosh int git-kubo-deployment/manifests/cfcr.yml \
 pushd git-kubo-deployment-output
 git config --global user.email "cfcr+cibot@pivotal.io"
 git config --global user.name "CFCR CI BOT"
-git add manifests/cfcr.yml
-git commit -m "Bump $release to version $version"
+
+git diff-index --quiet HEAD
+exit_status=$?
+if [ $exit_status -eq 0 ]; then
+    git add manifests/cfcr.yml
+    git commit -m "Bump $release to version $version"
+else
+    echo "No changes to commit."
+fi
