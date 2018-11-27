f30308a4b8ad432feaa5b77585b014a0f606931a
Oleksandr Slynko <oslynko@pivotal.io>
2018-11-27 16:59:35 +0000

    Get rid of director.yml in precompile pipeline
    [#159895436]

--- scripts/export-release.sh
+++ scripts/export-release.sh
@@ -2,24 +2,17 @@
 
 set -eu
 
-ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
-KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
+source git-kubo-ci/scripts/set-bosh-env gcs-source-json/source.json
 
-mkdir -p "${KUBO_ENVIRONMENT_DIR}"
-cp "${ROOT}/gaffer-director-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
-cp "${ROOT}/gaffer-director-metadata/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
-
-BOSH_ENV="${KUBO_ENVIRONMENT_DIR}" source "${ROOT}/git-kubo-ci/scripts/set_bosh_environment"
-
-STEMCELL_OS=$(bosh int ${ROOT}/git-kubo-deployment/manifests/cfcr.yml --path=/stemcells/0/os)
-STEMCELL_VERSION=$(bosh int ${ROOT}/git-kubo-deployment/manifests/cfcr.yml --path=/stemcells/0/version)
+STEMCELL_OS=$(bosh int git-kubo-deployment/manifests/cfcr.yml --path=/stemcells/0/os)
+STEMCELL_VERSION=$(bosh int git-kubo-deployment/manifests/cfcr.yml --path=/stemcells/0/version)
 
 for release in $RELEASE_LIST
 do
   if [[ "$release" == "kubo" ]]; then
     RELEASE_VERSION="$(cat kubo-version/version)"
   else
-    RELEASE_VERSION="$(bosh int ${ROOT}/git-kubo-deployment/manifests/cfcr.yml --path=/releases/name=${release}/version)"
+    RELEASE_VERSION="$(bosh int git-kubo-deployment/manifests/cfcr.yml --path=/releases/name=${release}/version)"
   fi
   bosh -d compilation export-release "$release/$RELEASE_VERSION" "$STEMCELL_OS/$STEMCELL_VERSION"
 
