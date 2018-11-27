204407f6cf36441e30ed1e8ea787e4a68dc05b63
Oleksandr Slynko <oslynko@pivotal.io>
2018-11-27 15:01:49 +0000

    Use source.json to upload stemcells if available

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -2,15 +2,17 @@
 
 set -eu
 
-ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
-KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
+if [ -f gcs-source-json/source.json ]; then
+    source git-kubo-ci/scripts/set-bosh-env gcs-source-json/source.json
+else
+    KUBO_ENVIRONMENT_DIR="environment"
 
-mkdir -p "${KUBO_ENVIRONMENT_DIR}"
-cp "${ROOT}/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
-cp "${ROOT}/kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
-
-BOSH_ENV="${KUBO_ENVIRONMENT_DIR}" source "${ROOT}/git-kubo-ci/scripts/set_bosh_environment"
-stemcell_version="$(bosh int --path=/stemcells/0/version $ROOT/git-kubo-deployment/manifests/cfcr.yml)"
-stemcell_line="$(bosh int --path=/stemcells/0/os $ROOT/git-kubo-deployment/manifests/cfcr.yml)"
+    mkdir -p "${KUBO_ENVIRONMENT_DIR}"
+    cp "gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
+    cp "kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
+    BOSH_ENV="${KUBO_ENVIRONMENT_DIR}" source "git-kubo-ci/scripts/set_bosh_environment"
+fi
+stemcell_version="$(bosh int --path=/stemcells/0/version git-kubo-deployment/manifests/cfcr.yml)"
+stemcell_line="$(bosh int --path=/stemcells/0/os git-kubo-deployment/manifests/cfcr.yml)"
 
 bosh upload-stemcell "https://boshstemcells.com/${IAAS}/${stemcell_line}/${stemcell_version}"
