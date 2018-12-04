a41a68c44a5fbf8a18e2ad6520ed5b10ae0535d5
Urvashi Reddy <ureddy@pivotal.io>
2018-12-04 15:42:04 -0800

    Remove the need for generate-test-config
    [#160794497]

--- scripts/run-k8s-upgrade-tests.sh
+++ scripts/run-k8s-upgrade-tests.sh
@@ -2,15 +2,12 @@
 
 set -eu -o pipefail
 
-ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
-
-export DEPLOYMENT_NAME="${DEPLOYMENT_NAME:="ci-service"}"
-KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
-
+export ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
+export KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
 export GOPATH="${ROOT}/git-kubo-ci"
 
 main() {
-  local tmpfile release_tarball
+  local release_tarball
 
   source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
   setup_env "${KUBO_ENVIRONMENT_DIR}"
@@ -18,13 +15,9 @@ main() {
   BOSH_ENV="${KUBO_ENVIRONMENT_DIR}" source "${ROOT}/git-kubo-ci/scripts/set_bosh_environment"
 
   release_tarball=$(find "${ROOT}/gcs-kubo-release-tarball/" -name "*kubo-*.tgz" | head -n1)
-
   bosh upload-release "$release_tarball"
 
   if [[ "$INTERNET_ACCESS" != "false" ]]; then
-    tmpfile="$(mktemp)"
-
-    "${ROOT}/git-kubo-ci/scripts/generate-test-config.sh" "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}" > "${tmpfile}"
     BOSH_DEPLOY_COMMAND="$ROOT/bosh-command/bosh-deploy.sh" ginkgo -r -v -progress "${ROOT}/git-kubo-ci/src/tests/upgrade-tests/"
   else
     $ROOT/bosh-command/bosh-deploy.sh
