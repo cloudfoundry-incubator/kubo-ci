c26a9c1da6a8d6da5c7563de498d287b4b1019ab
Iain Sproat <isproat@pivotal.io>
2018-12-05 17:55:12 +0000

    Rename gcs-source-json
    [#162409527]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -eu
-if [[ -f gcs-source-json/source.json ]]; then
-    source git-kubo-ci/scripts/set-bosh-env gcs-source-json/source.json
+if [[ -f source-json/source.json ]]; then
+    source git-kubo-ci/scripts/set-bosh-env source-json/source.json
 else
-    source git-kubo-ci/scripts/set-bosh-env gcs-source-json/metadata
+    source git-kubo-ci/scripts/set-bosh-env source-json/metadata
 fi
 
 stemcell_version="$(bosh int --path=/stemcells/0/version git-kubo-deployment/manifests/cfcr.yml)"
