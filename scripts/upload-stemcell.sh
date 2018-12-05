3c40daf873158e3dc08de3916ec99f4329708d21
Iain Sproat <isproat@pivotal.io>
2018-12-05 16:20:38 +0000

    Export BOSH_ALL_PROXY in set-bosh-env
    [#162409527]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -5,12 +5,8 @@ if [[ -f gcs-source-json/source.json ]]; then
     source git-kubo-ci/scripts/set-bosh-env gcs-source-json/source.json
 else
     source git-kubo-ci/scripts/set-bosh-env gcs-source-json/metadata
-    jumpbox_ssh_key="$(mktemp)"
-    bosh int --path=/jumpbox_ssh_key gcs-source-json/metadata > ${jumpbox_ssh_key}
-    proxy="ssh+socks5://jumpbox@$(bosh int --path=/jumpbox_url gcs-source-json/metadata)?private-key=${jumpbox_ssh_key}"
-    export BOSH_ALL_PROXY="${proxy}"
-    export CREDHUB_ALL_PROXY="${proxy}"
 fi
+
 stemcell_version="$(bosh int --path=/stemcells/0/version git-kubo-deployment/manifests/cfcr.yml)"
 stemcell_line="$(bosh int --path=/stemcells/0/os git-kubo-deployment/manifests/cfcr.yml)"
 
