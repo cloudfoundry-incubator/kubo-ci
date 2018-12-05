43ea3ead6c7c389d2bf2026488a3a77c5ccf0ad5
Iain Sproat <isproat@pivotal.io>
2018-12-05 15:05:23 +0000

    Upload stemcell will only use source.json
    [#162409527]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -2,16 +2,7 @@
 
 set -eu
 
-if [ -f gcs-source-json/source.json ]; then
-    source git-kubo-ci/scripts/set-bosh-env gcs-source-json/source.json
-else
-    KUBO_ENVIRONMENT_DIR="environment"
-
-    mkdir -p "${KUBO_ENVIRONMENT_DIR}"
-    cp "gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
-    cp "kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
-    BOSH_ENV="${KUBO_ENVIRONMENT_DIR}" source "git-kubo-ci/scripts/set_bosh_environment"
-fi
+source git-kubo-ci/scripts/set-bosh-env gcs-source-json/source.json
 stemcell_version="$(bosh int --path=/stemcells/0/version git-kubo-deployment/manifests/cfcr.yml)"
 stemcell_line="$(bosh int --path=/stemcells/0/os git-kubo-deployment/manifests/cfcr.yml)"
 
