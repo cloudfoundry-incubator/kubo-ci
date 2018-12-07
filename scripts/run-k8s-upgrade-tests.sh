baadd56ca10cf687f21b9a647f937a0edc78ecfc
Urvashi Reddy <ureddy@pivotal.io>
2018-12-07 10:46:32 -0800

    Add missing env vars for upgrade test
    - Remove set_bosh_environment since the same env vars are being set with
    source-json

--- scripts/run-k8s-upgrade-tests.sh
+++ scripts/run-k8s-upgrade-tests.sh
@@ -6,15 +6,21 @@ export ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 export KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
 export GOPATH="${ROOT}/git-kubo-ci"
 
+target_bosh_director() {
+  BOSH_DEPLOYMENT="${DEPLOYMENT_NAME}"
+  BOSH_ENVIRONMENT=$(bosh int source-json/source.json --path '/target')
+  BOSH_CLIENT=$(bosh int source-json/source.json --path '/client')
+  BOSH_CLIENT_SECRET=$(bosh int source-json/source.json --path '/client_secret')
+  BOSH_CA_CERT=$(bosh int source-json/source.json --path '/ca_cert')
+  export BOSH_DEPLOYMENT BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT
+}
+
 main() {
+  target_bosh_director
+
   local release_tarball
-
-  source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
-  setup_env "${KUBO_ENVIRONMENT_DIR}"
-
-  BOSH_ENV="${KUBO_ENVIRONMENT_DIR}" source "${ROOT}/git-kubo-ci/scripts/set_bosh_environment"
-
   release_tarball=$(find "${ROOT}/gcs-kubo-release-tarball/" -name "*kubo-*.tgz" | head -n1)
+
   bosh upload-release "$release_tarball"
 
   if [[ "$INTERNET_ACCESS" != "false" ]]; then
