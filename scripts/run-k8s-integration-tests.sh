7cf25cbad5dcb771e73c9a1859bbddbc8968a281
Mark James Hender <mhender@pivotal.io>
2018-12-10 11:58:53 +0000

    Make ssh session live longer
    [#162409527]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- scripts/run-k8s-integration-tests.sh
+++ scripts/run-k8s-integration-tests.sh
@@ -17,7 +17,7 @@ main() {
     chmod 0600 ssh.key
     cidr="$(bosh int kubo-lock/metadata --path=/internal_cidr)"
     jumpbox_url="$(bosh int kubo-lock/metadata --path=/jumpbox_url)"
-    sshuttle -r "jumpbox@${jumpbox_url}" "${cidr}" -e "ssh -i ssh.key -o StrictHostKeyChecking=no -o ServerAliveInterval=10" --daemon
+    sshuttle -r "jumpbox@${jumpbox_url}" "${cidr}" -e "ssh -i ssh.key -o StrictHostKeyChecking=no -o ServerAliveInterval=300 -o ServerAliveCountMax=10" --daemon
     trap 'kill -9 $(cat sshuttle.pid)' EXIT
   fi
   mkdir -p ~/.kube
