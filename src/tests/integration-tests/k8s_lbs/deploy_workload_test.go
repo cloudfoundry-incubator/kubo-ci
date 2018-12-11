ab061e327acd03bc55dd36376f883115f2fb24d1
Neil Hickey <nhickey@pivotal.io>
2018-12-11 10:36:14 +0000

    Increase timeout for LB creation for nginx
    [#162196975]
    
    Signed-off-by: Nikita Rathi <nrathi@pivotal.io>

--- src/tests/integration-tests/k8s_lbs/deploy_workload_test.go
+++ src/tests/integration-tests/k8s_lbs/deploy_workload_test.go
@@ -22,7 +22,7 @@ var _ = Describe("Deploy workload", func() {
 		Eventually(func() string {
 			loadbalancerAddress = kubectl.GetLBAddress("nginx", iaas)
 			return loadbalancerAddress
-		}, "240s", "5s").Should(Not(Equal("")))
+		}, "600s", "5s").Should(Not(Equal("")))
 
 		appUrl := fmt.Sprintf("http://%s", loadbalancerAddress)
 
