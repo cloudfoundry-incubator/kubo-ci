0f24272c09f7ac46ece88a4cf7b6e236162b136d
Oleksandr Slynko <oslynko@pivotal.io>
2018-12-13 11:24:34 +0000

    Ensure cleanup of k8s resources
    [#162341980]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/destroy-k8s-instance-long-lived-director.sh
+++ scripts/destroy-k8s-instance-long-lived-director.sh
@@ -6,11 +6,16 @@ main() {
     ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
     source "${ROOT}/git-kubo-ci/scripts/set-bosh-env" "${VARFILE}"
     deployment_name="$(bosh int "${VARFILE}" --path "/deployment_name")"
+    source ${ROOT}/git-kubo-ci/scripts/credhub-login "${VARFILE}"
+    ${ROOT}/git-kubo-ci/scripts/set_kubeconfig_long_lived_director
+
+    kubectl delete ns --all
+    kubectl delete pvc --all
+    kubectl delete pv --all
+    kubectl delete svc --all
 
     set +x
     bosh -d "${deployment_name}" -n delete-deployment --force
-
-    source ${ROOT}/git-kubo-ci/scripts/credhub-login "${VARFILE}"
 
     # don't delete leading & trailing slash. This is to scope to the deployment creds we want to delete
     credhub find -n "/${deployment_name}/" --output-json | jq -r .credentials[].name | xargs -L 1 credhub delete -n
