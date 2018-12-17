5711cb6c784cc2a5f8e3cb618d2799288352c760
Mark James Hender <mhender@pivotal.io>
2018-12-17 12:25:35 +0000

    Add task to deploy turbulence
    [#162636811]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

---
+++ scripts/deploy-turbulence.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BOSH_DEPLOYMENT="${DEPLOYMENT_NAME}"
+BOSH_ENVIRONMENT=$(bosh int source-json/source.json --path '/target')
+BOSH_CLIENT=$(bosh int source-json/source.json --path '/client')
+BOSH_CLIENT_SECRET=$(bosh int source-json/source.json --path '/client_secret')
+BOSH_CA_CERT=$(bosh int source-json/source.json --path '/ca_cert')
+export BOSH_DEPLOYMENT BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT
+
+bosh -n -d turbulence deploy ./git-turbulence-release/manifests/example.yml \
+  -v turbulence_api_ip="10.100.0.10" \
+  -v director_ip=$(bosh int "${ROOT}/kubo-lock/metadata" --path=/internal_ip) \
+  -v director_client=${BOSH_CLIENT} \
+  -v director_client_secret=${BOSH_CLIENT_SECRET} \
+  --vars-store /tmp/turbulence.yml
