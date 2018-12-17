98182e5d32855dd4f182f4f4d990c09f02410bea
Morena De Liddo <mdeliddo@pivotal.io>
2018-12-17 14:54:24 +0000

    Open sshuttle for turbulence tests
    [#162636811]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/run-k8s-turbulence-tests.sh
+++ scripts/run-k8s-turbulence-tests.sh
@@ -63,6 +63,15 @@ main() {
   target_bosh_director
   target_turbulence_api
 
+  if bosh int kubo-lock/metadata --path=/jumpbox_ssh_key &>/dev/null ; then
+    bosh int kubo-lock/metadata --path=/jumpbox_ssh_key > ssh.key
+    chmod 0600 ssh.key
+    cidr="$(bosh int kubo-lock/metadata --path=/internal_cidr)"
+    jumpbox_url="$(bosh int kubo-lock/metadata --path=/jumpbox_url)"
+    sshuttle -r "jumpbox@${jumpbox_url}" "${cidr}" -e "ssh -i ssh.key -o StrictHostKeyChecking=no -o ServerAliveInterval=300 -o ServerAliveCountMax=10" --daemon
+    trap 'kill -9 $(cat sshuttle.pid)' EXIT
+  fi
+
   ginkgo -skipPackage "${skipped_packages}" -failFast -progress -r "${ROOT}/git-kubo-ci/src/tests/turbulence-tests/"
 }
 
