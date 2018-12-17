8d5668f0daf0bc558311917fa22da4867517a790
Morena De Liddo <mdeliddo@pivotal.io>
2018-12-17 14:18:55 +0000

    Use metadata instead of source.json
    [#162636811]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/deploy-turbulence.sh
+++ scripts/deploy-turbulence.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 BOSH_DEPLOYMENT="${DEPLOYMENT_NAME}"
-BOSH_ENVIRONMENT=$(bosh int source-json/source.json --path '/target')
-BOSH_CLIENT=$(bosh int source-json/source.json --path '/client')
-BOSH_CLIENT_SECRET=$(bosh int source-json/source.json --path '/client_secret')
-BOSH_CA_CERT=$(bosh int source-json/source.json --path '/ca_cert')
+BOSH_ENVIRONMENT=$(bosh int kubo-lock/metadata --path '/target')
+BOSH_CLIENT=$(bosh int kubo-lock/metadata --path '/client')
+BOSH_CLIENT_SECRET=$(bosh int kubo-lock/metadata --path '/client_secret')
+BOSH_CA_CERT=$(bosh int kubo-lock/metadata --path '/ca_cert')
 export BOSH_DEPLOYMENT BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT
 
 bosh -n -d turbulence deploy ./git-turbulence-release/manifests/example.yml \
