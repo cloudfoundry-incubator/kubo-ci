5cb2de85b9a08761da8a031e6efb5f6b4fbff07f
Mark James Hender <mhender@pivotal.io>
2018-12-17 16:30:34 +0000

    Use xenial stemcell with turbulence deployment
    [#162636811]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- scripts/deploy-turbulence.sh
+++ scripts/deploy-turbulence.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+bosh int kubo-lock/metadata --path=/jumpbox_ssh_key > ssh.key
+chmod 0600 ssh.key
+cidr="$(bosh int kubo-lock/metadata --path=/internal_cidr)"
+jumpbox_url="$(bosh int kubo-lock/metadata --path=/jumpbox_url)"
+sshuttle -r "jumpbox@${jumpbox_url}" "${cidr}" -e "ssh -i ssh.key -o StrictHostKeyChecking=no -o ServerAliveInterval=300 -o ServerAliveCountMax=10" --daemon
+trap 'kill -9 $(cat sshuttle.pid)' EXIT
 
 BOSH_DEPLOYMENT="${DEPLOYMENT_NAME}"
 BOSH_ENVIRONMENT=$(bosh int kubo-lock/metadata --path '/target')
@@ -7,14 +13,9 @@ BOSH_CLIENT_SECRET=$(bosh int kubo-lock/metadata --path '/client_secret')
 BOSH_CA_CERT=$(bosh int kubo-lock/metadata --path '/ca_cert')
 export BOSH_DEPLOYMENT BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT
 
-bosh int kubo-lock/metadata --path=/jumpbox_ssh_key > ssh.key
-chmod 0600 ssh.key
-cidr="$(bosh int kubo-lock/metadata --path=/internal_cidr)"
-jumpbox_url="$(bosh int kubo-lock/metadata --path=/jumpbox_url)"
-sshuttle -r "jumpbox@${jumpbox_url}" "${cidr}" -e "ssh -i ssh.key -o StrictHostKeyChecking=no -o ServerAliveInterval=300 -o ServerAliveCountMax=10" --daemon
-trap 'kill -9 $(cat sshuttle.pid)' EXIT
-
 bosh -n -d turbulence deploy ./git-turbulence-release/manifests/example.yml \
+  -o git-kubo-ci/manifests/ops-files/use-dev-turbulence.yml \
+  -o git-kubo-ci/manifests/ops-files/use-xenial-stemcell.yml \
   -v turbulence_api_ip="10.100.0.10" \
   -v director_ip=$(bosh int "kubo-lock/metadata" --path=/internal_ip) \
   -v director_client=${BOSH_CLIENT} \
