03c54fd736065a738b2509305e5ae4eb0c687722
Fabio Mangione <fmangione@pivotal.io>
2018-12-19 12:01:09 +0000

    Wrap all credhub activity in conditional block
    * Deployment could be already deleted, broken, never deployed
    * Check in credhub first for any secrets belonging to deployment
    
    [#162636811]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/destroy-k8s-instance-long-lived-director.sh
+++ scripts/destroy-k8s-instance-long-lived-director.sh
@@ -7,23 +7,21 @@ main() {
     source "${ROOT}/git-kubo-ci/scripts/set-bosh-env" "${VARFILE}"
     deployment_name="$(bosh int "${VARFILE}" --path "/deployment_name")"
     source ${ROOT}/git-kubo-ci/scripts/credhub-login "${VARFILE}"
-    ${ROOT}/git-kubo-ci/scripts/set_kubeconfig_long_lived_director
 
     # Deployment might be deleted already or broken
-    set +e
-    kubectl delete ns --all
-    kubectl delete pvc --all
-    kubectl delete pv --all
-    kubectl delete svc --all
-    set -e
-
-    set +x
-    bosh -d "${deployment_name}" -n delete-deployment --force
-
-    # don't delete leading & trailing slash. This is to scope to the deployment creds we want to delete
     if credhub find -n "/${deployment_name}/"; then
-        credhub find -n "/${deployment_name}/" --output-json | jq -r .credentials[].name | xargs -L 1 credhub delete -n
+      ${ROOT}/git-kubo-ci/scripts/set_kubeconfig_long_lived_director
+
+      # don't delete leading & trailing slash. This is to scope to the deployment creds we want to delete
+      credhub find -n "/${deployment_name}/" --output-json | jq -r .credentials[].name | xargs -L 1 credhub delete -n
+      set +e
+      kubectl delete ns --all
+      kubectl delete pvc --all
+      kubectl delete pv --all
+      kubectl delete svc --all
+      set -e
     fi
+    bosh -d "${deployment_name}" -n delete-deployment --force
 }
 
 main
