847930207f2cff1ac799fe085cd6680a0c928903
Mark James Hender <mhender@pivotal.io>
2018-12-19 11:29:07 +0000

    Check deployment has credhub secrets first
    [#162636811]
    
    Signed-off-by: Fabio Mangione <fmangione@pivotal.io>

--- scripts/destroy-k8s-instance-long-lived-director.sh
+++ scripts/destroy-k8s-instance-long-lived-director.sh
@@ -21,7 +21,9 @@ main() {
     bosh -d "${deployment_name}" -n delete-deployment --force
 
     # don't delete leading & trailing slash. This is to scope to the deployment creds we want to delete
-    credhub find -n "/${deployment_name}/" --output-json | jq -r .credentials[].name | xargs -L 1 credhub delete -n
+    if credhub find -n "/${deployment_name}/"; then
+        credhub find -n "/${deployment_name}/" --output-json | jq -r .credentials[].name | xargs -L 1 credhub delete -n
+    fi
 }
 
 main
