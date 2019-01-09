6c628bd38e1a278d94e760828e8eb7d9206042aa
Iain Sproat <isproat@pivotal.io>
2019-01-09 10:08:21 +0000

    Dev version of turbulence should not be used
    * Bosh stemcell team updated xenial stemcell https://github.com/cloudfoundry/bosh-linux-stemcell-builder/issues/57
    * Last official release of turbulence now works with the updated xenial
    stemcell
    
    [#162636811]

--- scripts/deploy-turbulence.sh
+++ scripts/deploy-turbulence.sh
@@ -17,7 +17,6 @@ echo "${BOSH_CA_CERT}" > bosh_ca.tmp
 trap 'rm bosh_ca.tmp' EXIT
 
 bosh -n -d turbulence deploy ./git-turbulence-release/manifests/example.yml \
-  -o git-kubo-ci/manifests/ops-files/use-dev-turbulence.yml \
   -o git-kubo-ci/manifests/ops-files/use-xenial-stemcell.yml \
   -v turbulence_api_ip="10.0.255.0" \
   -v director_ip=$(bosh int "kubo-lock/metadata" --path=/internal_ip) \
