550b256eb80e101c2b2f6c97023c5a3718d4aed6
Morena De Liddo <mdeliddo@pivotal.io>
2019-01-09 14:29:36 +0000

    Fix name patterns
    [#162697909]
    
    Signed-off-by: Fabio Mangione <fmangione@pivotal.io>

--- scripts/aggregate-test-signals.sh
+++ scripts/aggregate-test-signals.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eux
 
-for shipable in ./gcs-*-shipables/*; do
-  shipable_signal="$(basename $shipable)"
+for shipable in ./gcs-*-shipables; do
+  shipable_signal="$(basename $shipable | sed -e "s/^gcs-//" -e "s/-shipables$//")"
   new_signal_version="$(tail -1 gcs-${shipable_signal}-shipables/${shipable_signal})"
   is_shipable=true
-  for ship_signal in ./gcs-*-shipables/*; do
-    signal="$(basename $ship_signal)"
+  for ship_signal in ./gcs-*-shipables; do
+    signal="$(basename $ship_signal | sed -e "s/^gcs-//" -e "s/-shipables$//")"
     if ! grep -q "$new_signal_version" "gcs-${signal}-shipables/$signal"; then
       echo "Version \`$new_signal_version\` is not shipable yet"
       echo "Kubo Release has not passed $signal pipeline"
