56d010fd26f17b7c93dc508740944974a837a526
Iain Sproat <isproat@pivotal.io>
2019-01-09 15:15:09 +0000

    Checks stemcell name and version before uploading
    * Removes dependency on boshstemcells.com (sorry Ben!)
    
    [#162480177]

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -7,7 +7,10 @@ else
     source git-kubo-ci/scripts/set-bosh-env source-json/metadata
 fi
 
+if [ ${IAAS} == "gcp" ]; then
+  IAAS="google"
+fi
+
 stemcell_version="$(bosh int --path=/stemcells/0/version git-kubo-deployment/manifests/cfcr.yml)"
 stemcell_line="$(bosh int --path=/stemcells/0/os git-kubo-deployment/manifests/cfcr.yml)"
-
-bosh upload-stemcell "https://boshstemcells.com/${IAAS}/${stemcell_line}/${stemcell_version}"
+bosh upload-stemcell --name="bosh-${IAAS}-kvm-${stemcell_line}-go_agent" --version="${stemcell_version}" "https://s3.amazonaws.com/bosh-core-stemcells/${IAAS}/bosh-stemcell-${stemcell_version}-${IAAS}-xen-hvm-${stemcell_line}-go_agent.tgz"
