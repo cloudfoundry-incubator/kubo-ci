0ae5ac8d934e3db2199c25da86394279a6c32012
Mark James Hender <mhender@pivotal.io>
2019-01-10 11:49:24 +0000

    VM type is defined for all IAAS when uploading stemcell
    [#162480177]
    
    Signed-off-by: Iain Sproat <isproat@pivotal.io>

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -7,10 +7,21 @@ else
     source git-kubo-ci/scripts/set-bosh-env source-json/metadata
 fi
 
+VM=""
 if [ ${IAAS} == "gcp" ]; then
   IAAS="google"
+  VM="kvm"
+elif [ ${IAAS} == "aws" ]; then
+  VM="xen-hvm"
+elif [ ${IAAS} == "vsphere" ]; then
+  VM="esxi"
+elif [ ${IAAS} == "azure" ]; then
+  VM="hyperv"
+elif [ ${IAAS} == "openstack" ]; then
+  VM="kvm"
 fi
+
 
 stemcell_version="$(bosh int --path=/stemcells/0/version git-kubo-deployment/manifests/cfcr.yml)"
 stemcell_line="$(bosh int --path=/stemcells/0/os git-kubo-deployment/manifests/cfcr.yml)"
-bosh upload-stemcell --name="bosh-${IAAS}-kvm-${stemcell_line}-go_agent" --version="${stemcell_version}" "https://s3.amazonaws.com/bosh-core-stemcells/${IAAS}/bosh-stemcell-${stemcell_version}-${IAAS}-xen-hvm-${stemcell_line}-go_agent.tgz"
+bosh upload-stemcell --name="bosh-${IAAS}-${VM}-${stemcell_line}-go_agent" --version="${stemcell_version}" "https://s3.amazonaws.com/bosh-core-stemcells/${IAAS}/bosh-stemcell-${stemcell_version}-${IAAS}-${VM}-${stemcell_line}-go_agent.tgz"
