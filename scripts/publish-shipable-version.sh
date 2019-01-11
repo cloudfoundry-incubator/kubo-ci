7f23f06747048338a1ead16a17bc09685d5bd1a1
Morena De Liddo <mdeliddo@pivotal.io>
2019-01-11 15:02:31 +0000

    Fix paths for publishing commit hash signal
    [#162697909]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/publish-shipable-version.sh
+++ scripts/publish-shipable-version.sh
@@ -3,5 +3,6 @@ set -exu -o pipefail
 
 signal="$(basename $(find gcs-shipable-version ! -name 'url' ! -name 'generation' -type f))"
 cp gcs-shipable-version/$signal gcs-shipable-version-output/shipable
+mkdir kubo-release-untarred
 tar -xzf kubo-release/kubo-*.tgz --directory kubo-release-untarred
-grep "commit_hash" release.MF | awk -F ' ' '{print $2}' >> gcs-shipable-version-output/shipable
+grep "commit_hash" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}' >> gcs-shipable-version-output/shipable
