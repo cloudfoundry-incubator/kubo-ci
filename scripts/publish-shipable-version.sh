a88ebdef08bd693b5e952f6a9a5e99fb98bd4062
Mark James Hender <mhender@pivotal.io>
2019-01-11 14:49:43 +0000

    Use commit hash instead of version as signal
    [#162697909]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- scripts/publish-shipable-version.sh
+++ scripts/publish-shipable-version.sh
@@ -3,4 +3,5 @@ set -exu -o pipefail
 
 signal="$(basename $(find gcs-shipable-version ! -name 'url' ! -name 'generation' -type f))"
 cp gcs-shipable-version/$signal gcs-shipable-version-output/shipable
-echo | cat kubo-version/number - >> gcs-shipable-version-output/shipable
+tar -xzf kubo-release/kubo-*.tgz --directory kubo-release-untarred
+grep "commit_hash" release.MF | awk -F ' ' '{print $2}' >> gcs-shipable-version-output/shipable
