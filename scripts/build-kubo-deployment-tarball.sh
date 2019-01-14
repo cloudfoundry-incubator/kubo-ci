47ad74274826bf86072a9f54052e7920cac57bd7
Oleksandr Slynko <oslynko@pivotal.io>
2019-01-14 10:39:16 +0000

    Include .git folder in kubo-deployment.tgz
    * Required to get commit hash from .git/short_ref
    * Used to determine tagging when shipping
    
    [#162697909]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/build-kubo-deployment-tarball.sh
+++ scripts/build-kubo-deployment-tarball.sh
@@ -4,10 +4,8 @@ set -exu -o pipefail
 
 mkdir tmp
 cp -r git-kubo-deployment tmp/kubo-deployment
-cp gcs-kubo-release-tarball-untested/kubo-*.tgz tmp/kubo-release.tgz
 
 cd tmp
 tar --exclude='src/kubo-deployment-tests' \
-    --exclude='.git' \
     --exclude='.gitignore' \
     -zcvf "../kubo-deployment-tarball/kubo-deployment-$(cat ../kubo-version/version).tgz" *
