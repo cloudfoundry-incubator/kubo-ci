1569de1508bb9102beab8902dc1e3f0dfa2a3408
Mark James Hender <mhender@pivotal.io>
2019-01-14 12:19:11 +0000

    Include kubo-deployment commit hash in aggregator
    * Publish a file containing both kubo-release and kubo-deployment commit
    hash
    * This will be used by `ship-it` pipeline to create releasable artifacts
    on the correct commit
    
    [#162697909]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/aggregate-test-signals.sh
+++ scripts/aggregate-test-signals.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 set -eux
 
-for shipable in ./gcs-*-shipables; do
-  shipable_signal="$(basename $shipable | sed -e "s/^gcs-//" -e "s/-shipables$//")"
-  new_signal_version="$(tail -1 gcs-${shipable_signal}-shipables/${shipable_signal})"
-  is_shipable=true
-  for ship_signal in ./gcs-*-shipables; do
-    signal="$(basename $ship_signal | sed -e "s/^gcs-//" -e "s/-shipables$//")"
-    if ! grep -q "$new_signal_version" "gcs-${signal}-shipables/$signal"; then
-      echo "Version \`$new_signal_version\` is not shipable yet"
-      echo "Kubo Release has not passed $signal pipeline"
-      is_shipable=false
-      break
-    else
-      echo "Version \`$new_signal_version\` passed $signal pipeline"
-    fi
-  done
-  if $is_shipable ; then
-    echo "Found shipable version $new_signal_version" > "${SLACK_MESSAGE_FILE}"
-    exit 0
-  fi
+first=$(find ./gcs-*-shipables/ -name '*shipable' | head -1)
+
+for shipable in ./gcs-*-shipables/*shipable; do
+  result=/tmp/temp_$shipable
+  comm -12 "$first" "$shipable" > "$result"
+  first=$result
 done
+
+new_signal_version=$(tail -1 "$result")
+if [ -n "$new_signal_version" ] ; then
+  echo "Found shipable version $new_signal_version" > "${SLACK_MESSAGE_FILE}"
+  echo "${new_signal_version}" > "${SHIPABLE_VERSION_FILE}"
+  exit 0
+fi
 echo "Failed to find a shipable tarball" > "${SLACK_MESSAGE_FILE}"
--- scripts/publish-shipable-version.sh
+++ scripts/publish-shipable-version.sh
@@ -5,4 +5,7 @@ signal="$(basename $(find gcs-shipable-version ! -name 'url' ! -name 'generation
 cp gcs-shipable-version/$signal gcs-shipable-version-output/shipable
 mkdir kubo-release-untarred
 tar -xzf kubo-release/kubo-*.tgz --directory kubo-release-untarred
-grep "commit_hash" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}' >> gcs-shipable-version-output/shipable
+release_hash="$(grep "commit_hash" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}')"
+deployment_hash="$(cat git-kubo-deployment/.git/short_ref)"
+
+printf "%s %s\n" "${release_hash}" "${deployment_hash}" >> "gcs-shipable-version-output/shipable"
