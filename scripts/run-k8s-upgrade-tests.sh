1c3a7a5a8d758071bcf2f60d295e65262983bc24
Iain Sproat <isproat@pivotal.io>
2019-01-14 14:54:40 +0000

    Remove inputs for upgrade test in long lived bosh
    [#163149293]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- scripts/run-k8s-upgrade-tests.sh
+++ scripts/run-k8s-upgrade-tests.sh
@@ -4,15 +4,16 @@ set -eu -o pipefail
 
 export ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 export GOPATH="${ROOT}/git-kubo-ci"
-export KUBECONFIG="${ROOT}/gcs-kubeconfig/config"
 
 target_bosh_director() {
-  BOSH_DEPLOYMENT="${DEPLOYMENT_NAME}"
-  BOSH_ENVIRONMENT=$(bosh int source-json/source.json --path '/target')
-  BOSH_CLIENT=$(bosh int source-json/source.json --path '/client')
-  BOSH_CLIENT_SECRET=$(bosh int source-json/source.json --path '/client_secret')
-  BOSH_CA_CERT=$(bosh int source-json/source.json --path '/ca_cert')
-  export BOSH_DEPLOYMENT BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT
+  if [[ -f source-json/source.json ]]; then
+    source="source-json/source.json"
+  else
+    source="kubo-lock/metadata"
+    DEPLOYMENT_NAME="$(bosh int kubo-lock/metadata --path=/deployment_name)"
+  fi
+  export BOSH_DEPLOYMENT="${DEPLOYMENT_NAME}"
+  source "${ROOT}/git-kubo-ci/scripts/set-bosh-env" ${source}
 }
 
 main() {
