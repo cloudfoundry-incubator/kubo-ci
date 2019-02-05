091e5b42a4082057e152722cf00a9979dec681a0
Mark Hender <henderjm@pivotal.io>
2019-02-05 12:23:05 +0000

    Add dev version to shippable file
    * Improves vision on what release version triggered to be shipable than
    just looking at commit hashes
    * Commit hashes will continue to be used by the `ship-it` pipeline to
    checkout and create finals
    
    Signed-off-by: Neil Hickey <nhickey@pivotal.io>

--- scripts/publish-shipable-version.sh
+++ scripts/publish-shipable-version.sh
@@ -7,5 +7,6 @@ mkdir kubo-release-untarred
 tar -xzf kubo-release/kubo-*.tgz --directory kubo-release-untarred
 release_hash="$(grep "commit_hash" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}')"
 deployment_hash="$(cat git-kubo-deployment/.git/short_ref)"
+dev_version="$(grep "version" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}')"
 
-printf "%s %s\n" "${release_hash}" "${deployment_hash}" >> "gcs-shipable-version-output/shipable"
+printf "%s %s %s\n" "${release_hash}" "${deployment_hash}" "${dev_version}">> "gcs-shipable-version-output/shipable"
