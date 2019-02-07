a2be53e68afa5ca321119d77adbca7b9e1f5bbc6
Oleksandr Slynko <oslynko@pivotal.io>
2019-02-07 11:05:54 +0000

    Remove extra quotes from release hash
    Signed-off-by: Mark Hender <henderjm@pivotal.io>

--- scripts/publish-shipable-version.sh
+++ scripts/publish-shipable-version.sh
@@ -5,8 +5,8 @@ signal="$(basename $(find gcs-shipable-version ! -name 'url' ! -name 'generation
 cp gcs-shipable-version/$signal gcs-shipable-version-output/shipable
 mkdir kubo-release-untarred
 tar -xzf kubo-release/kubo-*.tgz --directory kubo-release-untarred
-release_hash="$(grep "commit_hash" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}')"
-deployment_hash="$(cat git-kubo-deployment/.git/short_ref)"
+release_hash=$(grep "commit_hash" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}' | tr -d "'")
+deployment_hash="$(cat git-kubo-deployment/.git/short_ref | tr -d "'")"
 dev_version="$(grep "^version" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}')"
 
 printf "%s %s %s\n" "${release_hash}" "${deployment_hash}" "${dev_version}">> "gcs-shipable-version-output/shipable"
