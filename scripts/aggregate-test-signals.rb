e7aee1cbc7ab122a65a4411ed1555ac65f1098eb
Mark Hender <henderjm@pivotal.io>
2019-02-07 11:18:25 +0000

    Output build version in slack
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- scripts/aggregate-test-signals.rb
+++ scripts/aggregate-test-signals.rb
@@ -12,7 +12,7 @@ end
 if overlap.any?
   release_sha, deployment_sha, build_number = overlap.last.split
   File.write(ENV['SLACK_MESSAGE_FILE'],
-             "Ready to :ship: <https://github.com/cloudfoundry-incubator/kubo-release/tree/#{release_sha}/|#{release_sha}> <https://github.com/cloudfoundry-incubator/kubo-deployment/tree/#{deployment_sha}/|#{deployment_sha}>")
+             "Ready to :ship: <https://github.com/cloudfoundry-incubator/kubo-release/tree/#{release_sha}/|#{release_sha}> <https://github.com/cloudfoundry-incubator/kubo-deployment/tree/#{deployment_sha}/|#{deployment_sha}> Build number is #{build_number}")
   File.write(ENV['SHIPABLE_VERSION_FILE'], overlap.last)
 
 else
@@ -20,4 +20,4 @@ else
   File.write(ENV['SLACK_MESSAGE_FILE'], 'No shippable version found')
   exit 1
 end
-puts "Good versions are: #{release_sha}, #{deployment_sha}.  Build number is #{build_number}"
+puts "Good versions are: #{release_sha}, #{deployment_sha}. Build number is #{build_number}"
