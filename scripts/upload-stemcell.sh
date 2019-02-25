c5184989977977f1602bce97b83224ce09f3e3a6
Nick Tenczar <ntenczar@vmware.com>
2019-02-25 12:22:33 -0800

    Modify upload-stemcell to upload windows stemcell
    Assumes that the windows stemcell definition is second in the modified
    cfcr.yml manifest.
    
    Signed-off-by: Neil Hickey <nhickey@pivotal.io>

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -25,3 +25,7 @@ fi
 stemcell_version="$(bosh int --path=/stemcells/0/version git-kubo-deployment/manifests/cfcr.yml)"
 stemcell_line="$(bosh int --path=/stemcells/0/os git-kubo-deployment/manifests/cfcr.yml)"
 bosh upload-stemcell --name="bosh-${IAAS}-${VM}-${stemcell_line}-go_agent" --version="${stemcell_version}" "https://s3.amazonaws.com/bosh-core-stemcells/${IAAS}/bosh-stemcell-${stemcell_version}-${IAAS}-${VM}-${stemcell_line}-go_agent.tgz"
+
+stemcell_version="$(bosh int -o git-kubo-deployment/manifests/ops-files/windows/change-windows-stemcell.yml --path=/stemcells/1/version git-kubo-deployment/manifests/cfcr.yml)"
+stemcell_line="$(bosh int -o git-kubo-deployment/manifests/ops-files/windows/change-windows-stemcell.yml --path=/stemcells/1/os git-kubo-deployment/manifests/cfcr.yml)"
+bosh upload-stemcell --name="bosh-${IAAS}-${VM}-${stemcell_line}-go_agent" --version="${stemcell_version}" "https://s3.amazonaws.com/bosh-core-stemcells/${IAAS}/bosh-stemcell-${stemcell_version}-${IAAS}-${VM}-${stemcell_line}-go_agent.tgz"
