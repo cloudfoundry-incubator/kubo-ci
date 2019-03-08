7d0a5ce7332d6d3bb79a16646ef163f4bcca4839
Nick Tenczar <ntenczar@vmware.com>
2019-03-08 12:36:20 -0800

    Update upload stemcell to work off of alternate-stemcell directory
    Signed-off-by: Travis Hall <thall@vmware.com>

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -32,8 +32,6 @@ else
   bosh upload-stemcell --name="bosh-${IAAS}-${VM}-${stemcell_line}-go_agent" --version="${stemcell_version}" "https://s3.amazonaws.com/bosh-core-stemcells/${IAAS}/bosh-stemcell-${stemcell_version}-${IAAS}-${VM}-${stemcell_line}-go_agent.tgz"
 fi
 
-if [[ -f alternate-stemcell/bosh-stemcell-*.tgz ]]; then
-stemcell_version="$(bosh int -o git-kubo-deployment/manifests/ops-files/windows/change-windows-stemcell.yml --path=/stemcells/1/version git-kubo-deployment/manifests/cfcr.yml)"
-stemcell_line="$(bosh int -o git-kubo-deployment/manifests/ops-files/windows/change-windows-stemcell.yml --path=/stemcells/1/os git-kubo-deployment/manifests/cfcr.yml)"
+if [[ -d alternate-stemcell ]]; then
 bosh upload-stemcell "alternate-stemcell/bosh-stemcell-*.tgz"
 fi
