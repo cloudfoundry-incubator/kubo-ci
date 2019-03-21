36961db1fc3d1070090090d8a2696d092b742b57
Ben Moss <bmoss@pivotal.io>
2019-03-21 12:26:30 -0400

    Auto-bump windows releases.
    The windows-tools-release and windows-utilities-release used on windows
    nodes should be automatically updated. This generalizes the
    bump-boshrelease.sh script to handle modifying the opsfile for adding
    windows workers and adds these releases to bump-bosh-release.yml.
    
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- scripts/bump-boshrelease.sh
+++ scripts/bump-boshrelease.sh
@@ -6,12 +6,25 @@ cp -r git-kubo-deployment/. git-kubo-deployment-output
 
 release="${RELEASE_NAME}"
 version="$(cat boshrelease/version)"
+opsfile="${OPSFILE:-non-precompiled-release.yml}"
+mode="${MODE}"
 
 url="$(cat boshrelease/url)"
 
 sha1="$(cat boshrelease/sha1)"
 
-cat > update-$release-release.yml <<EOF
+if [ "$mode" == "name" ]; then
+  cat > update-$release-release.yml <<EOF
+- type: replace
+  path: /name=$release/value
+  value:
+    name: $release
+    version: $version
+    sha1: $sha1
+    url: $url
+EOF
+else
+  cat > update-$release-release.yml <<EOF
 - type: replace
   path: /0/value/name=$release
   value:
@@ -20,17 +33,18 @@ cat > update-$release-release.yml <<EOF
     sha1: $sha1
     url: $url
 EOF
+fi
 
-bosh int git-kubo-deployment/manifests/ops-files/non-precompiled-releases.yml \
-  -o update-$release-release.yml > git-kubo-deployment-output/manifests/ops-files/non-precompiled-releases.yml
+bosh int "git-kubo-deployment/manifests/ops-files/$opsfile" \
+  -o update-$release-release.yml > "git-kubo-deployment-output/manifests/ops-files/$opsfile"
 
 pushd git-kubo-deployment-output
 git config --global user.name "cfcr"
 git config --global user.email "cfcr@pivotal.io"
 
 if [ -n "$(git status --porcelain)" ]; then
-    git add manifests/ops-files/non-precompiled-releases.yml
-    git commit -m "Bump $release to version $version in non-precompiled-releases ops-file"
+    git add "manifests/ops-files/$opsfile"
+    git commit -m "Bump $release to version $version in $opsfile"
 else
     echo "No changes to commit."
 fi
