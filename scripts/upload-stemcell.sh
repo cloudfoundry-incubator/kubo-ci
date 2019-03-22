f31a845d2ca5bb436bd763183b857a3fe267debb
Travis Hall <thall@vmware.com>
2019-03-21 18:10:31 -0700

    Fix upload-stemcell version comparison to be semver compatible

--- scripts/upload-stemcell.sh
+++ scripts/upload-stemcell.sh
@@ -26,7 +26,11 @@ stemcell_version="$(bosh int --path=/stemcells/0/version git-kubo-deployment/man
 stemcell_line="$(bosh int --path=/stemcells/0/os git-kubo-deployment/manifests/cfcr.yml)"
 
 # 250.17 starts using a new directory structure for stemcells...
-if (( $(echo "${stemcell_version} > 250.17" |bc -l) )); then
+function version_gt() {
+  test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
+}
+
+if version_gt ${stemcell_version} 250.17; then
   bosh upload-stemcell --name="bosh-${IAAS}-${VM}-${stemcell_line}-go_agent" --version="${stemcell_version}" "https://s3.amazonaws.com/bosh-core-stemcells/${stemcell_version}/bosh-stemcell-${stemcell_version}-${IAAS}-${VM}-${stemcell_line}-go_agent.tgz"
 else
   bosh upload-stemcell --name="bosh-${IAAS}-${VM}-${stemcell_line}-go_agent" --version="${stemcell_version}" "https://s3.amazonaws.com/bosh-core-stemcells/${IAAS}/bosh-stemcell-${stemcell_version}-${IAAS}-${VM}-${stemcell_line}-go_agent.tgz"
