4ad619cd30c25c03ef320c5df7f3bfa6ae7c8744
Neil Hickey <nhickey@pivotal.io>
2019-05-15 15:42:07 +0100

    Add job to bump-k8s to include windows
    + Modify script to branch on windows and linux pr's
    + Did not template the pipeline file (as it is still small enough to not
    need templating, and templated pipelines are hard to reason about)
    
    [#164706892]

--- scripts/bump-k8s.sh
+++ scripts/bump-k8s.sh
@@ -56,8 +56,38 @@ EOF
   popd
 }
 
+pr_kubo_release_windows() {
+  version="$1"
+  tag="$2"
+
+  cp -r git-kubo-release-windows/. git-kubo-release-windows-output
+  pushd git-kubo-release-windows-output
+
+  ./scripts/download_k8s_binaries $version
+
+  if [ -n "$(git status --porcelain)" ]; then
+    cat <<EOF > "config/private.yml"
+blobstore:
+  options:
+    access_key_id: ${ACCESS_KEY_ID}
+    secret_access_key: ${SECRET_ACCESS_KEY}
+EOF
+    bosh upload-blobs
+    generate_pull_request "kubernetes" "$tag" "kubo-release-windows" "develop"
+  else
+    echo "Kubernetes version is already up-to-date"
+  fi
+
+  popd
+}
+
 tag=$(cat "$PWD/k8s-release/tag")
 version=$(cat "$PWD/k8s-release/version")
 
-pr_kubo_release "$version" "$tag"
+if [ $BASE_OS = "windows" ]; then
+  pr_kubo_release_windows "$version" "$tag"
+else
+  pr_kubo_release "$version" "$tag"
+fi
+
 pr_kubo_ci "$version" "$tag"
