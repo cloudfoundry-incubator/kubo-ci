ca3fd8f72a945b339bf33ed9c96ff2fe95de5765
Nick Tenczar <ntenczar@pivotal.io>
2019-07-14 20:20:55 -0700

    Update to Istio 1.1.11 for Istio test

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -3,7 +3,7 @@ set -euox pipefail
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 
 export KUBECONFIG=$ROOT/gcs-kubeconfig/config
-ISTIO_VERSION=1.1.0.snapshot.0
+ISTIO_VERSION=1.1.11
 
 git clone --depth 50 --branch $ISTIO_VERSION https://github.com/istio/istio $GOPATH/src/istio.io/istio
 
