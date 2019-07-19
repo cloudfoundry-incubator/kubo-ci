b55dce9fcaa700d017a486ebb7608df9afb0df7d
Lubron Zhan <lzhan@vmware.com>
2019-07-19 16:02:02 +0800

    Update docker-latest-version to update kubo and kubo to specific version Also add task to upload releases
    Signed-off-by: Nick Tenczar <ntenczar@pivotal.io>

--- scripts/upload-release.sh
+++ scripts/upload-release.sh
@@ -12,6 +12,9 @@ target_bosh_director() {
 
 main() {
   target_bosh_director
-  bosh upload-release ${PWD}/release/*.tgz
+  bosh upload-release https://storage.googleapis.com/kubo-public/docker-35.2.3-ubuntu-xenial-315.36-20190716-163114-008878.tgz
+  bosh upload-release https://storage.googleapis.com/kubo-public/docker-35.2.3-windows2019-2019.7-20190716-161813-432556.tgz
+  bosh upload-release https://storage.googleapis.com/kubo-public/kubo-1.0.0-dev.5-ubuntu-xenial-315.70-20190719-022102-888261013.tgz
+  bosh upload-release https://storage.googleapis.com/kubo-public/kubo-1.0.0-dev.5-windows2019-2019.4-20190719-022425-676949049.tgz
 }
 
