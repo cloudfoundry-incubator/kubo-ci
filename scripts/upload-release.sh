dc2f2ef108bcf430be18c4ce9464838f48608fd6
Nick Tenczar <ntenczar@pivotal.io>
2019-07-20 07:26:04 +0800

    Update version to 1.0.0-dev.101
    Signed-off-by: Lubron Zhan <lzhan@vmware.com>

--- scripts/upload-release.sh
+++ scripts/upload-release.sh
@@ -14,7 +14,7 @@ main() {
   target_bosh_director
   bosh upload-release https://storage.googleapis.com/kubo-public/docker-35.2.3-ubuntu-xenial-315.36-20190716-163114-008878.tgz
   bosh upload-release https://storage.googleapis.com/kubo-public/docker-35.2.3-windows2019-2019.7-20190716-161813-432556.tgz
-  bosh upload-release https://storage.googleapis.com/kubo-public/kubo-1.0.0-dev.5-ubuntu-xenial-315.70-20190719-022102-888261013.tgz
-  bosh upload-release https://storage.googleapis.com/kubo-public/kubo-1.0.0-dev.5-windows2019-2019.4-20190719-022425-676949049.tgz
+  bosh upload-release https://storage.googleapis.com/kubo-public/kubo-1.0.0-dev.101-ubuntu-xenial-456.1-20190719-232139-365998982.tgz
+  bosh upload-release https://storage.googleapis.com/kubo-public/kubo-1.0.0-dev.101-windows2019-2019.4-20190719-231442-026390434.tgz
 }
 
