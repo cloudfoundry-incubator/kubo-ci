eeb6c8d63b61b49898de79f0720935d9901d5c1f
Nick Tenczar <ntenczar@pivotal.io>
2019-07-18 18:00:38 -0700

    Update build-kubo to support recombined kubo/kubo-windows
    Signed-off-by: Lubron Zhan <lzhan@vmware.com>

--- scripts/export-release.sh
+++ scripts/export-release.sh
@@ -10,6 +10,12 @@ STEMCELL_OS=$(bosh int cfcr.yml --ops-file ops-files/windows/add-worker.yml --pa
 STEMCELL_VERSION=$(bosh stemcells --json | jq .Tables[0].Rows | jq -r "map(select(.os | contains(\"${STEMCELL_OS}\"))) | max_by(.version) | .version | sub(\"\\\*\"; \"\")")
 popd
 
+jobs=""
+for job in $JOBS_LIST
+do
+  jobs="${jobs} --job $job"
+done
+
 for release in $RELEASE_LIST
 do
   if [[ $release == kubo* ]]; then
@@ -17,7 +23,7 @@ do
   else
     RELEASE_VERSION="$(bosh int git-kubo-deployment/manifests/cfcr.yml -o git-kubo-deployment/manifests/ops-files/non-precompiled-releases.yml --path=/releases/name=${release}/version)"
   fi
-  bosh -d compilation-${stemcell_alias} export-release "$release/$RELEASE_VERSION" "$STEMCELL_OS/$STEMCELL_VERSION"
+  bosh -d compilation-${stemcell_alias} export-release "$release/$RELEASE_VERSION" "$STEMCELL_OS/$STEMCELL_VERSION" ${jobs}
 
   mv *.tgz "compiled-releases/"
 done
