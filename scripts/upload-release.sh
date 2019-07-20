c87e9a6e424676ced795db0c4175543041a44745
Lubron Zhan <lzhan@vmware.com>
2019-07-20 08:02:29 +0800

    Fix the upload-release script
    Signed-off-by: Nick Tenczar <ntenczar@pivotal.io>

--- scripts/upload-release.sh
+++ scripts/upload-release.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -euo pipefail
+set -exuo pipefail
 
 target_bosh_director() {
-  BOSH_ENVIRONMENT=$(bosh int source-json/source.json --path '/target')
-  BOSH_CLIENT=$(bosh int source-json/source.json --path '/client')
-  BOSH_CLIENT_SECRET=$(bosh int source-json/source.json --path '/client_secret')
-  BOSH_CA_CERT=$(bosh int source-json/source.json --path '/ca_cert')
-  export BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT
+  local source_file
+  if [[ -f source-json/source.json ]]; then
+      source git-kubo-ci/scripts/set-bosh-env source-json/source.json
+  else
+      source git-kubo-ci/scripts/set-bosh-env source-json/metadata
+  fi
 }
 
 main() {
