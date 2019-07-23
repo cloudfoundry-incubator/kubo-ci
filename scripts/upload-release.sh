0711b765bb1a110cbdb4172acac0198271c28541
Lubron Zhan <lzhan@vmware.com>
2019-07-23 20:42:06 +0800

    Upload only kubo release in this case
    Signed-off-by: Nick Tenczar <ntenczar@pivotal.io>

--- scripts/upload-release.sh
+++ scripts/upload-release.sh
@@ -16,7 +16,6 @@ main() {
   bosh upload-release https://storage.googleapis.com/kubo-public/docker-35.2.3-ubuntu-xenial-315.36-20190716-163114-008878.tgz
   bosh upload-release https://storage.googleapis.com/kubo-public/docker-35.2.3-windows2019-2019.7-20190716-161813-432556.tgz
   bosh upload-release https://storage.googleapis.com/kubo-public/kubo-1.0.0-dev.102-ubuntu-xenial-456.1-20190719-232139-365998982.tgz
-  bosh upload-release https://storage.googleapis.com/kubo-public/kubo-1.0.0-dev.102-windows2019-2019.4-20190719-231442-026390434.tgz
 }
 
 main
