30ba4ed9ff8564f62a77260e08f6078110e1c719
Nick Tenczar <ntenczar@pivotal.io>
2019-07-25 13:57:48 -0700

    Support precompiling docker-windows from latest docker-boshrelease

--- scripts/bump-boshrelease.sh
+++ scripts/bump-boshrelease.sh
@@ -6,6 +6,8 @@ cp -r git-kubo-deployment/. git-kubo-deployment-output
 
 release="${RELEASE_NAME}"
 version="$(cat boshrelease/version)"
+array_pos="${ARRAY_POS}"
+base_ops_file="${BASE_OPS_FILE}"
 
 url="$(cat boshrelease/url)"
 
@@ -13,7 +15,7 @@ sha1="$(cat boshrelease/sha1)"
 
 cat > update-$release-release.yml <<EOF
 - type: replace
-  path: /0/value/name=$release
+  path: /$array_pos/value/name=$release
   value:
     name: $release
     version: $version
@@ -21,16 +23,16 @@ cat > update-$release-release.yml <<EOF
     url: $url
 EOF
 
-bosh int git-kubo-deployment/manifests/ops-files/non-precompiled-releases.yml \
-  -o update-$release-release.yml > git-kubo-deployment-output/manifests/ops-files/non-precompiled-releases.yml
+bosh int git-kubo-deployment/$base_ops_file \
+  -o update-$release-release.yml > git-kubo-deployment-output/$base_ops_file
 
 pushd git-kubo-deployment-output
 git config --global user.name "cfcr"
 git config --global user.email "cfcr@pivotal.io"
 
 if [ -n "$(git status --porcelain)" ]; then
-    git add manifests/ops-files/non-precompiled-releases.yml
-    git commit -m "Bump $release to version $version in non-precompiled-releases ops-file"
+    git add $base_ops_file
+    git commit -m "Bump $release to version $version in $(basename $base_ops_file .yml) ops-file"
 else
     echo "No changes to commit."
 fi
