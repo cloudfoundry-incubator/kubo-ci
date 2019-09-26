bbccb33bd894ef6b0b9b7b51d389c1e53e8fd8fe
Todd Sedano <tsedano@pivotal.io>
2019-09-26 11:21:56 -0700

    Installs istio via helm templates without tiller
    https://istio.io/docs/setup/install/helm/#option-1-install-with-helm-via-helm-template
    
    [#168044301]
    
    Co-authored-by: Todd Sedano <tsedano@pivotal.io>
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -11,16 +11,19 @@ cd $GOPATH/src/istio.io/istio
 
 # init helm
 ./bin/init_helm.sh
-kubectl apply -f install/kubernetes/helm/helm-service-account.yaml
-helm init --service-account tiller
-kubectl rollout status -w -n kube-system deployment.apps/tiller-deploy
+kubectl create namespace istio-system
 
-# install Istio
-trap "helm del --purge istio-init; kubectl delete namespace istio-system" 0 1 2 3 15
-kubectl apply -f install/kubernetes/helm/helm-service-account.yaml
-helm install install/kubernetes/helm/istio-init --name istio-init --namespace istio-system
-trap "helm del --purge istio-system; kubectl delete namespace istio-system" 0 1 2 3 15
-helm install install/kubernetes/helm/istio --name istio-system --namespace istio-system --set global.mtls.enabled=true \
-  --set sidecarInjectorWebhook.enabled=true --set global.hub=istio --set global.tag=$ISTIO_VERSION --set global.crds=false --debug
+helm template install/kubernetes/helm/istio-init --name istio-init --namespace istio-system > istio-init.yml
+trap "kubectl delete -f istio-init.yml" 0 1 2 3 15
+kubectl apply -f istio-init.yml
+
+echo "This should be 23"
+kubectl get crds | grep 'istio.io' | wc -l
+
+helm template install/kubernetes/helm/istio --name istio-system --namespace istio-system --set global.mtls.enabled=true \
+  --set sidecarInjectorWebhook.enabled=true --set global.hub=istio --set global.tag=$ISTIO_VERSION \
+  --set global.crds=false > istio.yml
+trap "kubectl delete -f istio.yml; kubectl delete -f istio-init.yml" 0 1 2 3 15
+kubectl apply -f istio.yml
 
 make e2e_simple TAG=${ISTIO_VERSION} E2E_ARGS='--installer=helm --skip_setup'
