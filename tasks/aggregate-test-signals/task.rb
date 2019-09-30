26ed62d6d38035344e7728dbdce6b50d3cba9c67
Todd Sedano <tsedano@pivotal.io>
2019-09-30 10:04:25 -0700

    Prints build numbers at end of task

--- tasks/aggregate-test-signals/task.rb
+++ tasks/aggregate-test-signals/task.rb
@@ -25,7 +25,7 @@ else
   File.write(ENV['SLACK_MESSAGE_FILE'], 'No shippable version found')
   exit 1
 end
-puts Rainbow.green("Good versions are: #{release_sha}, #{deployment_sha}. Build number is #{build_number}")
+puts Rainbow("Good versions are: #{release_sha}, #{deployment_sha}. Build number is #{build_number}").green
 
 puts
 puts "Highest green build for each pipeline..."
