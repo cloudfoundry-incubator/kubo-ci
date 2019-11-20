6a81aa1ecbed199b7da7040b5e3de6323b6b0596
Daniel Keeney <dkeeney@pivotal.io>
2019-11-20 11:24:31 -0800

    Use explicit local tarball for releases
    Authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- pks-pipelines/deploy-k8s/upload-release/task.sh
+++ pks-pipelines/deploy-k8s/upload-release/task.sh
@@ -5,11 +5,11 @@ set -eux pipefail
 source git-kubo-ci/pks-pipelines/deploy-k8s/utils/all-env.sh
 
 pushd git-pks-kubo-release
-  bosh create-release --version="${KUBO_GIT_SHA}"
-  bosh upload-release
+  bosh create-release --version="${KUBO_GIT_SHA}" --tarball pipeline.tgz
+  bosh upload-release pipeline.tgz
 popd
 
 pushd git-pks-cfcr-etcd-release
-  bosh create-release --version="${ETCD_GIT_SHA}"
-  bosh upload-release
+  bosh create-release --version="${ETCD_GIT_SHA}" --tarball pipeline.tgz
+  bosh upload-release pipeline.tgz
 popd
