ccb8cdafef2f7aa7d8e20d54266632ede498e7e4
Larry Hamel <lhamel@pivotal.io>
2019-11-20 15:03:40 -0800

    Use a smaller manifest
    Co-authored-by: Larry Hamel <lhamel@pivotal.io>
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- pks-pipelines/deploy-k8s/deploy-manifest/task.sh
+++ pks-pipelines/deploy-k8s/deploy-manifest/task.sh
@@ -9,4 +9,4 @@ bosh --non-interactive deploy -d "${DEPLOYMENT_NAME}" \
   --var=kubo-version="${KUBO_GIT_SHA}" \
   --var=etcd-version="${ETCD_GIT_SHA}" \
   --var=docker-version="${DOCKER_GIT_SHA}" \
-  git-kubo-ci/pks-pipelines/manifest.yml
+  git-kubo-ci/pks-pipelines/print-manifest.yml
