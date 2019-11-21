ff07c5778a5b08d6ea8fd56f304f856d970770ec
Daniel Keeney <dkeeney@pivotal.io>
2019-11-21 14:19:08 -0800

    Continue with cleanup even if releases are still used
    Authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- pks-pipelines/deploy-k8s/cleanup/task.sh
+++ pks-pipelines/deploy-k8s/cleanup/task.sh
@@ -4,15 +4,29 @@ set -eux pipefail
 
 source git-kubo-ci/pks-pipelines/deploy-k8s/utils/all-env.sh
 
+#####################
+# Delete Deployment #
+#####################
 bosh delete-deployment \
   --non-interactive \
   --deployment="${DEPLOYMENT_NAME}"
+
+###################
+# Delete Releases #
+###################
 bosh delete-release \
   --non-interactive \
-  kubo/"${KUBO_GIT_SHA}"
+  kubo/"${KUBO_GIT_SHA}" \
+  || true
 bosh delete-release \
   --non-interactive \
-  cfcr-etcd/"${ETCD_GIT_SHA}"
+  kubo-windows/"${KUBO_WINDOWS_GIT_SHA}" \
+  || true
 bosh delete-release \
   --non-interactive \
-  docker/"${DOCKER_GIT_SHA}"
+  cfcr-etcd/"${ETCD_GIT_SHA}" \
+  || true
+bosh delete-release \
+  --non-interactive \
+  docker/"${DOCKER_GIT_SHA}" \
+  || true
