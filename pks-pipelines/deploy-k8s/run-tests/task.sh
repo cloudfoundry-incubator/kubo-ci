b5520927cbe43a65f40ed271bcd957978d09f48f
Daniel Keeney <dkeeney@pivotal.io>
2019-11-21 15:37:54 -0800

    Add comparison for kubernetes versions
    Authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- pks-pipelines/deploy-k8s/run-tests/task.sh
+++ pks-pipelines/deploy-k8s/run-tests/task.sh
@@ -16,4 +16,31 @@ bosh run-errand \
   --json \
   > windows_versions.txt
 
-exit 1
+# stdout will be 'Kubernetes v1.15.5'
+# cut will trim that down to 1.15.5
+WINDOWS_VERSION="$(cat windows_versions.txt | \
+  jq .Tables[0].Rows[0].stdout --raw-output | \
+  cut -d'v' -f2)"
+
+# stdout will be OSM format yml, such as:
+# other:kubernetes:1.15.5:
+#  name: kubernetes
+#  version: 1.15.5
+#  repository: Other
+#  license: Apache2.0
+#  other-distribution: /tmp/osl/v1.15.5.tar.gz
+#  url: https://github.com/kubernetes/kubernetes/archive/v1.15.5.tar.gz
+# grep finds the header line
+# cut trims down to 1.15.5
+LINUX_VERSION="$(cat linux_versions.txt | \
+  jq .Tables[0].Rows[0].stdout --raw-output | \
+  grep '^other:kubernetes:' | \
+  cut -d':' -f3)"
+
+if [ "$WINDOWS_VERSION" == "$LINUX_VERSION" ]
+then
+  echo "Versions match! $LINUX_VERSION"
+else
+  echo "Version mismatch!  Linux: $LINUX_VERSION, Windows: $WINDOWS_VERSION"
+  exit 1
+fi
