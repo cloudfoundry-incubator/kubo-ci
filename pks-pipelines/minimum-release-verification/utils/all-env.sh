27c933c480c1b881850d4d37e5bf7f44347dc717
Larry Hamel <lhamel@pivotal.io>
2019-11-25 13:25:35 -0800

    Rename to avoid misintpreation of "release" as verb
    Authored-by: Larry Hamel <lhamel@pivotal.io>

--- pks-pipelines/minimum-release-verification/utils/all-env.sh
+++ pks-pipelines/minimum-release-verification/utils/all-env.sh
@@ -3,4 +3,4 @@
 set -euxo pipefail
 
 source git-kubo-ci/pks-pipelines/minimum-release-verification/utils/lock-to-env.sh
-source git-kubo-ci/pks-pipelines/minimum-release-verification/utils/release-git-shas.sh
+source git-kubo-ci/pks-pipelines/minimum-release-verification/utils/define-git-shas-of-release.sh
