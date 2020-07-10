d86a3ab9156d4800c284bef4ef7a81488b1694fa
Stu Charlton <scharlton@vmware.com>
2020-07-09 23:45:57 -0600

    Bump istio tests

--- scripts/run-istio-tests.sh
+++ scripts/run-istio-tests.sh
@@ -3,14 +3,78 @@ set -euox pipefail
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 
 export KUBECONFIG=$ROOT/gcs-kubeconfig/config
-ISTIO_VERSION=1.2.2
+ISTIO_VERSION=1.5.8
 
 git clone --depth 50 --branch $ISTIO_VERSION https://github.com/istio/istio $GOPATH/src/istio.io/istio
 
 cd $GOPATH/src/istio.io/istio
 
-# init helm
-./bin/init_helm.sh
+# init helm - removed upstream , pasting here for now prior to migrating to istioctl
+
+export GO_TOP=${GO_TOP:-$(echo "${GOPATH}" | cut -d ':' -f1)}
+export OUT_DIR=${OUT_DIR:-${GO_TOP}/out}
+
+HELM_VER=${HELM_VER:-v2.10.0}
+
+export GOPATH=${GOPATH:-$GO_TOP}
+# Normally set by Makefile
+export ISTIO_BIN=${ISTIO_BIN:-${GOPATH}/bin}
+
+# Set the architecture. Matches logic in the Makefile.
+export GOARCH=${GOARCH:-'amd64'}
+
+# Determine the OS. Matches logic in the Makefile.
+LOCAL_OS=${OSTYPE}
+case $LOCAL_OS in
+  "linux"*)
+    LOCAL_OS='linux'
+    ;;
+  "darwin"*)
+    LOCAL_OS='darwin'
+    ;;
+  *)
+    echo "This system's OS ${LOCAL_OS} isn't recognized/supported"
+    exit 1
+    ;;
+esac
+export GOOS=${GOOS:-${LOCAL_OS}}
+
+# Gets the download command supported by the system (currently either curl or wget)
+# simplify because init.sh has already decided which one is
+DOWNLOAD_COMMAND=""
+function set_download_command () {
+    # Try curl.
+    if command -v curl > /dev/null; then
+        if curl --version | grep Protocols  | grep https > /dev/null; then
+	       DOWNLOAD_COMMAND='curl -Lo '
+	       return
+        fi
+    fi
+
+    # Try wget.
+    if command -v wget > /dev/null; then
+        DOWNLOAD_COMMAND='wget -O '
+        return
+    fi
+}
+set_download_command
+
+# test scripts seem to like to run this script directly rather than use make
+export ISTIO_OUT=${ISTIO_OUT:-${ISTIO_BIN}}
+
+# install helm if not present, it must be the local version.
+if [ ! -f "${ISTIO_OUT}/version.helm.${HELM_VER}" ] ; then
+    TD=$(mktemp -d)
+    # Install helm. Please keep it in sync with .circleci
+    cd "${TD}" && \
+        ${DOWNLOAD_COMMAND} "${TD}/helm.tgz" "https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VER}-${LOCAL_OS}-amd64.tar.gz" && \
+        tar xfz helm.tgz && \
+        mv ${LOCAL_OS}-amd64/helm "${ISTIO_OUT}/helm-${HELM_VER}" && \
+        cp "${ISTIO_OUT}/helm-${HELM_VER}" "${ISTIO_OUT}/helm" && \
+        rm -rf "${TD}" && \
+        touch "${ISTIO_OUT}/version.helm.${HELM_VER}"
+fi
+
 kubectl create namespace istio-system
 
 helm template install/kubernetes/helm/istio-init --name istio-init --namespace istio-system > istio-init.yml
