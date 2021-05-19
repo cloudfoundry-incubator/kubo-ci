441e8241004238c32bbc63dc94ed1e3fa449325b
YanzhaoLi <yanzhaol@vmware.com>
2021-05-19 06:14:56 -0700

    Draft set ulimit to 65535 need see tkg

--- src/tests/integration-tests/generic/ulimit_test.go
+++ src/tests/integration-tests/generic/ulimit_test.go
@@ -22,11 +22,11 @@ var _ = Describe("Ulimit", func() {
 		kubectl.Teardown()
 	})
 
-	It("Should have a ulimit at least of 1048576", func() {
+	It("Should have a ulimit at least of 65535", func() {
 		podName := test_helpers.GenerateRandomUUID()
 		output, err := kubectl.GetOutput("run", podName, "--image", "gcr.io/cf-pks-golf/pcfkubo/ulimit", "--restart=Never", "-i", "--rm")
 		Expect(err).NotTo(HaveOccurred())
-		Expect(strconv.Atoi(output[0])).To(BeNumerically(">=", 1048576))
+		Expect(strconv.Atoi(output[0])).To(BeNumerically(">=", 65535))
 	})
 
 })
