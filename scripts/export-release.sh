5fea5649ae888602a85363b8c73659adc7c54245
Neil Hickey <nhickey@vmware.com>
2021-07-01 16:13:35 -0600

    Export now uses kubo-release
    Signed-off-by: Neil Hickey <nhickey@vmware.com>

--- scripts/export-release.sh
+++ scripts/export-release.sh
@@ -5,7 +5,7 @@ set -eu
 stemcell_alias=${stemcell_alias:-default}
 source git-kubo-ci/scripts/set-bosh-env gcs-source-json/source.json
 
-pushd git-kubo-deployment/manifests
+pushd git-kubo-release/manifests
 STEMCELL_OS=$(bosh int cfcr.yml --ops-file ops-files/windows/add-worker.yml --path=/stemcells/alias=${stemcell_alias}/os)
 STEMCELL_VERSION=$(bosh int cfcr.yml --ops-file ops-files/windows/add-worker.yml --path=/stemcells/alias=${stemcell_alias}/version )
 popd
@@ -21,7 +21,7 @@ do
   if [[ $release == kubo* ]]; then
     RELEASE_VERSION="$(cat kubo-version/version)"
   else
-    RELEASE_VERSION="$(bosh int git-kubo-deployment/manifests/cfcr.yml -o git-kubo-deployment/manifests/ops-files/non-precompiled-releases.yml --path=/releases/name=${release}/version)"
+    RELEASE_VERSION="$(bosh int git-kubo-release/manifests/cfcr.yml -o git-kubo-release/manifests/ops-files/non-precompiled-releases.yml --path=/releases/name=${release}/version)"
   fi
   bosh -d compilation-${stemcell_alias} export-release "$release/$RELEASE_VERSION" "$STEMCELL_OS/$STEMCELL_VERSION" ${jobs}
 
