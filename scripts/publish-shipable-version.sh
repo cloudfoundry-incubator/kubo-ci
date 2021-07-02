b57c40065da476541015f9b874087741051b198e
Neil Hickey <nhickey@vmware.com>
2021-07-01 18:14:27 -0600

    Remove kubo-deployment from shippable hash
    Signed-off-by: Neil Hickey <nhickey@vmware.com>

--- scripts/publish-shipable-version.sh
+++ scripts/publish-shipable-version.sh
@@ -6,7 +6,6 @@ cp gcs-shipable-version/$signal gcs-shipable-version-output/shipable
 mkdir kubo-release-untarred
 tar -xzf kubo-release/kubo-*.tgz --directory kubo-release-untarred
 release_hash=$(grep "commit_hash" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}' | tr -d "'")
-deployment_hash="$(cat git-kubo-deployment/.git/short_ref | tr -d "'")"
 dev_version="$(grep "^version" kubo-release-untarred/release.MF | awk -F ' ' '{print $2}')"
 
-printf "%s %s %s\n" "${release_hash}" "${deployment_hash}" "${dev_version}">> "gcs-shipable-version-output/shipable"
+printf "%s %s\n" "${release_hash}" "${dev_version}">> "gcs-shipable-version-output/shipable"
