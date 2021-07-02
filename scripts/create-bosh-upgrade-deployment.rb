6a1527076d89c21a8ce3d4e175682c845ccb52b6
Neil Hickey <nhickey@vmware.com>
2021-07-01 19:07:18 -0600

    Update upgrade tasks to use kubo-release
    Signed-off-by: Neil Hickey <nhickey@vmware.com>

--- scripts/create-bosh-upgrade-deployment.rb
+++ scripts/create-bosh-upgrade-deployment.rb
@@ -1,10 +1,10 @@
 #! /usr/bin/env ruby
 
-ops_files = '-o git-kubo-deployment/manifests/ops-files/rename.yml\
- -o git-kubo-deployment/manifests/ops-files/vm-types.yml\
- -o git-kubo-deployment/manifests/ops-files/misc/dev.yml \
- -o git-kubo-deployment/manifests/ops-files/enable-nfs.yml \
- -o git-kubo-deployment/manifests/ops-files/use-persistent-disk-for-workers.yml \
+ops_files = '-o git-kubo-release/manifests/ops-files/rename.yml\
+ -o git-kubo-release/manifests/ops-files/vm-types.yml\
+ -o git-kubo-release/manifests/ops-files/misc/dev.yml \
+ -o git-kubo-release/manifests/ops-files/enable-nfs.yml \
+ -o git-kubo-release/manifests/ops-files/use-persistent-disk-for-workers.yml \
  -o git-kubo-ci/manifests/ops-files/add-api-server-endpoint.yml '
 vars_files = '-l gcs-load-balancer-vars/load-balancer-vars.yml -l kubo-lock/metadata '
 vars = "-v deployment_name=#{ENV['DEPLOYMENT_NAME']} -v worker_vm_type=worker -v master_vm_type=master -v apply_addons_vm_type=minimal -v disk_size=10120"
@@ -13,32 +13,32 @@ vars = "-v deployment_name=#{ENV['DEPLOYMENT_NAME']} -v worker_vm_type=worker -v
 if ENV['ENABLE_MULTI_AZ_TESTS'] != 'false'
   ops_files << '-o git-kubo-ci/manifests/ops-files/enable-multiaz-workers.yml '
 else
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/misc/single-master.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/misc/single-master.yml '
   ops_files << '-o git-kubo-ci/manifests/ops-files/scale-to-one-az.yml '
 end
 
 if ENV['IAAS'] =~ /^gcp/
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/iaas/gcp/cloud-provider.yml '
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/iaas/gcp/add-subnetwork-for-internal-load-balancer.yml '
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/use-vm-extensions.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/iaas/gcp/cloud-provider.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/iaas/gcp/add-subnetwork-for-internal-load-balancer.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/use-vm-extensions.yml '
 end
 
 if ENV['IAAS'] =~ /^vsphere/
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/iaas/vsphere/cloud-provider.yml '
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/iaas/vsphere/use-vm-extensions.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/iaas/vsphere/cloud-provider.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/iaas/vsphere/use-vm-extensions.yml '
   vars_files << '-l director_uuid/var.yml '
 end
 
 
 if ENV['IAAS'] =~ /^vsphere-proxy/
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/add-proxy.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/add-proxy.yml '
   ops_files << '-o git-kubo-ci/manifests/ops-files/add-master-static-ips.yml '
 end
 
 if ENV['IAAS'] =~ /^aws/
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/iaas/aws/cloud-provider.yml '
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/iaas/aws/lb.yml '
-  ops_files << '-o git-kubo-deployment/manifests/ops-files/use-vm-extensions.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/iaas/aws/cloud-provider.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/iaas/aws/lb.yml '
+  ops_files << '-o git-kubo-release/manifests/ops-files/use-vm-extensions.yml '
 end
 
 cmd = ['bosh -n -d',
